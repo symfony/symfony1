21a52d16acb3dc75f73fa654fbf26bc65a134074
Fabien Potencier <fabien.potencier@symfony-project.org>
2005-10-22 21:41:39 +0000

    fix some Notices on request unit tests
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@11 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/request/sfWebRequestTest.php
+++ test/request/sfWebRequestTest.php
@@ -12,6 +12,8 @@ class sfWebRequestTest extends UnitTestCase
 
   public function SetUp()
   {
+    sfRouting::getInstance()->clearRoutes();
+
     @define('SF_STATS', false);
     @define('SF_PATH_INFO_ARRAY', 'SERVER');
     @define('SF_PATH_INFO_KEY', 'PATH_INFO');
@@ -36,24 +38,34 @@ class sfWebRequestTest extends UnitTestCase
     $_SERVER['SCRIPT_NAME'] = '/index.php';
 
     if ($request_uri{0} != '/')
+    {
       $request_uri = '/'.$request_uri;
+    }
 
     // add index.php if needed
     if (!strpos($request_uri, '.php'))
+    {
       $request_uri = '/index.php'.$request_uri;
+    }
 
     // query string
     $_SERVER['QUERY_STRING'] = '';
     if ($query_string_pos = strpos($request_uri, '?'))
+    {
       $_SERVER['QUERY_STRING'] = substr($request_uri, $query_string_pos + 1);
+    }
     else
+    {
       $query_string_pos = strlen($request_uri);
+    }
 
     // path info
     $_SERVER['PATH_INFO'] = '/';
     $script_pos = strpos($request_uri, '.php') + 5;
     if ($script_pos < $query_string_pos)
+    {
       $_SERVER['PATH_INFO'] = '/'.substr($request_uri, $script_pos, $query_string_pos - $script_pos);
+    }
 
     // parse query string
     $params = explode('&', $_SERVER['QUERY_STRING']);
@@ -72,7 +84,10 @@ class sfWebRequestTest extends UnitTestCase
       $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
     }
     else
+    {
       unset($_SERVER['HTTP_X_REQUESTED_WITH']);
+    }
   }
 }
+
 ?>
