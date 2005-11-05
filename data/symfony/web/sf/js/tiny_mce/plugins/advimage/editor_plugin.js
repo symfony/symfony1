93631ff2abbb3c080f8bbee17271e503054e7639
Fabien Potencier <fabien.potencier@symfony-project.org>
2005-11-05 13:21:54 +0000

    removed tiny mce from svn
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@55 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/symfony/web/sf/js/tiny_mce/plugins/advimage/editor_plugin.js
+++
@@ -1,2 +0,0 @@
-/* Import plugin specific language pack */
- tinyMCE.importPluginLanguagePack('advimage','en,de,sv,zh_cn,cs,fa,fr_ca,fr,pl');function TinyMCE_advimage_getInsertImageTemplate(){var template=new Array();template['file']='../../plugins/advimage/image.htm';template['width']=430;template['height']=380;template['width']+=tinyMCE.getLang('lang_insert_image_delta_width',0);template['height']+=tinyMCE.getLang('lang_insert_image_delta_height',0);return template;}function TinyMCE_advimage_handleEvent(editor_id,body,doc){alert(editor_id+","+body.innerHTML);}
