de15aaca30c5b8113b80369d79c60b6a2c2e8dab
Fabien Potencier <fabien.potencier@symfony-project.org>
2005-11-17 14:54:51 +0000

    remove XsltProcessor dependancy in Propel
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@138 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/propel-generator/classes/propel/engine/database/transform/XmlToAppData.php
+++ lib/propel-generator/classes/propel/engine/database/transform/XmlToAppData.php
@@ -98,25 +98,10 @@ class XmlToAppData extends AbstractHandler {
             return;
         }
 
-			$domDocument = new DomDocument('1.0', 'UTF-8');
-			$domDocument->load($xmlFile);
+        // store current schema file path 
+        $this->schemasTagsStack[$xmlFile] = array(); 
 
-			$xsl = new XsltProcessor();
-			$xsl->importStyleSheet(DomDocument::load(realpath(dirname(__FILE__) . "/xsl/database.xsl")));
-			$transformed = $xsl->transformToDoc($domDocument);
-
-			$xmlFile = $xmlFile . "transformed.xml";
-			$transformed->save($xmlFile);
-
-        	// store current schema file path
-        	$this->schemasTagsStack[$xmlFile] = array();
-
-        	$this->currentXmlFile = $xmlFile;
-
-
-			if ($transformed->getElementsByTagName("database")->item(0)->getAttribute("noxsd") != "true")
-				if (!$transformed->schemaValidate(realpath(dirname(__FILE__) . "/xsd/database.xsd")))
-					throw new EngineException("XML schema does not validate, sorry...");
+        $this->currentXmlFile = $xmlFile;
 
         try {
             $fr = new FileReader($xmlFile);
