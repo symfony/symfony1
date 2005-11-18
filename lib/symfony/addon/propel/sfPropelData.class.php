e4f7ccc8aa47cbd935d09a4fc2ca15050b3ba289
Fabien Potencier <fabien.potencier@symfony-project.org>
2005-11-18 06:24:51 +0000

    added a new class to load YAML datas into database (Propel specific)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@141 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/symfony/addon/propel/sfPropelData.class.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once('pake/pakeFinder.class.php');
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004, 2005 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ *
+ * @package    symfony
+ * @subpackage addon
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+class sfPropelData
+{
+  private
+    $deleteCurrentDatas = true;
+
+  public function setDeleteCurrentDatas($boolean)
+  {
+    $this->deleteCurrentDatas = $boolean;
+  }
+
+  public function getDeleteCurrentDatas()
+  {
+    return $this->deleteCurrentDatas;
+  }
+
+  // data/ -> name voir c2 test en tableaux
+  // symfony load-data (file|dir) (option pour supprimer)
+  // todo: symfony dump-data
+  public function loadData($directory_or_file = null)
+  {
+    // directory or file?
+    $fixture_files = array();
+    if (!$directory_or_file)
+    {
+      $directory_or_file = SF_DATA_DIR.'/fixtures';
+    }
+
+    if (is_file($directory_or_file))
+    {
+      $fixture_files[] = $directory_or_file;
+    }
+    else if (is_dir($directory_or_file))
+    {
+      $fixture_files = pakeFinder::type('file')->name('*.yml')->in($directory_or_file);
+    }
+    else
+    {
+      throw new sfInitializationException('You must give a directory or file.');
+    }
+
+    $objects = array();
+
+    // delete all current datas in database
+    if ($this->deleteCurrentDatas)
+    {
+      rsort($fixture_files);
+      foreach ($fixture_files as $fixture_file)
+      {
+        $main_datas = sfYaml::load($fixture_file);
+        $classes = array_keys($main_datas);
+        krsort($classes);
+        foreach ($classes as $class)
+        {
+          $peer_class = $class.'Peer';
+
+          call_user_func(array($peer_class, 'doDeleteAll'));
+        }
+      }
+    }
+
+    sort($fixture_files);
+    $maps = array();
+    foreach ($fixture_files as $fixture_file)
+    {
+      // import new datas
+      $main_datas = sfYaml::load($fixture_file);
+      foreach ($main_datas as $class => $datas)
+      {
+        $peer_class = $class.'Peer';
+
+        // load map class
+        $class_map_builder = $class.'MapBuilder';
+        require_once('model/map/'.$class_map_builder.'.php');
+        if (!isset($maps[$class]))
+        {
+          $maps[$class] = new $class_map_builder();
+          $maps[$class]->doBuild();
+        }
+        $tableMap = $maps[$class]->getDatabaseMap()->getTable(constant($peer_class.'::TABLE_NAME'));
+
+        // iterate through datas for this class
+        foreach ($datas as $key => $data)
+        {
+          // create a new entry in the database
+          $obj = new $class();
+          foreach ($data as $name => $value)
+          {
+            // foreign key?
+            $column = $tableMap->getColumn($name);
+            if ($column->isForeignKey())
+            {
+              $relatedTable = $maps[$class]->getDatabaseMap()->getTable($column->getRelatedTableName());
+              $value = $objects[$relatedTable->getPhpName().'_'.$value];
+            }
+
+            $obj->setByName($name, $value, BasePeer::TYPE_FIELDNAME);
+          }
+          $obj->save();
+
+          // save the id for future reference
+          if (method_exists($obj, 'getPrimaryKey'))
+          {
+            $objects[$class.'_'.$key] = $obj->getPrimaryKey();
+          }
+        }
+      }
+    }
+  }
+}
+
+?>
