4f1ade0ff02a0a819ef89746c9ac93f38d9bb5db
Fabien Potencier <fabien.potencier@symfony-project.org>
2005-11-26 09:31:27 +0000

    fixed case on execute action method name
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@180 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/symfony/action/sfActions.class.php
+++ lib/symfony/action/sfActions.class.php
@@ -43,12 +43,8 @@ abstract class sfActions extends sfAction
   public function execute()
   {
     // Dispatch action
-    $actionToRun = '';
-    if (method_exists($this, 'execute'.$this->getActionName()))
-    {
-      $actionToRun = 'execute'.$this->getActionName();
-    }
-    else
+    $actionToRun = 'execute'.ucfirst($this->getActionName());
+    if (!method_exists($this, $actionToRun))
     {
       // action not found
       $error = 'sfAction initialization failed for module "%s", action "%s"';
