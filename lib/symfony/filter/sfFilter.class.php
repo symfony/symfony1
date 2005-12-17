b39056bb4d09dae7cf28f774938dad26e4d90f9f
Fabien Potencier <fabien.potencier@symfony-project.org>
2005-12-17 10:36:30 +0000

    added methods for sfFilter parameters
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@273 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/symfony/filter/sfFilter.class.php
+++ lib/symfony/filter/sfFilter.class.php
@@ -65,6 +65,26 @@ abstract class sfFilter
 
     return true;
   }
+
+  public function getParameterHolder()
+  {
+    return $this->parameter_holder;
+  }
+
+  public function getParameter($name, $default = null, $ns = null)
+  {
+    return $this->parameter_holder->get($name, $default, $ns);
+  }
+
+  public function hasParameter($name, $ns = null)
+  {
+    return $this->parameter_holder->has($name, $ns);
+  }
+
+  public function setParameter($name, $value, $ns = null)
+  {
+    return $this->parameter_holder->set($name, $value, $ns);
+  }
 }
 
 ?>
