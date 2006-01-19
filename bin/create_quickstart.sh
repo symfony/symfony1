4fefc80f358779825feb051195ad95ca36efd67b
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-01-19 21:00:59 +0000

    modified quickstart to be able to run without symfony installed
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@476 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- bin/create_quickstart.sh
+++ bin/create_quickstart.sh
@@ -11,8 +11,19 @@ SVN_PATH=trunk
 echo ">>> project initialization"
 mkdir ${SANDBOX_NAME}
 cd ${SANDBOX_NAME}
-symfony init-project ${SANDBOX_NAME}
-symfony init-app ${APP_NAME}
+mkdir lib bin data web
+
+# pake
+
+echo ">>> add pake package"
+mkdir tmp
+cd tmp
+curl http://www.pake-project.org/downloads/pake-current.tgz > pake.tgz
+tar zxpf pake.tgz
+mv pake-`awk '/<release>/ {print $1}' package.xml | sed 's#<release>##' | sed 's#</release>##' | head -n 1`/lib/pake ../lib/pake
+mv pake-`awk '/<release>/ {print $1}' package.xml | sed 's#<release>##' | sed 's#</release>##' | head -n 1`/bin/pake.php ../bin/
+cd ..
+rm -rf tmp
 
 # symfony libraries
 
@@ -26,14 +37,23 @@ mv data/symfony/web/sf web/sf
 # symfony command line
 
 echo ">>> add symfony command line"
-svn export http://svn.symfony-project.com/${SVN_PATH}/bin/ bin/
-mv bin/symfony.sh symfony.sh
-mv bin/symfony.bat symfony.bat
+mkdir tmp
+svn export http://svn.symfony-project.com/${SVN_PATH}/bin/ tmp/bin/
+mv tmp/bin/symfony.sh symfony.sh
+mv tmp/bin/symfony.bat symfony.bat
+mv tmp/bin/symfony.php bin/symfony.php
+rm -rf tmp
 sed -i '' -e "s#@PEAR-DIR@#bin#g" symfony.sh
 sed -i '' -e "s#@PEAR-DIR@#bin#g" symfony.bat
 sed -i '' -e "s#'@PEAR-DIR@'#dirname(__FILE__).'/../lib'#g" -e "s#'@DATA-DIR@'#dirname(__FILE__).'/../data'#g" -e "s#@SYMFONY-VERSION@#0.6.0#g" -e "s#require_once 'pake.php'#require_once dirname(__FILE__).'/pake.php'#g" bin/symfony.php
 sed -i '' -e "s#'@PEAR-DIR@/symfony'#dirname(__FILE__)#g" -e "s#'@DATA-DIR@#dirname(__FILE__).'/../../data#g" -e "s#@SYMFONY-VERSION@#0.6.0#g" lib/symfony/pear.php
 chmod 755 symfony.sh
+
+# default project / app
+
+echo ">>> create a new project and a new app"
+./symfony.sh init-project ${SANDBOX_NAME}
+./symfony.sh -f data/symfony/bin/pakefile.php init-app ${APP_NAME}
 
 # phing
 
@@ -43,18 +63,6 @@ cd tmp
 curl http://phing.info/pear/phing-current.tgz > phing.tgz
 tar zxpf phing.tgz
 mv phing-`awk '/<version>/ {print $1}' package.xml | sed 's#<version>##' | sed 's#</version>##' | head -n 1` ../lib/phing
-cd ..
-rm -rf tmp
-
-# pake
-
-echo ">>> add pake package"
-mkdir tmp
-cd tmp
-curl http://www.pake-project.org/downloads/pake-current.tgz > pake.tgz
-tar zxpf pake.tgz
-mv pake-`awk '/<release>/ {print $1}' package.xml | sed 's#<release>##' | sed 's#</release>##' | head -n 1`/lib/pake ../lib/pake
-mv pake-`awk '/<release>/ {print $1}' package.xml | sed 's#<release>##' | sed 's#</release>##' | head -n 1`/bin/pake.php ../bin/
 cd ..
 rm -rf tmp
 
