94cf71e296404e721c3dc3d97242ed03d4f9abd8
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-01-30 18:46:04 +0000

    fixed propelCrudGeneratorTest
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@641 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/liveapp/propelCrudGenerator/propelCrudGeneratorTest.php
+++ test/liveapp/propelCrudGenerator/propelCrudGeneratorTest.php
@@ -30,9 +30,9 @@ class propelCrudGeneratorTest extends sfLiveProjectUnitTestCase
     $autoload_config_file = sfConfig::get('sf_app_config_dir_name').'/autoload.yml';
     if (is_readable($autoload_config_file))
     {
-      unlink(sfConfigCache::getCacheName($autoload_config_file));
+      unlink(sfConfigCache::getInstance()->getCacheName($autoload_config_file));
     }
-    require(sfConfigCache::checkConfig($autoload_config_file));
+    require(sfConfigCache::getInstance()->checkConfig($autoload_config_file));
 
     // create database
     $this->runSymfony('insert-sql');
