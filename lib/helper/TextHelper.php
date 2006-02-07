a7ba04be190813d6dcd2dde1ee4ac00532aa566f
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-02-07 15:29:28 +0000

    fixed a typo
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@757 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/TextHelper.php
+++ lib/helper/TextHelper.php
@@ -206,5 +206,12 @@
   {
     return preg_replace('/([\w\.!#\$%\-+.]+@[A-Za-z0-9\-]+(\.[A-Za-z0-9\-]+)+)/', '<a href="mailto:\\1">\\1</a>', $text);
   }
-
+  
+  /**
+   * Wraps echo to automatically provide a newline
+   */
+  function echoln($string)
+  {
+    echo $string."\n";
+  }
 ?>
