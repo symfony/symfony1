9001e4ed7534f58c5d7d00bd23572cd62d3938d9
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-02-09 12:28:48 +0000

    added possibility to remove all actions from list in admin generator
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@780 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelAdmin/default/template/templates/_list_actions.php
+++ data/generator/sfPropelAdmin/default/template/templates/_list_actions.php
@@ -1,8 +1,11 @@
 <ul class="sf_admin_actions">
-<?php if ($this->getParameterValue('list.actions')): ?>
-<?php foreach ($this->getParameterValue('list.actions') as $actionName => $params): ?>
-  <?php echo $this->getButtonToAction($actionName, $params, false) ?>
-<?php endforeach ?>
+<?php $listActions = $this->getParameterValue('list.actions') ?>
+<?php if ($listActions): ?>
+  <?php if (is_array($listActions)): ?>
+    <?php foreach ($listActions as $actionName => $params): ?>
+      <?php echo $this->getButtonToAction($actionName, $params, false) ?>
+    <?php endforeach ?>
+  <?php endif ?>
 <?php else: ?>
   <?php echo $this->getButtonToAction('_create', array(), false) ?>
 <?php endif ?>
