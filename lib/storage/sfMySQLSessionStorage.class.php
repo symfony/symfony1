63e8978af7cc4685ad45f1f3240665dc7afdcf4d
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-02-26 18:33:05 +0000

    fixed a bug in MySQL session storage (fixed #206)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@893 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/storage/sfMySQLSessionStorage.class.php
+++ lib/storage/sfMySQLSessionStorage.class.php
@@ -142,7 +142,7 @@ class sfMySQLSessionStorage extends sfSessionStorage
 
     // delete the record associated with this id
     $sql = 'DELETE FROM ' . $db_table . ' ' .
-           'WHERE ' . $db_time_col . ' < ' . $lifetime;
+           'WHERE ' . $db_time_col . ' < ' . $life;
 
     if (@mysql_query($sql, $this->resource))
       return true;
