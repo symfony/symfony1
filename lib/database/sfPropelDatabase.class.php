203a846037e71b93bdce363a0a095ae8208a8a83
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-03-15 10:40:42 +0000

    added a new setConnectionParameter to sfPropelDatabase class
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@974 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/database/sfPropelDatabase.class.php
+++ lib/database/sfPropelDatabase.class.php
@@ -89,6 +89,11 @@ class sfPropelDatabase extends sfCreoleDatabase
   {
     return self::$config;
   }
+
+  public function setConnectionParameter ($key, $value)
+  {
+    self::$config['propel']['datasources'][$this->getParameter('datasource')]['connection'][$key] = $value;
+  }
 }
 
 ?>
