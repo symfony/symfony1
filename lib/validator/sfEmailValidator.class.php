9cf8f85e3fc8001235d962751a0c0d2463e87d78
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-04-07 09:15:45 +0000

    changed email validation regex (closes #399 - Cal Henderson)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1140 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/validator/sfEmailValidator.class.php
+++ lib/validator/sfEmailValidator.class.php
@@ -27,15 +27,41 @@ class sfEmailValidator extends sfValidator
    * @param mixed A file or parameter value/array.
    * @param error An error message reference.
    *
-   * @return bool true, if this validator executes successfully, otherwise
-   *              false.
-   *
-   * @author Sean Kerr (skerr@mojavi.org)
-   * @since  3.0.0
+   * @return bool true, if this validator executes successfully, otherwise false.
    */
   public function execute (&$value, &$error)
   {
-    if (!preg_match('~^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$~i', $value))
+    /* Cal Henderson: http://iamcal.com/publish/articles/php/parsing_email/pdf/
+     * The long regular expression below is made by the following code
+     * fragment:
+     *
+     *   $qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
+     *   $dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
+     *   $atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c'
+     *         . '\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
+     *   $quoted_pair = '\\x5c\\x00-\\x7f';
+     *   $domain_literal = "\\x5b($dtext|$quoted_pair)*\\x5d";
+     *   $quoted_string = "\\x22($qtext|$quoted_pair)*\\x22";
+     *   $domain_ref = $atom;
+     *   $sub_domain = "($domain_ref|$domain_literal)";
+     *   $word = "($atom|$quoted_string)";
+     *   $domain = "$sub_domain(\\x2e$sub_domain)*";
+     *   $local_part = "$word(\\x2e$word)*";
+     *   $addr_spec = "$local_part\\x40$domain";
+     */
+    $re = '/^([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-'
+         .'\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80-\\xff]|\\x5c\\x00-'
+         .'\\x7f)*\\x22)(\\x2e([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-'
+         .'\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80'
+         .'-\\xff]|\\x5c\\x00-\\x7f)*\\x22))*\\x40([^\\x00-\\x20\\x22\\x28\\x29'
+         .'\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x5b([^'
+         .'\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*\\x5d)(\\x2e([^\\x00-'
+         .'\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-'
+         .'\\xff]+|\\x5b([^\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*'
+         .'\\x5d))*$/'
+    ;
+
+    if (!preg_match($re, $value))
     {
       $error = $this->getParameterHolder()->get('email_error');
       return false;
