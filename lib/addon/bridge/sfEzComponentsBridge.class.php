ca19be67fb01c39bfb9362541202fe14da2f5e18
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-04-23 05:44:10 +0000

    added sfZendFrameworkBridge and sfEzComponentsBridge classes
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1248 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/addon/bridge/sfEzComponentsBridge.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once((sfConfig::get('sf_ez_lib_dir') ? sfConfig::get('sf_ez_lib_dir').'/' : '').'Base/src/base.php');
+
+/**
+ * This class makes easy to use ez components classes within symfony
+ *
+ * @package    symfony
+ * @subpackage addon
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+class sfEzComponentsBridge
+{
+  public static function autoload($class)
+  {
+    return ezcBase::autoload($class);
+  }
+}
+
+?>
