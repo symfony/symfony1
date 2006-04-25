eb9ba9b5bcd316f78f8220695990213cbf979b8f
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-04-25 09:10:59 +0000

    added 2 new methods to action class in admin generator (saveObject and deleteObject) to ease inheritance
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1268 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelAdmin/default/template/actions/actions.class.php
+++ data/generator/sfPropelAdmin/default/template/actions/actions.class.php
@@ -60,7 +60,8 @@ class <?php echo $this->getGeneratedModuleName() ?>Actions extends sfActions
       $this-><?php echo $this->getSingularName() ?> = $this->get<?php echo $this->getClassName() ?>OrCreate();
 
       $this->update<?php echo $this->getClassName() ?>FromRequest();
-      $this-><?php echo $this->getSingularName() ?>->save();
+
+      $this->save<?php echo $this->getClassName() ?>($this-><?php echo $this->getSingularName() ?>);
 
       $this->setFlash('notice', 'Your modifications have been saved');
 
@@ -87,7 +88,7 @@ class <?php echo $this->getGeneratedModuleName() ?>Actions extends sfActions
     $this-><?php echo $this->getSingularName() ?> = <?php echo $this->getClassName() ?>Peer::retrieveByPk(<?php echo $this->getRetrieveByPkParamsForAction(40) ?>);
     $this->forward404Unless($this-><?php echo $this->getSingularName() ?>);
 
-    $this-><?php echo $this->getSingularName() ?>->delete();
+    $this->delete<?php echo $this->getClassName() ?>($this-><?php echo $this->getSingularName() ?>);
 
 <?php foreach ($this->getColumnCategories('edit.display') as $category): ?>
 <?php foreach ($this->getColumns('edit.display', $category) as $name => $column): ?>
@@ -113,6 +114,16 @@ class <?php echo $this->getGeneratedModuleName() ?>Actions extends sfActions
     $this->update<?php echo $this->getClassName() ?>FromRequest();
 
     return sfView::SUCCESS;
+  }
+
+  protected function save<?php echo $this->getClassName() ?>($<?php echo $this->getSingularName() ?>)
+  {
+    $<?php echo $this->getSingularName() ?>->save();
+  }
+
+  protected function delete<?php echo $this->getClassName() ?>($<?php echo $this->getSingularName() ?>)
+  {
+    $<?php echo $this->getSingularName() ?>->delete();
   }
 
   protected function update<?php echo $this->getClassName() ?>FromRequest()
