46a2076e28ad5e3c0352fa9203aada2170f81a71
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-05-09 18:07:48 +0000

    fixed Problem with sfRequest (closes #523)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1334 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/request/sfRequest.class.php
+++ lib/request/sfRequest.class.php
@@ -63,7 +63,8 @@ abstract class sfRequest
   {
     $array = array();
 
-    foreach ($this->parameter_holder->getAll() as $key => &$value)
+    $parameters =& $this->parameter_holder->getAll();
+    foreach ($parameters as $key => &$value)
     {
       if (in_array($key, $names))
       {
