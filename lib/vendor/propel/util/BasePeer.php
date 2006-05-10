64d1b30c2b9070d314cf867cef18856ca12934cc
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-05-10 18:54:03 +0000

    fixed problem with mysql 5 in Propel
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1336 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/vendor/propel/util/BasePeer.php
+++ lib/vendor/propel/util/BasePeer.php
@@ -832,7 +832,7 @@ class BasePeer
 		$sql =  "SELECT "
 				.($selectModifiers ? implode(" ", $selectModifiers) . " " : "")
 				.implode(", ", $selectClause)
-				." FROM ".implode(", ", $fromClause)
+				." FROM ".(count($fromClause) > 1 ? "(".implode(", ", $fromClause).")" : $fromClause[0])
 								.($joinClause ? ' ' . implode(' ', $joinClause) : '')
 				.($whereClause ? " WHERE ".implode(" AND ", $whereClause) : "")
 				.($groupByClause ? " GROUP BY ".implode(",", $groupByClause) : "")
