f46e5d2613179df0d54aa7a1b7c52d68b2fa9b1c
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-05-12 19:55:44 +0000

    updated pl calendar translation (closes #531 - patch from krzychu.danek)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1349 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/web/sf/js/calendar/lang/calendar-pl.js
+++ data/web/sf/js/calendar/lang/calendar-pl.js
@@ -1,19 +1,81 @@
-// ** I18N
+﻿// ** I18N
+
 // Calendar PL language
-// Author: Artur Filipiak, <imagen@poczta.fm>
-// January, 2004
+// Author: Krzychu Danek, <krzychu.danek@gmail.com>
 // Encoding: UTF-8
+// Distributed under the same terms as the calendar itself.
+
+// For translators: please use UTF-8 if possible.  We strongly believe that
+// Unicode is the answer to a real internationalized world.  Also please
+// include your contact information in the header, as can be seen above.
+
+// full day names
 Calendar._DN = new Array
-("Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela");
+("Niedziela",
+ "Poniedziałek",
+ "Wtorek",
+ "Środa",
+ "Czwartek",
+ "Piątek",
+ "Sobota",
+ "Niedziela");
 
+// Please note that the following array of short day names (and the same goes
+// for short month names, _SMN) isn't absolutely necessary.  We give it here
+// for exemplification on how one can customize the short day names, but if
+// they are simply the first N letters of the full name you can simply say:
+//
+//   Calendar._SDN_len = N; // short day name length
+//   Calendar._SMN_len = N; // short month name length
+//
+// If N = 3 then this is not needed either since we assume a value of 3 if not
+// present, to be compatible with translation files that were written before
+// this feature.
+
+// short day names
 Calendar._SDN = new Array
-("N", "Pn", "Wt", "Śr", "Cz", "Pt", "So", "N");
+("N",
+ "Pon",
+ "Wt",
+ "Śr",
+ "Czw",
+ "Pi",
+ "So",
+ "N");
 
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 1;
+
+// full month names
 Calendar._MN = new Array
-("Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień");
+("Styczeń",
+ "Luty",
+ "Marzec",
+ "Kwiecień",
+ "Maj",
+ "Czerwiec",
+ "Lipiec",
+ "Sierpień",
+ "Wrzesień",
+ "Październik",
+ "Listopad",
+ "Grudzień");
 
+// short month names
 Calendar._SMN = new Array
-("Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru");
+("Sty",
+ "Lut",
+ "Mar",
+ "Kwi",
+ "Maj",
+ "Cze",
+ "Lip",
+ "Sie",
+ "Wrz",
+ "Paź",
+ "Lis",
+ "Gru");
 
 // tooltips
 Calendar._TT = {};
@@ -43,14 +105,23 @@ Calendar._TT["NEXT_YEAR"] = "Nast. rok (przytrzymaj dla menu)";
 Calendar._TT["SEL_DATE"] = "Wybierz datę";
 Calendar._TT["DRAG_TO_MOVE"] = "Przesuń okienko";
 Calendar._TT["PART_TODAY"] = " (dziś)";
-Calendar._TT["MON_FIRST"] = "Pokaż Poniedziałek jako pierwszy";
-Calendar._TT["SUN_FIRST"] = "Pokaż Niedzielę jako pierwszą";
+
+// the following is to inform that "%s" is to be the first day of week
+// %s will be replaced with the day name.
+Calendar._TT["DAY_FIRST"] = "Pokaż dzień %s jako pierwszy";
+
+// This may be locale-dependent.  It specifies the week-end days, as an array
+// of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
+// means Monday, etc.
+Calendar._TT["WEEKEND"] = "0,6";
+
 Calendar._TT["CLOSE"] = "Zamknij";
 Calendar._TT["TODAY"] = "Dziś";
 Calendar._TT["TIME_PART"] = "(Shift-)klik | drag, aby zmienić wartość";
 
 // date formats
-Calendar._TT["DEF_DATE_FORMAT"] = "%Y.%m.%d";
+Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
 Calendar._TT["TT_DATE_FORMAT"] = "%a, %b %e";
 
 Calendar._TT["WK"] = "wk";
+Calendar._TT["TIME"] = "Czas:";
