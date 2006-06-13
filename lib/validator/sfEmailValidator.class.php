8aad14b4a9ae02d789c9611b0e8ec95b337ecdcb
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-06-13 12:33:45 +0000

    * Added 'strict' option, with a more rigid regex. (Fixes ticket #462)  * Note: 'strict' option is marked true by default (backward compatibility issue) (closes #462 - patch from slickrick)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1437 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/validator/sfEmailValidator.class.php
+++ lib/validator/sfEmailValidator.class.php
@@ -31,35 +31,44 @@ class sfEmailValidator extends sfValidator
    */
   public function execute (&$value, &$error)
   {
-    /* Cal Henderson: http://iamcal.com/publish/articles/php/parsing_email/pdf/
-     * The long regular expression below is made by the following code
-     * fragment:
-     *
-     *   $qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
-     *   $dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
-     *   $atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c'
-     *         . '\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
-     *   $quoted_pair = '\\x5c\\x00-\\x7f';
-     *   $domain_literal = "\\x5b($dtext|$quoted_pair)*\\x5d";
-     *   $quoted_string = "\\x22($qtext|$quoted_pair)*\\x22";
-     *   $domain_ref = $atom;
-     *   $sub_domain = "($domain_ref|$domain_literal)";
-     *   $word = "($atom|$quoted_string)";
-     *   $domain = "$sub_domain(\\x2e$sub_domain)*";
-     *   $local_part = "$word(\\x2e$word)*";
-     *   $addr_spec = "$local_part\\x40$domain";
-     */
-    $re = '/^([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-'
-         .'\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80-\\xff]|\\x5c\\x00-'
-         .'\\x7f)*\\x22)(\\x2e([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-'
-         .'\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80'
-         .'-\\xff]|\\x5c\\x00-\\x7f)*\\x22))*\\x40([^\\x00-\\x20\\x22\\x28\\x29'
-         .'\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x5b([^'
-         .'\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*\\x5d)(\\x2e([^\\x00-'
-         .'\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-'
-         .'\\xff]+|\\x5b([^\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*'
-         .'\\x5d))*$/'
-    ;
+    $strict = $this->getParameterHolder()->get('strict');
+    if ($strict == true)
+    {
+      $re = '/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i';
+    }
+    else
+    {
+      /* Cal Henderson: http://iamcal.com/publish/articles/php/parsing_email/pdf/
+       * The long regular expression below is made by the following code
+       * fragment:
+       *
+       *   $qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
+       *   $dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
+       *   $atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c'
+       *         . '\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
+       *   $quoted_pair = '\\x5c\\x00-\\x7f';
+       *   $domain_literal = "\\x5b($dtext|$quoted_pair)*\\x5d";
+       *   $quoted_string = "\\x22($qtext|$quoted_pair)*\\x22";
+       *   $domain_ref = $atom;
+       *   $sub_domain = "($domain_ref|$domain_literal)";
+       *   $word = "($atom|$quoted_string)";
+       *   $domain = "$sub_domain(\\x2e$sub_domain)*";
+       *   $local_part = "$word(\\x2e$word)*";
+       *   $addr_spec = "$local_part\\x40$domain";
+       */
+
+      $re = '/^([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-'
+           .'\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80-\\xff]|\\x5c\\x00-'
+           .'\\x7f)*\\x22)(\\x2e([^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-'
+           .'\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x22([^\\x0d\\x22\\x5c\\x80'
+           .'-\\xff]|\\x5c\\x00-\\x7f)*\\x22))*\\x40([^\\x00-\\x20\\x22\\x28\\x29'
+           .'\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+|\\x5b([^'
+           .'\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*\\x5d)(\\x2e([^\\x00-'
+           .'\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-'
+           .'\\xff]+|\\x5b([^\\x0d\\x5b-\\x5d\\x80-\\xff]|\\x5c\\x00-\\x7f)*'
+           .'\\x5d))*$/'
+      ;
+    }
 
     if (!preg_match($re, $value))
     {
@@ -76,6 +85,7 @@ class sfEmailValidator extends sfValidator
     parent::initialize($context);
 
     // set defaults
+    $this->getParameterHolder()->set('strict',      true);
     $this->getParameterHolder()->set('email_error', 'Invalid input');
 
     $this->getParameterHolder()->add($parameters);
