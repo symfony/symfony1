501c4a13af0ea9b36d2f7258a9c8a6e71b5906ae
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-06-20 09:33:15 +0000

    fixed harcoded i18n class name in object builder
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1478 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/addon/propel/builder/SfObjectBuilder.php
+++ lib/addon/propel/builder/SfObjectBuilder.php
@@ -11,9 +11,10 @@ class sfObjectBuilder extends PHP5ComplexObjectBuilder
     // include the i18n classes if needed
     if ($this->getTable()->getAttribute('isI18N'))
     {
+      $relatedTable = $this->getDatabase()->getTable($this->getTable()->getAttribute('i18nTable'));
       $script .= '
-require_once \''.$this->getFilePath($this->getStubObjectBuilder()->getPackage().'.'.$this->getStubObjectBuilder()->getClassName().'I18nPeer').'\';
-require_once \''.$this->getFilePath($this->getStubObjectBuilder()->getPackage().'.'.$this->getStubObjectBuilder()->getClassName().'I18n').'\';
+require_once \''.$this->getFilePath($this->getStubObjectBuilder()->getPackage().'.'.$relatedTable->getAttribute('phpName').'Peer').'\';
+require_once \''.$this->getFilePath($this->getStubObjectBuilder()->getPackage().'.'.$relatedTable->getAttribute('phpName')).'\';
 ';
     }
   }
