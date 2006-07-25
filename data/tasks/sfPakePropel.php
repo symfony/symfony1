0986ef8ac3770195c2bf58e88cd065f32d116077
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-07-25 16:06:57 +0000

    added load-data to the symfony command line (closes #150 - based on a patch from dwhittle)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1620 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/tasks/sfPakePropel.php
+++ data/tasks/sfPakePropel.php
@@ -18,6 +18,9 @@ pake_task('propel-convert-yml-schema', 'project_exists');
 pake_desc('create schema.yml from schema.xml');
 pake_task('propel-convert-xml-schema', 'project_exists');
 
+pake_desc('load data from fixtures directory');
+pake_task('propel-load-data', 'project_exists');
+
 pake_desc('create database for current model');
 pake_task('propel-build-db', 'project_exists');
 pake_task('build-db');
@@ -161,13 +164,64 @@ function run_propel_build_schema($task, $args)
     $schema = preg_replace('/<database\s+name="[^"]+"/s', '<database name="propel"', $schema);
     file_put_contents('config/schema.xml', $schema);
   }
+
   if (!isset($args[0]) || $args[0] != 'xml')
   {
     _propel_convert_xml_schema(false, '');
     $finder = pakeFinder::type('file')->name('schema.xml');
-    pake_remove($finder, 'config'); 
+    pake_remove($finder, 'config');
   }
-  
+}
+
+/**
+ * loads yml data from fixtures directory and inserts into database
+ *
+ * @example symfony load-data frontend
+ * @example symfony load-data frontend dev fixtures append
+ *
+ * @todo replace delete argument with flag -d
+ *
+ * @param object $task
+ * @param array $args
+ */
+function run_propel_load_data($task, $args)
+{
+  if (!count($args))
+  {
+    throw new Exception('You must provide the app.');
+  }
+
+  $app = $args[0];
+
+  if (!is_dir(sfConfig::get('sf_app_dir').DIRECTORY_SEPARATOR.$app))
+  {
+    throw new Exception('The app "'.$app.'" does not exist.');
+  }
+
+  $env = empty($args[1]) ? 'dev' : $args[1];
+
+  // define constants
+  define('SF_ROOT_DIR',    sfConfig::get('sf_root_dir'));
+  define('SF_APP',         $app);
+  define('SF_ENVIRONMENT', $env);
+  define('SF_DEBUG',       true);
+
+  // get configuration
+  require_once SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+
+  $fixtures_dir = empty($args[2]) ? 'fixtures' : $args[2];
+  $fixtures_dir = sfConfig::get('sf_data_dir').DIRECTORY_SEPARATOR.$fixtures_dir;
+  if (!is_readable($fixtures_dir))
+  {
+    throw new Exception(sprintf('Fixture directory "%s" does not exist.', $fixtures_dir));
+  }
+
+  $databaseManager = new sfDatabaseManager();
+  $databaseManager->initialize();
+
+  $data = new sfPropelData();
+  $data->setDeleteCurrentData(!empty($args[3]) && $args[3] == 'append' ? false : true);
+  $data->loadData($fixtures_dir);
 }
 
 function _call_phing($task, $task_name, $check_schema = true)
