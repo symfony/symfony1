8a25c27fd9889ebfbaa8a2bd32d8a4bb5696fa0c
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-08-21 08:53:07 +0000

    fixed submit_image_tag() should use an alternative-text (closes #806)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1678 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/FormHelper.php
+++ lib/helper/FormHelper.php
@@ -978,6 +978,14 @@ function reset_tag($value = 'Reset', $options = array())
  */
 function submit_image_tag($source, $options = array())
 {
+  if (!isset($options['alt']))
+  {
+    $path_pos = strrpos($source, '/');
+    $dot_pos = strrpos($source, '.');
+    $begin = $path_pos ? $path_pos + 1 : 0;
+    $nb_str = ($dot_pos ? $dot_pos : strlen($source)) - $begin;
+    $options['alt'] = ucfirst(substr($source, $begin, $nb_str));
+  }
   return tag('input', array_merge(array('type' => 'image', 'name' => 'commit', 'src' => image_path($source)), _convert_options_to_javascript(_convert_options($options))));
 }
 
