3f4471ffa18cd28d3e2061374eaabe1582f28a3d
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-08-24 13:03:56 +0000

    fixed sfDateFormat-&gt;format() should check properly for timestamp (closes #552)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1817 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/i18n/sfDateFormat.class.php
+++ lib/i18n/sfDateFormat.class.php
@@ -106,7 +106,7 @@ class sfDateFormat
    */
   public function format($time, $pattern='F', $charset='UTF-8')
   {
-    if(is_string($time))
+    if(!ctype_digit((string) $time))
       $time = @strtotime($time);
     
     if(is_null($pattern))
