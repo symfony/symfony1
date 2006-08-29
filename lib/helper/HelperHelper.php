ab85841f73a5ae4813030e0274a3d95fb9c949ac
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-08-29 14:01:19 +0000

    allow the use_helper() helper to accept more than one argument (closes #841)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1856 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/HelperHelper.php
+++ lib/helper/HelperHelper.php
@@ -17,37 +17,46 @@
  * @version    SVN: $Id$
  */
 
+/**
+ * <b>DEPRECATED:</b> use use_helper() instead with the same syntax. 
+ */ 
 function use_helpers()
 {
+  if (sfConfig::get('sf_logging_active')) sfContext::getInstance()->getLogger()->err('The function "use_helpers()" is deprecated. Please use "use_helper()"'); 
+
   foreach (func_get_args() as $helperName)
   {
     use_helper($helperName);
   }
 }
 
-function use_helper($helperName)
+function use_helper()
 {
   static $loaded = array();
-  if (isset($loaded[$helperName]))
-  {
-    return;
-  }
 
-  if (is_readable(sfConfig::get('sf_symfony_lib_dir').'/helper/'.$helperName.'Helper.php'))
+  foreach (func_get_args() as $helperName)
   {
-    // global helper
-    include_once(sfConfig::get('sf_symfony_lib_dir').'/helper/'.$helperName.'Helper.php');
-  }
-  else if (is_readable(sfConfig::get('sf_app_module_dir').'/'.sfContext::getInstance()->getModuleName().'/'.sfConfig::get('sf_app_module_lib_dir_name').'/helper/'.$helperName.'Helper.php'))
-  {
-    // current module helper
-    include_once(sfConfig::get('sf_app_module_dir').'/'.sfContext::getInstance()->getModuleName().'/'.sfConfig::get('sf_app_module_lib_dir_name').'/helper/'.$helperName.'Helper.php');
-  }
-  else
-  {
-    // helper in include_path
-    include_once('helper/'.$helperName.'Helper.php');
-  }
+    if (isset($loaded[$helperName]))
+    {
+      return;
+    }
 
-  $loaded[$helperName] = true;
+    if (is_readable(sfConfig::get('sf_symfony_lib_dir').'/helper/'.$helperName.'Helper.php'))
+    {
+      // global helper
+      include_once(sfConfig::get('sf_symfony_lib_dir').'/helper/'.$helperName.'Helper.php');
+    }
+    else if (is_readable(sfConfig::get('sf_app_module_dir').'/'.sfContext::getInstance()->getModuleName().'/'.sfConfig::get('sf_app_module_lib_dir_name').'/helper/'.$helperName.'Helper.php'))
+    {
+      // current module helper
+      include_once(sfConfig::get('sf_app_module_dir').'/'.sfContext::getInstance()->getModuleName().'/'.sfConfig::get('sf_app_module_lib_dir_name').'/helper/'.$helperName.'Helper.php');
+    }
+    else
+    {
+      // helper in include_path
+      include_once('helper/'.$helperName.'Helper.php');
+    }
+
+    $loaded[$helperName] = true;
+  }
 }
