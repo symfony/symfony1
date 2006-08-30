8cf1fff617abd9c2d7cfbf61789d8c59f601faef
Ian Christian <ian.christian@symfony-project.org>
2006-08-30 15:46:25 +0000

    pookey: fixing sight but in outputescaper stuff
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1884 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/view/escaper/sfOutputEscaperObjectDecorator.class.php
+++ lib/view/escaper/sfOutputEscaperObjectDecorator.class.php
@@ -46,8 +46,7 @@ class sfOutputEscaperObjectDecorator extends sfOutputEscaperGetterDecorator
     if (count($args) > 0)
     {
       $escapingMethod = $args[count($args) - 1];
-
-      if (substr($escapingMethod, 0, 4) === 'esc_')
+      if (is_string($escapingMethod) && substr($escapingMethod, 0, 4) === 'esc_')
       {
         array_pop($args);
       }
