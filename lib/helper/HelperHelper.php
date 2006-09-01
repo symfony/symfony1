10a8933fe773397a1ad236ac84ae23cfec8faca5
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-01 08:01:45 +0000

    added some files to ignore for eclipse users (closes #846)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@1915 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/HelperHelper.php
+++ lib/helper/HelperHelper.php
@@ -32,43 +32,5 @@ function use_helpers()
 
 function use_helper()
 {
-  static $loaded = array();
-
-  $dirs = array(
-    sfConfig::get('sf_app_module_dir').'/'.sfContext::getInstance()->getModuleName().'/'.sfConfig::get('sf_app_module_lib_dir_name').'/helper', // module dir
-    sfConfig::get('sf_app_lib_dir').'/helper',                                                                                                  // application dir
-    sfConfig::get('sf_plugin_lib_dir').'/symfony/helper',                                                                                       // plugin dir
-    sfConfig::get('sf_symfony_lib_dir').'/helper',                                                                                              // global dir
-  );
-
-  foreach (func_get_args() as $helperName)
-  {
-    if (isset($loaded[$helperName]))
-    {
-      continue;
-    }
-
-    $fileName = $helperName.'Helper.php';
-    foreach ($dirs as $dir)
-    {
-      $included = false;
-      if (is_readable($dir.'/'.$fileName))
-      {
-        include_once($dir.'/'.$fileName);
-        $included = true;
-        break;
-      }
-    }
-
-    if (!$included)
-    {
-      // search in the include path
-      if ((@include_once('helper/'.$fileName)) != 1)
-      {
-        throw new sfViewException(sprintf('Unable to load "%s" helper in: %s', $helperName, implode(', ', array_merge($dirs, explode(PATH_SEPARATOR, get_include_path())))));
-      }
-    }
-
-    $loaded[$helperName] = true;
-  }
+  sfLoader::loadHelpers(func_get_args(), sfContext::getInstance()->getModuleName());
 }
