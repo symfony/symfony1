5faae37733fce465cf443151a0726f576f9c2e3f
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-14 06:55:07 +0000

    updated test pake task and test skeleton for the new test framework
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2070 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/skeleton/module/test/actionsTest.php
+++ data/skeleton/module/test/actionsTest.php
@@ -1,26 +1,13 @@
 <?php
 
-class ##MODULE_NAME##ActionsWebBrowserTest extends UnitTestCase
-{
-  private
-    $browser = null;
+// create a new test browser
+$browser = new sfTestBrowser();
+$browser->initialize();
 
-  public function setUp ()
-  {
-    // create a new test browser
-    $this->browser = new sfTestBrowser();
-    $this->browser->initialize('hostname');
-  }
-
-  public function tearDown ()
-  {
-    $this->browser->shutdown();
-  }
-
-  public function test_simple()
-  {
-    $url = '/##MODULE_NAME##/index';
-    $html = $this->browser->get($url);
-    $this->assertWantedPattern('/##MODULE_NAME##/', $html);
-  }
-}
+$browser->
+  get('/##MODULE_NAME##/index')->
+  isStatusCode(200)->
+  isRequestParameter('module', '##MODULE_NAME##')->
+  isRequestParameter('action', 'index')->
+  checkResponseElement('body', '/##MODULE_NAME##/')
+;
--- data/tasks/sfPakeTest.php
+++ data/tasks/sfPakeTest.php
@@ -3,9 +3,44 @@
 pake_desc('launch project test suite');
 pake_task('test');
 
+pake_desc('launch a test');
+pake_task('onetest');
+
 pake_desc('launch test suite for a plugin');
 pake_task('plugin-test');
 
+function run_onetest($task, $args)
+{
+  if (!count($args))
+  {
+    throw new Exception('You must provide the app to test.');
+  }
+
+  $app = $args[0];
+
+  if (!is_dir(sfConfig::get('sf_app_dir').DIRECTORY_SEPARATOR.$app))
+  {
+    throw new Exception(sprintf('The app "%s" does not exist.', $app));
+  }
+
+  if (!isset($args[1]))
+  {
+    throw new Exception('You must provide a test file.');
+  }
+
+  $test_file = $args[1];
+
+  // define constants
+  define('SF_ROOT_DIR',    getcwd());
+  define('SF_APP',         $app);
+  define('SF_ENVIRONMENT', 'test');
+  define('SF_DEBUG',       true);
+
+  // get configuration
+  require_once SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+
+  include(sfConfig::get('sf_test_dir').DIRECTORY_SEPARATOR.$app.DIRECTORY_SEPARATOR.$test_file.'Test.php');
+}
 function run_test($task, $args)
 {
   if (!count($args))
@@ -29,14 +64,21 @@ function run_test($task, $args)
   // get configuration
   require_once SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
 
-  $dirs_to_test = array($app);
+  require_once(sfConfig::get('sf_symfony_lib_dir').'/vendor/lime/lime.php');
+
+  $h = new lime_harness(new lime_output_color());
+
+  $h->base_dir = sfConfig::get('sf_test_dir');
+
+  // unit tests
+  $finder = pakeFinder::type('file')->name('*Test.php');
   if (is_dir('test/project'))
   {
-    $dirs_to_test[] = 'project';
+    $h->register($finder->in($h->base_dir.'/project'));
   }
+  $h->register($finder->in($h->base_dir.'/'.$app));
 
-  pake_import('simpletest', false);
-  pakeSimpletestTask::call_simpletest($task, 'text', $dirs_to_test);
+  $h->run();
 }
 
 function run_plugin_test($task, $args)
