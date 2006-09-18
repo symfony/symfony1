dd1039d8cd43343775ea53c6e9824b549de440f2
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-18 16:06:02 +0000

    changed plugin directory in sandbox creation
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2118 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/bin/create_quickstart.sh
+++ data/bin/create_quickstart.sh
@@ -59,11 +59,11 @@ sed -i '' -e "s/^#//g" config/databases.yml
 
 # add some empty files in empty directories
 touch apps/${APP_NAME}/modules/.sf apps/${APP_NAME}/i18n/.sf test/${APP_NAME}/.sf doc/.sf web/images/.sf
-touch log/.sf cache/.sf batch/.sf data/sql/.sf data/model/.sf data/plugins/.sf
+touch log/.sf cache/.sf batch/.sf data/sql/.sf data/model/.sf
 touch data/symfony/generator/sfPropelAdmin/default/skeleton/templates/.sf
 touch data/symfony/generator/sfPropelAdmin/default/skeleton/validate/.sf
 touch data/symfony/modules/default/config/.sf data/symfony/skeleton/project/build/.sf
-touch lib/model/.sf lib/plugins/.sf web/js/.sf
+touch lib/model/.sf plugins/.sf web/js/.sf
 
 # create archive
 
