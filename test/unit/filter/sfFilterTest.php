9b4c83dd2f786515e689f4433c86c9ffcbb90ddc
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-21 14:49:32 +0000

    added more unit tests for sfFilter
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2148 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/filter/sfFilterTest.php
+++ test/unit/filter/sfFilterTest.php
@@ -15,15 +15,60 @@ require_once($_test_dir.'/../lib/config/sfConfig.class.php');
 require_once($_test_dir.'/../lib/util/sfParameterHolder.class.php');
 require_once($_test_dir.'/../lib/filter/sfFilter.class.php');
 
-$t = new lime_test(13, new lime_output_color());
+$t = new lime_test(23, new lime_output_color());
 
 class myFilter extends sfFilter
 {
+  public function isFirstCallBeforeExecution()
+  {
+    return parent::isFirstCallBeforeExecution();
+  }
+
+  public function isFirstCallBeforeRendering()
+  {
+    return parent::isFirstCallBeforeRendering();
+  }
+
+  public function isFirstCall($type = 'beforeExecution')
+  {
+    return parent::isFirstCall($type);
+  }
 }
 
 $context = new sfContext();
 $filter = new myFilter();
+
+// ->initialize()
+$t->diag('->initialize()');
+$filter = new myFilter();
+$t->is($filter->getContext(), null, '->initialize() takes a sfContext object as its first argument');
+$filter->initialize($context, array('foo' => 'bar'));
+$t->is($filter->getParameter('foo'), 'bar', '->initialize() takes an array of parameters as its second argument');
+
+// ->getContext()
+$t->diag('->getContext()');
 $filter->initialize($context);
+$t->is($filter->getContext(), $context, '->getContext() returns the current context');
+
+// ->isFirstCall()
+$t->diag('->isFirstCall()');
+$t->is($filter->isFirstCall('beforeExecution'), true, '->isFirstCall() returns true if this is the first call with this argument');
+$t->is($filter->isFirstCall('beforeExecution'), false, '->isFirstCall() returns false if this is not the first call with this argument');
+$t->is($filter->isFirstCall('beforeExecution'), false, '->isFirstCall() returns false if this is not the first call with this argument');
+
+// ->isFirstCallBeforeExecution()
+$filter1 = new myFilter();
+$filter2 = new myFilter();
+$t->diag('->isFirstCallBeforeExecution()');
+$t->is($filter1->isFirstCall('beforeExecution'), $filter2->isFirstCallBeforeExecution(), '->isFirstCallBeforeExecution() is an alias for ->isFirstCall() with "beforeExecution" as an argument');
+$t->is($filter1->isFirstCall('beforeExecution'), $filter2->isFirstCallBeforeExecution(), '->isFirstCallBeforeExecution() is an alias for ->isFirstCall() with "beforeExecution" as an argument');
+
+// ->isFirstCallBeforeRendering()
+$filter1 = new myFilter();
+$filter2 = new myFilter();
+$t->diag('->isFirstCallBeforeRendering()');
+$t->is($filter1->isFirstCall('beforeRendering'), $filter2->isFirstCallBeforeRendering(), '->isFirstCallBeforeRendering() is an alias for ->isFirstCall() with "beforeRendering" as an argument');
+$t->is($filter1->isFirstCall('beforeRendering'), $filter2->isFirstCallBeforeRendering(), '->isFirstCallBeforeRendering() is an alias for ->isFirstCall() with "beforeRendering" as an argument');
 
 // parameter holder proxy
 require_once($_test_dir.'/unit/sfParameterHolderTest.class.php');
