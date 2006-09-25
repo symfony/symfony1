ebccd7ff011d822bfa8295bdfee6bfdbdf3a9539
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-25 14:49:46 +0000

    added more unit tests for sfConfigHandler
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2175 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/config/sfConfigHandlerTest.php
+++ test/unit/config/sfConfigHandlerTest.php
@@ -13,7 +13,7 @@ require_once($_test_dir.'/../lib/vendor/lime/lime.php');
 require_once($_test_dir.'/unit/sfContextMock.class.php');
 require_once($_test_dir.'/unit/bootstrap.php');
 
-$t = new lime_test(3, new lime_output_color());
+$t = new lime_test(6, new lime_output_color());
 
 class myConfigHandler extends sfConfigHandler
 {
@@ -40,6 +40,24 @@ $t->is($value, 'my value with a bar constant', '::replaceConstantsCallback() rep
 $value = '%Y/%m/%d %H:%M';
 myConfigHandler::replaceConstantsCallback($value);
 $t->is($value, '%Y/%m/%d %H:%M', '::replaceConstantsCallback() does not replace unknown constants');
+
+// ::replaceConstants()
+$t->diag('::replaceConstants()');
+sfConfig::set('foo', 'bar');
+$value = 'my value with a %foo% constant';
+myConfigHandler::replaceConstantsCallback($value);
+$t->is($value, 'my value with a bar constant', '::replaceConstants() replaces constants enclosed in %');
+
+sfConfig::set('foo', 'bar');
+$value = array(
+  'foo' => 'my value with a %foo% constant',
+  'bar' => array(
+    'foo' => 'my value with a %foo% constant',
+  ),
+);
+$value = myConfigHandler::replaceConstants($value);
+$t->is($value['foo'], 'my value with a bar constant', '::replaceConstants() replaces constants in arrays recursively');
+$t->is($value['bar']['foo'], 'my value with a bar constant', '::replaceConstants() replaces constants in arrays recursively');
 
 // ->getParameterHolder()
 $t->diag('->getParameterHolder()');
