5bf961e466ca83027393fdacf06a2d0a898178d0
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-25 14:23:53 +0000

    added some unit tests for NumberHelper and sfRegexValidator
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2168 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/helper/NumberHelperTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$_test_dir = realpath(dirname(__FILE__).'/../..');
+require_once($_test_dir.'/../lib/vendor/lime/lime.php');
+require_once($_test_dir.'/unit/bootstrap.php');
+
+sfLoader::loadHelpers(array('Helper', 'Tag', 'Number'));
+
+$t = new lime_test(2, new lime_output_color());
+
+// format_number()
+$t->diag('format_number()');
+$t->is(format_number(10012.1, 'en'), '10,012.1', 'format_number() takes a number as its first argument');
+//$t->is(format_number(10012.1, 'fr'), '10.012,1', 'format_number() takes a culture as its second argument');
+
+$t->todo('format_number() takes the current user culture if no second argument is given');
+
+// format_currency()
