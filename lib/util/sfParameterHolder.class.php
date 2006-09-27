1ea6c364b6061d46f409b393dfb47103a0226d0a
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-27 23:38:59 +0000

    fixed coding standards
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2211 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/util/sfParameterHolder.class.php
+++ lib/util/sfParameterHolder.class.php
@@ -193,22 +193,25 @@ class sfParameterHolder
       $ns = $this->default_namespace;
     }
 
-    if (false !== ($offset = strpos($name, '['))) {
+    if (false !== ($offset = strpos($name, '[')))
+    {
       if (isset($this->parameters[$ns][substr($name, 0, $offset)]))
       {
         $array = $this->parameters[$ns][substr($name, 0, $offset)];
 
-        while ($pos = strpos($name, '[', $offset)) {
+        while ($pos = strpos($name, '[', $offset))
+        {
           $end = strpos($name, ']', $pos);
-          if ($end == $pos+1) {
+          if ($end == $pos + 1)
+          {
             // reached a []
             return true;
           }
-          elseif (!isset($array[substr($name, $pos+1, $end-$pos-1)]))
+          else if (!isset($array[substr($name, $pos + 1, $end - $pos - 1)]))
           {
             return false;
           }
-          $array = $array[substr($name, $pos+1, $end-$pos-1)];
+          $array = $array[substr($name, $pos + 1, $end - $pos - 1)];
           $offset = $end;
         }
 
