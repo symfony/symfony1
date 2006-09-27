9c3b9585b2af8c2bf75f1989112ef34c6cf0294f
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-27 06:19:54 +0000

    added unit tests for Spyc (hmmm, Spyc needs a lot of work to be YAML compliant)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2203 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/util/SpycTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$_test_dir = realpath(dirname(__FILE__).'/../..');
+require_once($_test_dir.'/../lib/vendor/lime/lime.php');
+require_once($_test_dir.'/../lib/util/Spyc.class.php');
+
+$t = new lime_test(40, new lime_output_color());
+
+$path = dirname(__FILE__).'/fixtures/Spyc';
+$files = Spyc::YAMLLoad($path.'/index.yml');
+foreach ($files as $file)
+{
+  $t->diag($file);
+
+  $yamls = file_get_contents($path.'/'.$file.'.yml');
+
+  // split YAMLs documents
+  foreach (preg_split('/^---( %YAML\:1\.0)?/m', $yamls) as $yaml)
+  {
+    if (!$yaml)
+    {
+      continue;
+    }
+
+    $test = Spyc::YAMLLoad($yaml);
+
+    if (isset($test['todo']) && $test['todo'])
+    {
+      $t->todo($test['test']);
+    }
+    else
+    {
+      $t->is(var_export(Spyc::YAMLLoad($test['yaml']), true), var_export(eval('return '.trim($test['php']).';'), true), $test['test']);
+    }
+  }
+}
