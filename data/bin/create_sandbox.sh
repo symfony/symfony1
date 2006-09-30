76c1ce1e8fa229708eaf09a745845af593010734
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-09-30 05:56:24 +0000

    simplified sandbox creation script (use freeze now)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2236 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/bin/create_sandbox.sh
+++ data/bin/create_sandbox.sh
@@ -1,75 +1,55 @@
 #!/bin/sh
 
-# create a quickstart package
+# creates a symfony sandbox for this symfony version
 
-# initialization
-
+echo ">>> initialization"
+DIR=../`dirname $0`
 SANDBOX_NAME=sf_sandbox
 APP_NAME=frontend
-SVN_PATH=trunk
+PHP=php
 
 echo ">>> project initialization"
+rm -rf ${SANDBOX_NAME}
 mkdir ${SANDBOX_NAME}
 cd ${SANDBOX_NAME}
-mkdir lib bin data web
-
-# symfony libraries
-
-echo ">>> freeze symfony libraries"
-svn export http://svn.symfony-project.com/${SVN_PATH}/lib/ lib/symfony/
-
-echo ">>> freeze symfony data"
-svn export http://svn.symfony-project.com/${SVN_PATH}/data/ data/symfony/
-mv data/symfony/web/sf web/sf
-
-# symfony command line
-
-echo ">>> add symfony command line"
-cp data/symfony/bin/symfony.sh symfony.sh
-cp data/symfony/bin/symfony.bat symfony.bat
-cp data/symfony/bin/symfony.php symfony.php
-chmod 755 symfony.sh
-
-# default project / app
 
 echo ">>> create a new project and a new app"
-./symfony.sh init-project ${SANDBOX_NAME}
-./symfony.sh init-app ${APP_NAME}
+php ${DIR}/symfony.php init-project ${SANDBOX_NAME}
+php ${DIR}/symfony.php init-app ${APP_NAME}
 
-# license
+echo ">>> add symfony command line"
+cp ${DIR}/symfony.sh symfony.sh
+cp ${DIR}/symfony.bat symfony.bat
+cp ${DIR}/symfony.php symfony.php
+chmod 755 symfony.sh
 
 echo ">>> add LICENSE"
-svn export http://svn.symfony-project.com/${SVN_PATH}/LICENSE LICENSE
-
-# readme
+cp ${DIR}/../../LICENSE LICENSE
 
 echo ">>> add README"
-svn export http://svn.symfony-project.com/${SVN_PATH}/doc/SANDBOX_README README
+cp ${DIR}/../../doc/SANDBOX_README README
 
-# default: sqlite db
+echo ">>> freeze symfony"
+php ${DIR}/symfony.php freeze
 
 echo ">>> default to sqlite"
 sed -i '' -e "s#\(propel.database *= *\)mysql#\1sqlite#" config/propel.ini
 sed -i '' -e "s#\(propel.database.createUrl *= *\).*#\1sqlite://./../../../../data/sandbox.db#" config/propel.ini
 sed -i '' -e "s#\(propel.database.url *= *\).*#\1sqlite://./../../../../data/sandbox.db#" config/propel.ini
-
 sed -i '' -e "s#\( *dsn *: *\).*#\1sqlite://./../data/sandbox.db#" config/databases.yml
-
 sed -i '' -e "s/^#//g" config/databases.yml
 
-# add some empty files in empty directories
+echo ">>> add some empty files in empty directories"
 touch apps/${APP_NAME}/modules/.sf apps/${APP_NAME}/i18n/.sf test/${APP_NAME}/.sf doc/.sf web/images/.sf
 touch log/.sf cache/.sf batch/.sf data/sql/.sf data/model/.sf
 touch data/symfony/generator/sfPropelAdmin/default/skeleton/templates/.sf
 touch data/symfony/generator/sfPropelAdmin/default/skeleton/validate/.sf
-touch data/symfony/modules/default/config/.sf data/symfony/skeleton/project/build/.sf
+touch data/symfony/modules/default/config/.sf
 touch lib/model/.sf plugins/.sf web/js/.sf
 
-# create archive
-
+echo ">>> create archive"
 cd ..
 tar zcpf ${SANDBOX_NAME}.tgz ${SANDBOX_NAME}
 
-# cleanup
-
+echo ">>> cleanup"
 rm -rf ${SANDBOX_NAME}
