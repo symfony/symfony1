67542bfb4a548c8a12b1a0b6747ccdd5e9fc6188
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-10-04 19:36:27 +0000

    added new sfOptimizer classes
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2316 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/tasks/sfPakeMisc.php
+++ data/tasks/sfPakeMisc.php
@@ -25,10 +25,44 @@ pake_desc('purges an applications log files');
 pake_task('purge-logs', 'project_exists');
 
 pake_desc('enables an application in a given environment');
-pake_task('enable', 'app_exists'); 
+pake_task('enable', 'app_exists');
 
 pake_desc('disables an application in a given environment');
-pake_task('disable', 'app_exists'); 
+pake_task('disable', 'app_exists');
+
+pake_desc('optimizes symfony cache files for production environment');
+pake_task('optimize', 'app_exists');
+
+function run_optimize($task, $args)
+{
+  if (count($args) < 2)
+  {
+    throw new Exception('You must provide an application and an environment.');
+  }
+
+  $app = $args[0];
+  $env = $args[1];
+
+  $config = sprintf('cache/%s/%s/config/config_core_compile.yml.php', $app, $env);
+  // simulate a request to populate config cache files
+  // and get the current configuration
+  define('SF_ROOT_DIR',    realpath('./'));
+  define('SF_APP',         $app);
+  define('SF_ENVIRONMENT', $env);
+  define('SF_DEBUG',       false);
+
+  require_once('apps'.DIRECTORY_SEPARATOR.$app.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+
+  if (!is_readable($config))
+  {
+    throw new Exception('Unable to optimze your config files');
+  }
+
+  $o = new sfOptimizer();
+  $o->initialize(file_get_contents($config));
+  $o->registerStandardOptimizers();
+  file_put_contents($config, $o->optimize());
+}
 
 /**
  * fixes permissions in a symfony project
