6b4ea9d734b1245cc8c6b51e31907ade47c7ffc2
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-11-27 10:41:40 +0000

    fixed wrong test number (closes #1136)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2832 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/util/sfBrowserTest.php
+++ test/unit/util/sfBrowserTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(38, new lime_output_color());
+$t = new lime_test(39, new lime_output_color());
 
 // ->click()
 $t->diag('->click()');
