8e52b0fcb0ae9678584f913ddf5f2ed8a81786a2
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-12-06 12:54:04 +0000

    added functional tests for production environment
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2934 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/functional/prodTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(dirname(__FILE__).'/../../lib/util/sfToolkit.class.php');
+sfToolkit::clearDirectory(dirname(__FILE__).'/fixtures/project/cache');
+
+$app = 'frontend';
+$debug = false;
+$ret = include(dirname(__FILE__).'/../bootstrap/functional.php');
+if (!$ret)
+{
+  return;
+}
+
+$b = new sfTestBrowser();
+$b->initialize();
+
+// default main page (without cache)
+$b->
+  get('/')->
+  isStatusCode(200)->
+  isRequestParameter('module', 'default')->
+  isRequestParameter('action', 'index')->
+  checkResponseElement('body', '/congratulations/i')
+;
+
+// default main page (with cache)
+$b->
+  get('/')->
+  isStatusCode(200)->
+  isRequestParameter('module', 'default')->
+  isRequestParameter('action', 'index')->
+  checkResponseElement('body', '/congratulations/i')
+;
