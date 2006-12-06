1af941a1a8d91f76892f28ff27fb63fe074b7eda
Fabien Potencier <fabien.potencier@symfony-project.org>
2006-12-06 14:12:25 +0000

    fixed xdebug backtrace in sfStopException (closes #1139 - patch from aferber)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@2940 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/exception/sfStopException.class.php
+++ lib/exception/sfStopException.class.php
@@ -27,6 +27,13 @@ class sfStopException extends sfException
   public function __construct ($message = null, $code = 0)
   {
     $this->setName('sfStopException');
+
+    // disable xdebug to avoid backtrace in error log
+    if (function_exists('xdebug_disable'))
+    {
+      xdebug_disable();
+    }
+
     parent::__construct($message, $code);
   }
 
