8c40d4058bfdae96bf64d6dec5f48b376c124858
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-01-08 08:52:06 +0000

    fixed typo (closes #925)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@3183 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/view/escaper/sfOutputEscaperObjectDecorator.class.php
+++ lib/view/escaper/sfOutputEscaperObjectDecorator.class.php
@@ -98,7 +98,7 @@ class sfOutputEscaperObjectDecorator extends sfOutputEscaperGetterDecorator
     }
     else
     {
-      throw new sfException(sprintf('Object of class "%s" cannot be converted to string (Please create a __toString() method)', get_class($name)));
+      throw new sfException(sprintf('Object of class "%s" cannot be converted to string (Please create a __toString() method)', get_class($this->value)));
     }
   }
 }
