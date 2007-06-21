f2060b350809d88e5245e57d1b7143fdeda96d4b
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-06-21 15:35:22 +0000

    fixed fixtures for sfTestBrowser
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@4302 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/functional/fixtures/project/apps/frontend/modules/browser/actions/actions.class.php
+++ test/functional/fixtures/project/apps/frontend/modules/browser/actions/actions.class.php
@@ -21,26 +21,3 @@ class browserActions extends sfActions
     return $this->renderText('text');
   }
 }
-<?php
-
-/**
- * browser actions.
- *
- * @package    project
- * @subpackage browser
- * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
- * @version    SVN: $Id$
- */
-class browserActions extends sfActions
-{
-  public function executeIndex()
-  {
-    return $this->renderText('<html><body><h1>html</h1></body></html>');
-  }
-
-  public function executeText()
-  {
-    $this->getResponse()->setContentType('text/plain');
-    return $this->renderText('text');
-  }
-}
