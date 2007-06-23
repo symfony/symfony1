89913e4f725713663f26458d722d04bf87a227a3
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-06-23 05:40:04 +0000

    added unit tests for new set notation of sfChoiceFomat
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@4336 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/i18n/sfChoiceFormatTest.php
+++ test/unit/i18n/sfChoiceFormatTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(24, new lime_output_color());
+$t = new lime_test(89, new lime_output_color());
 
 $n = new sfChoiceFormat();
 
@@ -96,3 +96,65 @@ $t->is($n->format($strings[0][0], 4), false, '->format() returns the string that
 $t->is($n->format($strings[4][0], 0), $strings[4][1][1][0], '->format() returns the string that match the number');
 $t->is($n->format($strings[4][0], 1), $strings[4][1][1][1], '->format() returns the string that match the number');
 $t->is($n->format($strings[4][0], 12), $strings[4][1][1][2], '->format() returns the string that match the number');
+
+// test set notation
+// tests adapted from Prado unit test suite
+$t->diag('set notation');
+$string = '{n: n%2 == 0} are even numbers |{n: n >= 5} are not even and greater than or equal to 5';
+$t->is($n->format($string, 0), 'are even numbers', '->format() can takes a set notation in the format string');
+$t->is($n->format($string, 2), 'are even numbers', '->format() can takes a set notation in the format string');
+$t->is($n->format($string, 4), 'are even numbers', '->format() can takes a set notation in the format string');
+$t->is(!$n->format($string, 1), 'are even numbers', '->format() can takes a set notation in the format string');
+$t->is($n->format($string, 5), 'are not even and greater than or equal to 5', '->format() can takes a set notation in the format string');
+
+$t->diag('set notation for polish');
+$string = '[1] plik |{2,3,4} pliki |[5,21] pliko\'w |{n: n % 10 > 1 && n %10 < 5} pliki |{n: n%10 >= 5 || n%10 <=1} pliko\'w';
+$wants = array(
+  'plik' => array(1),
+  'pliki' => array(2, 3, 4, 22, 23, 24),
+  'pliko\'w' => array(5, 6, 7, 11, 12, 15, 17, 20, 21, 25, 26, 30),
+);
+foreach ($wants as $want => $numbers)
+{
+  foreach ($numbers as $number)
+  {
+    $t->is($n->format($string, $number), $want, '->format() can deal with polish!');
+  }
+}
+
+$t->diag('set notation for russian');
+$string = '
+{n: n % 10 == 1 && n % 100 != 11} test1
+|{n: n % 10 >= 2 && n % 10 <= 4 && ( n % 100 < 10 || n % 100 >= 20 )} test2
+|{n: 2} test3';
+
+$wants = array(
+  'test1' => array(1, 21, 31, 41),
+  'test2' => array(2, 4, 22, 24, 32, 34),
+  'test3' => array(0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 20, 25, 26, 30),
+);
+foreach ($wants as $want => $numbers)
+{
+  foreach ($numbers as $number)
+  {
+    $t->is($n->format($string, $number), $want, '->format() can deal with russian!');
+  }
+}
+
+$t->diag('set notation for english');
+$string = '[0] none |{n: n % 10 == 1} 1st |{n: n % 10 == 2} 2nd |{n: n % 10 == 3} 3rd |{n:n} th';
+
+$wants = array(
+  'none' => array(0),
+  '1st' => array(1, 11, 21),
+  '2nd' => array(2, 12, 22),
+  '3rd' => array(3, 13, 23),
+  'th' => array(4, 5, 6, 7, 14, 15),
+);
+foreach ($wants as $want => $numbers)
+{
+  foreach ($numbers as $number)
+  {
+    $t->is($n->format($string, $number), $want, '->format() can deal with english!');
+  }
+}
