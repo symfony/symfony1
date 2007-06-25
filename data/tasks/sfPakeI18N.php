8480aafbe98e2c28ce14ca1ad528ee2c2acdf21c
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-06-25 18:40:01 +0000

    added a very simple i18n-find task to find strings that are not i18n ready in your templates
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@4395 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/tasks/sfPakeI18N.php
+++ data/tasks/sfPakeI18N.php
@@ -17,6 +17,87 @@
 pake_desc('extract i18n strings from php files');
 pake_task('i18n-extract');
 
+pake_desc('find non "i18n ready" strings in an application');
+pake_task('i18n-find');
+
+function run_i18n_find($task, $args)
+{
+  if (!count($args))
+  {
+    throw new Exception('You must provide the application.');
+  }
+
+  $app = $args[0];
+
+  if (!is_dir(sfConfig::get('sf_app_dir').DIRECTORY_SEPARATOR.$app))
+  {
+    throw new Exception(sprintf('The app "%s" does not exist.', $app));
+  }
+
+  pake_echo_action('i18n', sprintf('find non "i18n ready" strings in the "%s" application', $app));
+
+  sfConfig::set('sf_app', $app);
+  sfConfig::set('sf_environment', 'dev');
+  include(sfConfig::get('sf_symfony_data_dir').'/config/constants.php');
+
+  // Look in templates
+  $moduleNames = sfFinder::type('dir')->maxdepth(0)->ignore_version_control()->relative()->in(sfConfig::get('sf_app_dir').'/modules');
+  $strings = array();
+  foreach ($moduleNames as $moduleName)
+  {
+    $dir = sfConfig::get('sf_app_dir').'/modules/'.$moduleName.'/templates';
+    $templates = pakeFinder::type('file')->name('*.php')->relative()->in($dir);
+    foreach ($templates as $template)
+    {
+      $dom = new DomDocument('1.0', sfConfig::get('sf_charset', 'UTF-8'));
+      @$dom->loadXML('<doc>'.file_get_contents($dir.'/'.$template).'</doc>');
+
+      $nodes = array($dom);
+      while ($nodes)
+      {
+        $node = array_shift($nodes);
+
+        if (XML_TEXT_NODE === $node->nodeType)
+        {
+          if (!$node->isWhitespaceInElementContent())
+          {
+            if (!isset($strings[$moduleName][$template]))
+            {
+              if (!isset($strings[$moduleName]))
+              {
+                $strings[$moduleName] = array();
+              }
+
+              $strings[$moduleName][$template] = array();
+            }
+
+            $strings[$moduleName][$template][] = $node->nodeValue;
+          }
+        }
+        else if ($node->childNodes)
+        {
+          for ($i = 0, $max = $node->childNodes->length; $i < $max; $i++)
+          {
+            $nodes[] = $node->childNodes->item($i);
+          }
+        }
+      }
+    }
+  }
+
+  foreach ($strings as $moduleName => $templateStrings)
+  {
+    foreach ($templateStrings as $template => $messages)
+    {
+      pake_echo_action('i18n', sprintf('strings in "%s:%s"', $moduleName, $template));
+      foreach ($messages as $message)
+      {
+        echo "  $message\n";
+      }
+    }
+  }
+}
+
 function run_i18n_extract($task, $args, $options)
 {
   if (!count($args))
