7c0b8b262847ae32a996031bc599e900215cb18b
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-10-23 07:07:14 +0000

    fixed sfValidatorBoolean
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@5634 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/validator/sfValidatorBooleanTest.php
+++ test/unit/validator/sfValidatorBooleanTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(16, new lime_output_color());
+$t = new lime_test(17, new lime_output_color());
 
 $v = new sfValidatorBoolean();
 
@@ -20,13 +20,6 @@ $t->is($v->getErrorCodes(), array('required', 'invalid'), '->getErrorCodes() ret
 
 // ->clean()
 $t->diag('->clean()');
-
-// empty
-$t->diag('empty');
-$v->setOption('required', false);
-$t->ok($v->clean(null) === null, '->clean() returns null if no value is given');
-$v->setOption('empty_value', true);
-$t->ok($v->clean(null) === true, '->clean() returns the value of the empty_value option if no value is given');
 
 // true values
 $t->diag('true values');
@@ -42,6 +35,9 @@ foreach ($v->getOption('false_values') as $false_value)
   $t->is($v->clean($false_value), false, '->clean() returns false if the value is in the false_values option');
 }
 
+// required is false by default
+$t->is($v->clean(null), false, '->clean() returns false if the value is null');
+
 try
 {
   $v->clean('astring');
@@ -51,3 +47,10 @@ catch (sfValidatorError $e)
 {
   $t->pass('->clean() throws an error if the input value is not a true or a false value');
 }
+
+// empty
+$t->diag('empty');
+$v->setOption('required', false);
+$t->ok($v->clean(null) === null, '->clean() returns null if no value is given');
+$v->setOption('empty_value', true);
+$t->ok($v->clean(null) === true, '->clean() returns the value of the empty_value option if no value is given');
