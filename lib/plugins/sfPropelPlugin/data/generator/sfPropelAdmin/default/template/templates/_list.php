e52123a2740a9f01e3967b804a693d419c4a5ad2
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-10-26 09:45:51 +0000

    fixed admin generator template incorrect xhtml tag placement - <tfoot> (closes #2263)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@5702 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/data/generator/sfPropelAdmin/default/template/templates/_list.php
+++ lib/plugins/sfPropelPlugin/data/generator/sfPropelAdmin/default/template/templates/_list.php
@@ -13,15 +13,6 @@
 <?php endif; ?>
 </tr>
 </thead>
-<tbody>
-[?php $i = 1; foreach ($pager->getResults() as $<?php echo $this->getSingularName() ?>): $odd = fmod(++$i, 2) ?]
-<tr class="sf_admin_row_[?php echo $odd ?]">
-[?php include_partial('list_td_batch_actions', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>)) ?]
-[?php include_partial('list_td_<?php echo $this->getParameterValue('list.layout', 'tabular') ?>', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>)) ?]
-[?php include_partial('list_td_actions', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>)) ?]
-</tr>
-[?php endforeach; ?]
-</tbody>
 <tfoot>
 <tr><th colspan="<?php echo count($this->getColumns('list.display')) + ($this->getParameterValue('list.object_actions') ? 1 : 0) + ($this->getParameterValue('list.batch_actions') ? 1 : 0) ?>">
 <div class="float-right">
@@ -40,4 +31,13 @@
 [?php echo format_number_choice('[0] no result|[1] 1 result|(1,+Inf] %1% results', array('%1%' => $pager->getNbResults()), $pager->getNbResults()) ?]
 </th></tr>
 </tfoot>
+<tbody>
+[?php $i = 1; foreach ($pager->getResults() as $<?php echo $this->getSingularName() ?>): $odd = fmod(++$i, 2) ?]
+<tr class="sf_admin_row_[?php echo $odd ?]">
+[?php include_partial('list_td_batch_actions', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>)) ?]
+[?php include_partial('list_td_<?php echo $this->getParameterValue('list.layout', 'tabular') ?>', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>)) ?]
+[?php include_partial('list_td_actions', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>)) ?]
+</tr>
+[?php endforeach; ?]
+</tbody>
 </table>
