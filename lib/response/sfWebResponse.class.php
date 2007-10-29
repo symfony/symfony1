578670f420bd67e67b936e637f9f62584a8e00f8
Dustin Whittle <dustin.whittle@symfony-project.org>
2007-10-29 06:16:43 +0000

    fixed typo in sfWebResponse
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@5735 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/response/sfWebResponse.class.php
+++ lib/response/sfWebResponse.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -12,7 +12,7 @@
  * sfWebResponse class.
  *
  * This class manages web reponses. It supports cookies and headers management.
- * 
+ *
  * @package    symfony
  * @subpackage response
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
@@ -587,7 +587,7 @@ class sfWebResponse extends sfResponse
       return $this->javascripts;
     }
 
-    return isset($this->javascript[$position]) ? $this->javascript[$position] : array();
+    return isset($this->javascripts[$position]) ? $this->javascripts[$position] : array();
   }
 
   /**
@@ -599,12 +599,12 @@ class sfWebResponse extends sfResponse
    */
   public function addJavascript($js, $position = '', $options = array())
   {
-    if (!isset($this->javascript[$position]))
+    if (!isset($this->javascripts[$position]))
     {
-      $this->javascript[$position] = array();
+      $this->javascripts[$position] = array();
     }
 
-    $this->javascript[$position][$js] = $options;
+    $this->javascripts[$position][$js] = $options;
   }
 
   /**
