93d8f03085eedbb0b37dd3682d39c6ba7dee97f0
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-11-04 16:09:49 +0000

    fixed sfZendFrameworkBridge AutoLoad Not Working (closes #2314)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@5852 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfCompat10Plugin/lib/bridge/sfZendFrameworkBridge.class.php
+++ lib/plugins/sfCompat10Plugin/lib/bridge/sfZendFrameworkBridge.class.php
@@ -25,11 +25,14 @@ sfZendFrameworkBridge::requireZendLoader();
  */
 class sfZendFrameworkBridge
 {
+  static protected
+    $withLoader = true;
+
   public static function autoload($class)
   {
     try
     {
-      if (class_exists('Zend_Version'))
+      if (self::$withLoader)
       {
         Zend_Loader::loadClass($class);
       }
@@ -50,15 +53,15 @@ class sfZendFrameworkBridge
    * Detect and return the path to current Zend loader class.
    *
    * Starting from ZF 0.9.0 autoloading function has been moved
-   * from Zend.php to Zend/Version.php class.
+   * from Zend.php to Zend/Loader.php class.
    * Starting from ZF 1.0.0 Zend.php class no longer exists.
    *
-   * This function tries to detect whether Zend_Version exists
+   * This function tries to detect whether Zend_Loader exists
    * and returns its path if yes.
    * If the first step fails, the class will try to find Zend.php library
    * available in ZF <= 0.9.0 and returns its path if its exists.
-   * 
-   * If neither Zend/Version.php nor Zend.php exists,
+   *
+   * If neither Zend/Loader.php nor Zend.php exists,
    * then this function will raise a sfConfigurationException exception.
    *
    * @return  string  Path to default Zend Loader class
@@ -71,22 +74,24 @@ class sfZendFrameworkBridge
     // get base path according to sf setting
     $base = sfConfig::get('sf_zend_lib_dir') ? sfConfig::get('sf_zend_lib_dir').'/' : '';
 
-    // first check whether Zend/Version.php exists
-    // Zend/Version.php is available starting from ZF 0.9.0
+    // first check whether Zend/Loader.php exists
+    // Zend/Loader.php is available starting from ZF 0.9.0
     // Before ZF 0.9.0 you should call Zend.php
     // Plese note that Zend.php is still available in ZF 0.9.0
     // but it should not be called because deprecated
-    if (file_exists($base.'Zend/Version.php'))
+    if (file_exists($base.'Zend/Loader.php'))
     {
-      require_once($base.'Zend/Version.php');
+      require_once($base.'Zend/Loader.php');
+      self::$withLoader = true;
     }
     else if (file_exists($base.'Zend.php'))
     {
       require_once($base.'Zend.php');
+      self::$withLoader = false;
     }
     else
     {
-      throw new sfConfigurationException('Invalid Zend Framework library structure, unable to find Zend/Version.php (ZF >= 0.9.0) or Zend.php (ZF < 0.9.0) library');
+      throw new sfConfigurationException('Invalid Zend Framework library structure, unable to find Zend/Loader.php (ZF >= 0.9.0) or Zend.php (ZF < 0.9.0) library');
     }
   }
 }
