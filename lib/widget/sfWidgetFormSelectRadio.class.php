8c23b611f1c23bca1e88b85d0605758bc0df22bd
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-11-10 20:29:22 +0000

    added sfWidgetFormSelectRadio
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@5963 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/widget/sfWidgetFormSelectRadio.class.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfWidgetFormSelectRadio represents radio HTML tags.
+ *
+ * @package    symfony
+ * @subpackage widget
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+class sfWidgetFormSelectRadio extends sfWidgetForm
+{
+  /**
+   * Constructor.
+   *
+   * Available options:
+   *
+   *  * choices:         An array of possible choices (required)
+   *  * label_separator: The separator to use between the input radio and the label
+   *  * separator:       The separator to use between each input radio
+   *  * formatter:       A callable to call to format the radio choices
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array())
+  {
+    $this->addRequiredOption('choices');
+
+    $this->addOption('label_separator', '&nbsp;');
+    $this->addOption('separator', "\n");
+    $this->addOption('formatter', array($this, 'formatter'));
+  }
+
+  /**
+   * @see sfWidgetForm
+   */
+  public function render($name, $value = null, $attributes = array(), $errors = array())
+  {
+    $choices = $this->getOption('choices');
+    if ($choices instanceof sfCallable)
+    {
+      $choices = $choices->call();
+    }
+
+    $inputs = array();
+    foreach ($choices as $key => $option)
+    {
+      $attributes = array(
+        'name'  => $name,
+        'type'  => 'radio',
+        'value' => self::escapeOnce($key),
+        'id'    => $id = $this->generateId($name.'[]', self::escapeOnce($key)),
+      );
+
+      if (strval($key) == strval($value))
+      {
+        $attributes['checked'] = 'checked';
+      }
+
+      $inputs[] = array(
+        'input' => $this->renderTag('input', $attributes),
+        'label' => $this->renderContentTag('label', $option, array('for' => $id)),
+      );
+    }
+
+    return call_user_func($this->getOption('formatter'), $inputs);
+  }
+
+  public function formatter($inputs)
+  {
+    $rows = array();
+    foreach ($inputs as $input)
+    {
+      $rows[] = $this->renderContentTag('li', $input['input'].$this->getOption('label_separator').$input['label']);
+    }
+
+    return $this->renderContentTag('ul', implode($this->getOption('separator'), $rows), array('class' => 'radio_list'));
+  }
+}
---
+++ test/unit/widget/sfWidgetFormSelectRadioTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
+
+$t = new lime_test(2, new lime_output_color());
+
+$dom = new DomDocument('1.0', 'utf-8');
+$dom->validateOnParse = true;
+
+// ->render()
+$t->diag('->render()');
+$w = new sfWidgetFormSelectRadio(array('choices' => array('foo' => 'bar', 'foobar' => 'foo')));
+$output = '<ul class="radio_list"><li><input name="foo" type="radio" value="foo" id="foo_foo" />&nbsp;<label for="foo_foo">bar</label></li>
+<li><input name="foo" type="radio" value="foobar" id="foo_foobar" checked="checked" />&nbsp;<label for="foo_foobar">foo</label></li></ul>';
+$t->is($w->render('foo', 'foobar'), $output, '->render() renders a select tag with the value selected');
+
+// choices as a callable
+$t->diag('choices as a callable');
+
+function choice_callable()
+{
+  return array(1, 2, 3);
+}
+$w = new sfWidgetFormSelectRadio(array('choices' => new sfCallable('choice_callable')));
+$dom->loadHTML($w->render('foo'));
+$css = new sfDomCssSelector($dom);
+$t->is(count($css->matchAll('input[type="radio"]')->getNodes()), 3, '->render() accepts a sfCallable as a choices option');
