be8edd6e41a25ca22e8b99d0432cdad9e3112b59
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-11-14 11:33:17 +0000

    added sfValidatorCSRFToken
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@6022 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/validator/sfValidatorCSRFTokenTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
+
+$t = new lime_test(4, new lime_output_color());
+
+// __construct()
+$t->diag('__construct()');
+try
+{
+  new sfValidatorCSRFToken();
+  $t->fail('__construct() throws an sfException if you don\'t pass a token option');
+}
+catch (sfException $e)
+{
+  $t->pass('__construct() throws an sfException if you don\'t pass a token option');
+}
+
+$v = new sfValidatorCSRFToken(array('token' => 'symfony'));
+
+// ->clean()
+$t->diag('->clean()');
+$t->is($v->clean('symfony'), 'symfony', '->clean() checks that the token is valid');
+
+try
+{
+  $v->clean('another');
+  $t->fail('->clean() throws an sfValidatorError if the token is not valid');
+}
+catch (sfValidatorError $e)
+{
+  $t->pass('->clean() throws an sfValidatorError if the token is not valid');
+}
+
+// ->asString()
+$t->diag('->asString()');
+$t->is($v->asString(), 'CSRFToken({ token: symfony })', '->asString() returns a string representation of the validator');
