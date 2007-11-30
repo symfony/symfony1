807f7001146dd2a7c6f5900338579594d7528040
Dustin Whittle <dustin.whittle@symfony-project.org>
2007-11-30 00:17:28 +0000

    fixed e_strict warning in php5.2 in functional bootstrap
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@6222 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/skeleton/project/test/bootstrap/functional.php
+++ data/skeleton/project/test/bootstrap/functional.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -13,7 +13,9 @@ if (!isset($app))
 {
   $traces = debug_backtrace();
   $caller = $traces[0];
-  $app = array_pop(explode(DIRECTORY_SEPARATOR, dirname($caller['file'])));
+
+  $dirPieces = explode(DIRECTORY_SEPARATOR, dirname($caller['file']));
+  $app = array_pop($dirPieces);
 }
 
 // define symfony constant
