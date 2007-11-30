fa5ebe4d535cea88aafc7cc6a8557fa3b110d8e5
Dustin Whittle <dustin.whittle@symfony-project.org>
2007-11-30 00:17:02 +0000

    dwhittle: added components.class.php to skeleton module
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@6221 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ data/skeleton/module/module/actions/components.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * ##MODULE_NAME## components.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage ##MODULE_NAME##
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: components.class.php 2823 2006-11-26 08:26:18Z dwhittle $
+ */
+class ##MODULE_NAME##Components extends sfComponents
+{
+
+}
