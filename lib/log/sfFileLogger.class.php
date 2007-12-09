8839670ee63e146f4c705b7031b4dd425870ad29
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-12-09 18:44:53 +0000

    added 2 options for sfFileLogger (closes #2145)
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@6397 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/log/sfFileLogger.class.php
+++ lib/log/sfFileLogger.class.php
@@ -26,8 +26,10 @@ class sfFileLogger extends sfLogger
    *
    * Available options:
    *
-   * - file: The file path or a php wrapper to log messages
-   *         You can use any support php wrapper. To write logs to the Apache error log, use php://stderr
+   * - file:      The file path or a php wrapper to log messages
+   *              You can use any support php wrapper. To write logs to the Apache error log, use php://stderr
+   * - dir_mode:  The mode to use when creating a directory (default to 0777)
+   * - file_mode: The mode to use when creating a file (default to 0666)
    *
    * @param  sfEventDispatcher A sfEventDispatcher instance
    * @param  array        An array of options.
@@ -44,15 +46,19 @@ class sfFileLogger extends sfLogger
     $dir = dirname($options['file']);
     if (!is_dir($dir))
     {
-      mkdir($dir, 0777, true);
+      mkdir($dir, isset($options['dir_mode']) ? $options['dir_mode'] : 0777, true);
     }
 
-    if (!is_writable($dir) || (file_exists($options['file']) && !is_writable($options['file'])))
+    if (!is_writable($dir) || ($fileExists = file_exists($options['file']) && !is_writable($options['file'])))
     {
       throw new sfFileException(sprintf('Unable to open the log file "%s" for writing.', $options['file']));
     }
 
     $this->fp = fopen($options['file'], 'a');
+    if (!$fileExists)
+    {
+      chmod($options['file'], isset($options['file_mode']) ? $options['file_mode'] : 0666);
+    }
 
     return parent::initialize($dispatcher, $options);
   }
