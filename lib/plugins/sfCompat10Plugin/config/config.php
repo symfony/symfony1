5e518b99307f77ae94ccd16ef86ca835e280de5a
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-12-14 10:50:41 +0000

    wrapped sfCompat10Plugin config.php ti be sure it is executed only once per request
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@6504 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfCompat10Plugin/config/config.php
+++ lib/plugins/sfCompat10Plugin/config/config.php
@@ -1,19 +1,22 @@
 <?php
 
-class sfCompatAutoload extends sfSimpleAutoload
+if (!class_exists('sfCompatAutoload', false))
 {
-}
+  class sfCompatAutoload extends sfSimpleAutoload
+  {
+  }
 
-if (sfConfig::get('sf_compat_10'))
-{
-  // autoload classes
-  $autoload = sfCompatAutoload::getInstance(sfConfig::get('sf_app_cache_dir').'/sf_compat_autoloader.txt');
-  $autoload->addDirectory(dirname(__FILE__).'/../lib');
-  $autoload->register();
+  if (sfConfig::get('sf_compat_10'))
+  {
+    // autoload classes
+    $autoload = sfCompatAutoload::getInstance(sfConfig::get('sf_app_cache_dir').'/sf_compat_autoloader.txt');
+    $autoload->addDirectory(dirname(__FILE__).'/../lib');
+    $autoload->register();
 
-  // register config handler for validate/*.yml files
-  sfConfigCache::getInstance()->registerConfigHandler('modules/*/validate/*.yml', 'sfValidatorConfigHandler');
+    // register config handler for validate/*.yml files
+    sfConfigCache::getInstance()->registerConfigHandler('modules/*/validate/*.yml', 'sfValidatorConfigHandler');
 
-  // register the validation execution filter
-  sfConfig::set('sf_execution_filter', array('sfValidationExecutionFilter', array()));
+    // register the validation execution filter
+    sfConfig::set('sf_execution_filter', array('sfValidationExecutionFilter', array()));
+  }
 }
