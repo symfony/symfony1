1f55872edaa10d0ec76f8e0df25fdba0415f4ad3
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-12-15 15:15:33 +0000

    updated sfCompat10Plugin fixtures
    git-svn-id: file:///var/www/production/sfweb/svn/trunk@6508 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfCompat10Plugin/test/functional/fixtures/apps/frontend/config/config.php
+++ lib/plugins/sfCompat10Plugin/test/functional/fixtures/apps/frontend/config/config.php
@@ -6,3 +6,10 @@ include(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php
 // symfony bootstraping
 require_once($sf_symfony_lib_dir.'/util/sfCore.class.php');
 sfCore::bootstrap($sf_symfony_lib_dir, $sf_symfony_data_dir);
+
+// insert your own autoloading callables here
+
+if (sfConfig::get('sf_debug'))
+{
+  spl_autoload_register(array(sfAutoload::getInstance(), 'autoloadAgain'));
+}
