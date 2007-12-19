31914f502d3461b9fdea9489115e2089932473dd
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-12-19 20:16:14 +0000

    fixed web debug toolbar asset paths (closes #2676)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@6592 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/log/sfWebDebugLogger.class.php
+++ lib/log/sfWebDebugLogger.class.php
@@ -89,11 +89,12 @@ class sfWebDebugLogger extends sfLogger
     }
 
     // add needed assets for the web debug toolbar
+    $root = $this->context->getRequest()->getRelativeUrlRoot(); 
     $assets = sprintf('
       <script type="text/javascript" src="%s"></script>
       <link rel="stylesheet" type="text/css" media="screen" href="%s" />',
-      sfConfig::get('sf_web_debug_web_dir').'/js/main.js',
-      sfConfig::get('sf_web_debug_web_dir').'/css/main.css'
+      $root.sfConfig::get('sf_web_debug_web_dir').'/js/main.js',
+      $root.sfConfig::get('sf_web_debug_web_dir').'/css/main.css'
     );
     $content = str_ireplace('</head>', $assets.'</head>', $content);
 
