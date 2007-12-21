af246f64258c4b6fb140ac7d96685add964e46b1
Dustin Whittle <dustin.whittle@symfony-project.org>
2007-12-21 21:20:42 +0000

    1.1: fixed test plan for sfFormTest
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@6674 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/form/sfFormTest.php
+++ test/unit/form/sfFormTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(60, new lime_output_color());
+$t = new lime_test(61, new lime_output_color());
 
 class FormTest extends sfForm
 {
