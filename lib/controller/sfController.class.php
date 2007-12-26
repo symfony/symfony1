70dc79a9fb9149d1daec70a0df61fe085918ab47
Fabien Potencier <fabien.potencier@symfony-project.org>
2007-12-26 21:55:23 +0000

    fixed typo (closes #2600)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@6726 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/controller/sfController.class.php
+++ lib/controller/sfController.class.php
@@ -410,7 +410,7 @@ abstract class sfController
   {
     if (sfConfig::get('sf_logging_enabled'))
     {
-      $this->dispatcher->notify(new sfEvent($this, 'application.log', array('sendEmail method is deprecated', 'priority' => 'err')));
+      $this->dispatcher->notify(new sfEvent($this, 'application.log', array('sendEmail method is deprecated', 'priority' => sfLogger::ERR)));
     }
 
     return $this->getPresentationFor($module, $action, 'sfMail');
