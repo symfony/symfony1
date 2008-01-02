6de504bb15ce66088ddcc9817db52167f1252d9e
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-01-02 06:41:37 +0000

    fixed functional test skeleton file
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@6880 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/skeleton/module/test/actionsTest.php
+++ data/skeleton/module/test/actionsTest.php
@@ -4,7 +4,6 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
 // create a new test browser
 $browser = new sfTestBrowser();
-$browser->initialize();
 
 $browser->
   get('/##MODULE_NAME##/index')->
