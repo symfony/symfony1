6e2c5a0e4d20f486ce4d5c2798fb884765d6d0a1
romanb <romanb@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-01-09 17:07:18 +0000

    Made the query parser puke when encountering a duplicate class alias
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/beta2/lib@3476 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Query.php
+++ Doctrine/Query.php
@@ -1487,7 +1487,7 @@ class Doctrine_Query extends Doctrine_Query_Abstract implements Countable, Seria
 
             // if the current alias already exists, skip it
             if (isset($this->_queryComponents[$componentAlias])) {
-                continue;
+                throw new Doctrine_Query_Exception("Duplicate alias '$componentAlias' in query.");
             }
 
             if ( ! isset($table)) {
