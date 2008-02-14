f6786ec21206b1160695a5a20a11da2ef75314fe
Dustin Whittle <dustin.whittle@symfony-project.org>
2008-02-14 21:36:17 +0000

    1.1: fixed failed test from r7480
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@7489 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/test/functional/browseTest.php
+++ lib/plugins/sfPropelPlugin/test/functional/browseTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -23,7 +23,7 @@ $b->
   isStatusCode(200)->
   isRequestParameter('module', 'error')->
   isRequestParameter('action', 'index')->
-  throwsException('sfInitializationException', '/Unable to scaffold unexistant model/')
+  throwsException('sfInitializationException', '/Unable to scaffold nonexistent model/')
 ;
 
 // list page
