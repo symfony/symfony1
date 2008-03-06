c045df6e27a03e9084671d44306b788e1b9f4ae0
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-03-06 06:10:46 +0000

    fixes #830
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/0.10/lib@3935 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Task/GenerateModelsYaml.php
+++ Doctrine/Task/GenerateModelsYaml.php
@@ -35,11 +35,11 @@ class Doctrine_Task_GenerateModelsYaml extends Doctrine_Task
     public $description          =   'Generates your Doctrine_Record definitions from a Yaml schema file',
            $requiredArguments    =   array('yaml_schema_path'   =>  'Specify the complete directory path to your yaml schema files.',
                                            'models_path'        =>  'Specify complete path to your Doctrine_Record definitions.'),
-           $optionalArguments    =   array();
+           $optionalArguments    =   array('generate_models_options'    =>  'Array of options for generating models');
     
     public function execute()
     {
-        Doctrine::generateModelsFromYaml($this->getArgument('yaml_schema_path'), $this->getArgument('models_path'));
+        Doctrine::generateModelsFromYaml($this->getArgument('yaml_schema_path'), $this->getArgument('models_path'), $this->getArgument('generate_models_options', array()));
         
         $this->notify('Generated models successfully from YAML schema');
     }
