d0d53840a97d44553a2a74bc34f82c91ef072fdc
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-03-15 00:54:34 +0000

    fixed phpdoc for sfYaml* classes
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@7897 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/util/sfYamlDumper.class.php
+++ lib/util/sfYamlDumper.class.php
@@ -1,7 +1,23 @@
 <?php
 
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once(dirname(__FILE__).'/sfYamlInline.class.php');
 
+/**
+ * sfYamlDumper class.
+ *
+ * @package    symfony
+ * @subpackage util
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
 class sfYamlDumper
 {
   /**
--- lib/util/sfYamlParser.class.php
+++ lib/util/sfYamlParser.class.php
@@ -1,7 +1,23 @@
 <?php
 
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once(dirname(__FILE__).'/sfYamlInline.class.php');
 
+/**
+ * sfYamlParser class.
+ *
+ * @package    symfony
+ * @subpackage util
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
 class sfYamlParser
 {
   protected
