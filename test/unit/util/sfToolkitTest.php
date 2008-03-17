caef6f27aedd8eafdddbf13055d640fb7bb54c5f
Ian Christian <ian.christian@symfony-project.org>
2008-03-17 11:43:57 +0000

    refs #3134
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@7935 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/util/sfToolkitTest.php
+++ test/unit/util/sfToolkitTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(92, new lime_output_color());
+$t = new lime_test(93, new lime_output_color());
 
 // ::stringToArray()
 $t->diag('::stringToArray()');
@@ -113,6 +113,18 @@ $stripped_php = '<?php $i = 1; ';
 $t->is(preg_replace('/\s*(\r?\n)+/', ' ', sfToolkit::stripComments($php)), $stripped_php, '::stripComments() strip all comments from a php string');
 sfConfig::set('sf_strip_comments', false);
 $t->is(sfToolkit::stripComments($php), $php, '::stripComments() do nothing if "sf_strip_comments" is false');
+
+sfConfig::set('sf_strip_comments', true);
+
+$php = <<<EOF
+<?php
+  \$pluginDirs = '/*/modules/lib/helper';
+  \$pluginDirs = '/*/lib/helper';
+
+EOF;
+
+$t->is(sfToolkit::stripComments($php), $php);
+
 
 // ::stripslashesDeep()
 $t->diag('::stripslashesDeep()');
