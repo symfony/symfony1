b1878bb8dc7e464fdb81087ad2d5c10a4e1c1d97
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-04-02 09:51:54 +0000

    fixed warning in sfView unit tests
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8206 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/view/sfViewTest.php
+++ test/unit/view/sfViewTest.php
@@ -21,20 +21,6 @@ class myView extends sfView
   function render () {}
 }
 
-class sfViewParameterHolder extends sfParameterHolder
-{
-  public function initialize($context, $parameters = array())
-  {
-    $this->context = $context;
-    $this->add($parameters);
-  }
-
-  public function toArray()
-  {
-    return $this->getAll();
-  }
-}
-
 $context = sfContext::getInstance();
 $view = new myView($context, '', '', '');
 
