6a4aaa4042807915b2444a702331b2a25a8ae08a
Dustin Whittle <dustin.whittle@symfony-project.org>
2008-04-08 23:39:27 +0000

    1.1: tweaked propel upgrade task to only execute if propel.ini exists
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8369 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/project/upgrade1.1/sfPropelUpgrade.class.php
+++ lib/task/project/upgrade1.1/sfPropelUpgrade.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -21,29 +21,31 @@ class sfPropelUpgrade extends sfUpgrade
   public function upgrade()
   {
     $file = sfConfig::get('sf_config_dir').'/propel.ini';
-
-    $content = file_get_contents($file);
-    $content = str_replace('addon.propel.builder.', 'plugins.sfPropelPlugin.lib.propel.builder.', $content, $count);
-
-    if ($count)
+    if(is_readable($file))
     {
-      $this->logSection('propel', sprintf('Migrating %s', $file));
-      file_put_contents($file, $content);
-    }
+      $content = file_get_contents($file);
+      $content = str_replace('addon.propel.builder.', 'plugins.sfPropelPlugin.lib.propel.builder.', $content, $count);
 
-    // add default date and time format
-    $content = file_get_contents($file);
-    if (false === strpos($content, 'propel.defaultDateFormat'))
-    {
-      $content .= <<<EOF
+      if ($count)
+      {
+        $this->logSection('propel', sprintf('Migrating %s', $file));
+        file_put_contents($file, $content);
+      }
+
+      // add default date and time format
+      $content = file_get_contents($file);
+      if (false === strpos($content, 'propel.defaultDateFormat'))
+      {
+        $content .= <<<EOF
 
 propel.defaultTimeStampFormat = Y-m-d H:i:s
 propel.defaultTimeFormat = H:i:s
 propel.defaultDateFormat = Y-m-d
 
 EOF;
-      $this->logSection('propel', sprintf('Migrating %s', $file));
-      file_put_contents($file, $content);
+        $this->logSection('propel', sprintf('Migrating %s', $file));
+        file_put_contents($file, $content);
+      }
     }
   }
 }
