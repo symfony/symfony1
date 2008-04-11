c58357791bf0c8c60b69fec4bd2a22a080bf5cea
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-04-11 07:41:48 +0000

    fixed crud functional tests for m2m relationships
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8409 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/plugins/sfPropelPlugin/test/functional/fixtures/lib/form/MovieForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Movie form.
+ *
+ * @package    form
+ * @subpackage movie
+ * @version    SVN: $Id$
+ */
+class MovieForm extends BaseMovieForm
+{
+  public function configure()
+  {
+  }
+}
