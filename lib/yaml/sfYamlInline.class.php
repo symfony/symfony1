76397de251e70d5207f8889bcf9cf43b9385c1c3
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-04-11 07:53:10 +0000

    added quotes when dumping string that contains & and * (aliases) to YAML
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8411 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/yaml/sfYamlInline.class.php
+++ lib/yaml/sfYamlInline.class.php
@@ -74,7 +74,7 @@ class sfYamlInline
         return is_infinite($value) ? str_ireplace('INF', '.Inf', strval($value)) : (is_string($value) ? "'$value'" : $value);
       case false !== strpos($value, "\n"):
         return sprintf('"%s"', str_replace(array('"', "\n"), array('\\"', '\n'), $value));
-      case preg_match('/[ \s \' " \: \{ \} \[ \] , ]/x', $value):
+      case preg_match('/[ \s \' " \: \{ \} \[ \] , & \*]/x', $value):
         return sprintf("'%s'", str_replace('\'', '\'\'', $value));
       case '' == $value:
         return "''";
