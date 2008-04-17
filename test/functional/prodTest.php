b48a173380563b6abb2cd3abb87db61aa1ce010b
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-04-17 19:27:02 +0000

    removed uneeded code in a functional test
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8517 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/functional/prodTest.php
+++ test/functional/prodTest.php
@@ -8,9 +8,6 @@
  * file that was distributed with this source code.
  */
 
-require_once(dirname(__FILE__).'/../../lib/util/sfToolkit.class.php');
-sfToolkit::clearDirectory(dirname(__FILE__).'/fixtures/project/cache');
-
 $app = 'frontend';
 $debug = false;
 if (!include(dirname(__FILE__).'/../bootstrap/functional.php'))
