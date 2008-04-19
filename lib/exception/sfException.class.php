4da9402411f38923973bd8f9d6e59d1ca8b2e11d
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-04-19 01:15:36 +0000

    fixed pbe in previous commit
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8549 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/exception/sfException.class.php
+++ lib/exception/sfException.class.php
@@ -63,7 +63,7 @@ class sfException extends Exception
       error_log($exception->getMessage());
 
       // clean current output buffer
-//      while (@ob_end_clean());
+      while (@ob_end_clean());
 
       ob_start(sfConfig::get('sf_compressed') ? 'ob_gzhandler' : '');
 
