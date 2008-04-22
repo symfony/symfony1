8724f171e15482751b7d3d6973eb361fec96fa91
Dustin Whittle <dustin.whittle@symfony-project.org>
2008-04-22 20:31:39 +0000

    1.1: removed config_handlers.yml from sfCompat10Plugin (and added to config.php)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8596 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfCompat10Plugin/config/config.php
+++ lib/plugins/sfCompat10Plugin/config/config.php
@@ -6,6 +6,7 @@ if (sfConfig::get('sf_compat_10'))
   {
     class sfCompatAutoload extends sfSimpleAutoload
     {
+
     }
 
     // autoload classes
@@ -16,6 +17,9 @@ if (sfConfig::get('sf_compat_10'))
 
   // register config handler for validate/*.yml files
   sfProjectConfiguration::getActive()->getConfigCache()->registerConfigHandler('modules/*/validate/*.yml', 'sfValidatorConfigHandler');
+
+  // register config handler for config/mailer.yml files
+  sfProjectConfiguration::getActive()->getConfigCache()->registerConfigHandler('modules/*/config/mailer.yml', 'sfDefineEnvironmentConfigHandler', array('prefix' => 'sf_mailer_', 'module' => 'yes'));
 
   // register the validation execution filter
   sfConfig::set('sf_execution_filter', array('sfValidationExecutionFilter', array()));
