a6602ed1ff854c43b9348eca9e261f16598816e2
Fabian Lange <fabian.lange@symfony-project.org>
2008-05-02 10:14:13 +0000

    fixed typo. fixes #3353
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8721 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/ObjectAdminHelper.php
+++ lib/helper/ObjectAdminHelper.php
@@ -107,7 +107,7 @@ function object_admin_double_list($object, $method, $options = array(), $callbac
   return sprintf($html,
     $label_all,
     $select1,
-    submit_image_tag(sfConfig::get('sf_admin_web_dir').'/images/next.png', "style=\"border: 0\" onclick=\"double_list_move('{$name1}), '{$name2}'); return false;\""),
+    submit_image_tag(sfConfig::get('sf_admin_web_dir').'/images/next.png', "style=\"border: 0\" onclick=\"double_list_move('{$name1}', '{$name2}'); return false;\""),
     submit_image_tag(sfConfig::get('sf_admin_web_dir').'/images/previous.png', "style=\"border: 0\" onclick=\"double_list_move('{$name2}', '{$name1}'); return false;\""),
     $label_assoc,
     $select2
