75198f1fb874165822ee659a87820f5305dc8524
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-05-04 16:58:45 +0000

    removed Propel fix in MapBuilder as the bug has been fixed upstream
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8769 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/lib/propel/builder/SfMapBuilderBuilder.php
+++ lib/plugins/sfPropelPlugin/lib/propel/builder/SfMapBuilderBuilder.php
@@ -37,17 +37,4 @@ class SfMapBuilderBuilder extends PHP5MapBuilderBuilder
 
     parent::addIncludes($script);
   }
-
-  protected function addDoBuild(&$script)
-  {
-    parent::addDoBuild($script);
-
-    // fix http://propel.phpdb.org/trac/ticket/235: Column sizes not being inserted into [table]MapBuilder->DoBuild() by PHP5MapBuilderBuilder
-    $sizes = array();
-    foreach ($this->getTable()->getColumns() as $col)
-    {
-      $sizes[$col->getPhpName()] = !$col->getSize() ? 'null' : $col->getSize();
-    }
-    $script = preg_replace("/\\\$tMap\->addColumn\('([^']+)', '([^']+)', '([^']+)', CreoleTypes\:\:VARCHAR, (false|true)\)/e", '"\\\$tMap->addColumn(\'$1\', \'$2\', \'$3\', CreoleTypes::VARCHAR, $4, {$sizes[\'$2\']})"', $script);
-  }
 }
