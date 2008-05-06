ce21721eb8147235a5938244b9839595212c1c8f
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-05-06 14:49:50 +0000

    fixed loc.php
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8811 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/bin/loc.php
+++ test/bin/loc.php
@@ -12,7 +12,12 @@ printf("==============%s\n\n", str_repeat('=', strlen($version)));
 
 // symfony core LOC
 $total_loc = 0;
-$files = sfFinder::type('file')->name('*.php')->prune('vendor')->in($root_dir.'/lib');
+$files = sfFinder::type('file')->name('*.php')->prune('vendor', 'plugins')->in($root_dir.'/lib');
+foreach ($files as $file)
+{
+  $total_loc += count(lime_coverage::get_php_lines($file));
+}
+$files = sfFinder::type('file')->name('*.php')->prune('vendor')->in($root_dir.'/lib/plugins/*/lib');
 foreach ($files as $file)
 {
   $total_loc += count(lime_coverage::get_php_lines($file));
@@ -20,7 +25,15 @@ foreach ($files as $file)
 
 // symfony tests LOC
 $total_tests_loc = 0;
-$files = sfFinder::type('file')->name('*Test.php')->in(array($root_dir.'/test/unit', $root_dir.'/test/functional', $root_dir.'/test/other'));
+$files = sfFinder::type('file')->name('*Test.php')->in(array(
+  $root_dir.'/lib/plugins/sfCompat10Plugin/test/unit',
+  $root_dir.'/lib/plugins/sfCompat10Plugin/test/functional',
+  $root_dir.'/lib/plugins/sfPropelPlugin/test/unit',
+  $root_dir.'/lib/plugins/sfPropelPlugin/test/functional',
+  $root_dir.'/test/unit',
+  $root_dir.'/test/functional',
+  $root_dir.'/test/other',
+));
 foreach ($files as $file)
 {
   $total_tests_loc += count(lime_coverage::get_php_lines($file));
