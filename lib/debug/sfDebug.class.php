0639ac23fe086d5237578b72242a3d78b21f3c4b
Dustin Whittle <dustin.whittle@symfony-project.org>
2008-05-09 00:29:00 +0000

    1.1: tweaked php extension sorting in web debug toolbar
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@8870 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/debug/sfDebug.class.php
+++ lib/debug/sfDebug.class.php
@@ -44,12 +44,18 @@ class sfDebug
       'extensions' => get_loaded_extensions(),
     );
 
-    // assign extension version
-    if ($values['extensions'])
-    {
-      foreach($values['extensions'] as $key => $extension)
+      natcasesort($values['extensions']);
+
+      // assign extension version
+      if($values['extensions'])
       {
-        $values['extensions'][$key] = phpversion($extension) ? sprintf('%s (%s)', $extension, phpversion($extension)) : $extension;
+        foreach($values['extensions'] as $key => $extension)
+        {
+          if($version = phpversion($extension))
+          {
+            $values['extensions'][$key] = sprintf('%s (%s)', $extension, $version);
+          }
+        }
       }
     }
 
