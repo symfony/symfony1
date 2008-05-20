23dacf645836603012935b68bc0d2a9b9bd340bc
Fabian Lange <fabian.lange@symfony-project.org>
2008-05-20 08:56:56 +0000

    made sfValidatorOrTest robust against different line endings
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@9103 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/validator/sfValidatorOrTest.php
+++ test/unit/validator/sfValidatorOrTest.php
@@ -97,21 +97,9 @@ $t->diag('->asString()');
 $v1 = new sfValidatorString(array('max_length' => 3));
 $v2 = new sfValidatorString(array('min_length' => 3));
 $v = new sfValidatorOr(array($v1, $v2));
-$t->is($v->asString(), <<<EOF
-(
-  String({ max_length: 3 })
-  or
-  String({ min_length: 3 })
-)
-EOF
+$t->is($v->asString(), "(\n  String({ max_length: 3 })\n  or\n  String({ min_length: 3 })\n)"
 , '->asString() returns a string representation of the validator');
 
 $v = new sfValidatorOr(array($v1, $v2), array(), array('required' => 'This is required.'));
-$t->is($v->asString(), <<<EOF
-(
-  String({ max_length: 3 })
-  or({}, { required: 'This is required.' })
-  String({ min_length: 3 })
-)
-EOF
+$t->is($v->asString(), "(\n  String({ max_length: 3 })\n  or({}, { required: 'This is required.' })\n  String({ min_length: 3 })\n)"
 , '->asString() returns a string representation of the validator');
