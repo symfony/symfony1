8102eb88c2cb27fbffccbf110da64d20bc0c048e
Fabian Lange <fabian.lange@symfony-project.org>
2008-05-21 11:01:43 +0000

    forgot to increment the lime test number
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@9143 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/util/sfParameterHolderTest.php
+++ test/unit/util/sfParameterHolderTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(26, new lime_output_color());
+$t = new lime_test(27, new lime_output_color());
 
 // ->clear()
 $t->diag('->clear()');
