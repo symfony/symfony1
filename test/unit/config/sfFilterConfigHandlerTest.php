a340400681740c12f9fc569a9a085a1895cd847d
Dustin Whittle <dustin.whittle@symfony-project.org>
2008-06-02 00:52:26 +0000

    1.1: fixed filter config handler unit test
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@9402 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/config/sfFilterConfigHandlerTest.php
+++ test/unit/config/sfFilterConfigHandlerTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -96,5 +96,5 @@ $files = array(
 );
 
 $data = $handler->execute($files);
-$data = preg_replace('#date\: \d+/\d+/\d+ \d+\:\d+\:\d+#', '', $data);
+$data = preg_replace('#date\: \d+/\d+/\d+ \d+\:\d+\:\d+\n#', '', $data);
 $t->is($data, str_replace("\r\n", "\n", file_get_contents($dir.'result.php')), 'core filters.yml can be overriden');
