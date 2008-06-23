53b46271e5e005762e774f37854dc57d7305e517
Dustin Whittle <dustin.whittle@symfony-project.org>
2008-06-23 04:52:42 +0000

    1.1: fixed sfWidgetFormPropelSelect doesn't work with i18n tables (closes #3715)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@9775 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/lib/propel/widget/sfWidgetFormPropelSelect.class.php
+++ lib/plugins/sfPropelPlugin/lib/propel/widget/sfWidgetFormPropelSelect.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -55,6 +55,7 @@ class sfWidgetFormPropelSelect extends sfWidgetFormSelect
     $this->addOption('criteria', null);
     $this->addOption('connection', null);
     $this->addOption('multiple', false);
+    $this->addOption('value_method', 'getPrimaryKey');
 
     parent::configure($options, $attributes);
   }
@@ -78,14 +79,15 @@ class sfWidgetFormPropelSelect extends sfWidgetFormSelect
     if ($order = $this->getOption('order_by'))
     {
       $method = sprintf('add%sOrderByColumn', 0 === strpos(strtoupper($order[1]), 'ASC') ? 'Ascending' : 'Descending');
-      $criteria->$method(call_user_func(array($class, 'translateFieldName'), $order[0], BasePeer::TYPE_PHPNAME, BasePeer::TYPE_COLNAME));
+      $criteria->$method(call_user_func(array($class, 'translateFieldName'), strtolower($order[0]), BasePeer::TYPE_PHPNAME, BasePeer::TYPE_COLNAME));
     }
     $objects = call_user_func(array($class, 'doSelect'), $criteria, $this->getOption('connection'));
 
+    $valueMethod = $this->getOption('value_method') ? $this->getOption('value_method') : 'getPrimaryKey';
     $method = $this->getOption('method');
     foreach ($objects as $object)
     {
-      $choices[$object->getPrimaryKey()] = $object->$method();
+      $choices[$object->$valueMethod()] = $object->$method();
     }
 
     return $choices;
