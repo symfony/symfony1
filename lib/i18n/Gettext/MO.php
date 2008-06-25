f41ea9dd1cbaa4fa04d496d65cc5af568601be1d
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-06-25 11:33:49 +0000

    removed double licenses from gettext files (with the permission of Qiang Xue)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.1@9856 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/i18n/Gettext/MO.php
+++ lib/i18n/Gettext/MO.php
@@ -1,22 +1,5 @@
 <?php
-/**
- * TGettext_MO class file.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the BSD License.
- *
- * Copyright(c) 2004 by Qiang Xue. All rights reserved.
- *
- * To contact the author write to {@link mailto:qiang.xue@gmail.com Qiang Xue}
- * The latest version of PRADO can be obtained from:
- * {@link http://prado.sourceforge.net/}
- *
- * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
- * @version $Revision$  $Date$
- * @package System.I18N.core
- */
 
- 
 // +----------------------------------------------------------------------+
 // | PEAR :: File :: Gettext :: MO                                        |
 // +----------------------------------------------------------------------+
--- lib/i18n/Gettext/PO.php
+++ lib/i18n/Gettext/PO.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * TGettext_PO class file.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the BSD License.
- *
- * Copyright(c) 2004 by Qiang Xue. All rights reserved.
- *
- * To contact the author write to {@link mailto:qiang.xue@gmail.com Qiang Xue}
- * The latest version of PRADO can be obtained from:
- * {@link http://prado.sourceforge.net/}
- *
- * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
- * @version $Revision$  $Date$
- * @package System.I18N.core
- */
 
 // +----------------------------------------------------------------------+
 // | PEAR :: File :: Gettext :: PO                                        |
