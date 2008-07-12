e4c833f39ed7a603473d02df56508ef1cfc3d748
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-07-12 23:54:48 +0000

    Removing unused functions.
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.0/lib@4680 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Lib.php
+++ Doctrine/Lib.php
@@ -257,67 +257,6 @@ class Doctrine_Lib
         }
     }
 
-    // Code from symfony sfToolkit class. See LICENSE
-    /**
-     * stringToArray
-     *
-     * @param string $string 
-     * @return void
-     */
-    public static function stringToArray($string)
-    {
-        preg_match_all('/
-          \s*(\w+)              # key                               \\1
-          \s*=\s*               # =
-          (\'|")?               # values may be included in \' or " \\2
-          (.*?)                 # value                             \\3
-          (?(2) \\2)            # matching \' or " if needed        \\4
-          \s*(?:
-            (?=\w+\s*=) | \s*$  # followed by another key= or the end of the string
-          )
-        /x', $string, $matches, PREG_SET_ORDER);
-
-        $attributes = array();
-        foreach ($matches as $val) {
-            $attributes[$val[1]] = self::literalize($val[3]);
-        }
-
-        return $attributes;
-    }
-
-    /**
-     * Finds the type of the passed value, returns the value as the new type.
-     *
-     * @param  string
-     * @return mixed
-     */
-    public static function literalize($value, $quoted = false)
-    {
-        // lowercase our value for comparison
-        $value  = trim($value);
-        $lvalue = strtolower($value);
-
-        if (in_array($lvalue, array('null', '~', '')))
-        {
-            $value = null;
-        } else if (in_array($lvalue, array('true', 'on', '+', 'yes'))) {
-            $value = true;
-        } else if (in_array($lvalue, array('false', 'off', '-', 'no'))) {
-            $value = false;
-        } else if (ctype_digit($value)) {
-            $value = (int) $value;
-        } else if (is_numeric($value)) {
-            $value = (float) $value;
-        } else {
-            if ($quoted)
-            {
-                $value = '\''.str_replace('\'', '\\\'', $value).'\'';
-            }
-        }
-
-        return $value;
-    }
-
     /**
      * getValidators
      *
