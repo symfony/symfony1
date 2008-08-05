bbc7b1517d6282188d3b88e06b5042885f57db63
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-08-05 19:16:07 +0000

    [1.1, 1.2] fixed propel tasks when Phing is installed as PEAR (closes #4059)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@10678 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/lib/task/sfPropelBaseTask.class.php
+++ lib/plugins/sfPropelPlugin/lib/task/sfPropelBaseTask.class.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__).'/../vendor');
+set_include_path(dirname(__FILE__).'/../vendor'.PATH_SEPARATOR.get_include_path());
 
 /**
  * Base class for all symfony Propel tasks.
