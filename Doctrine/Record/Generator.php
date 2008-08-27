f2a0589081ce97bebc98028bd76eed93a87f693b
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-08-27 19:04:26 +0000

    fixes #4850
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.0/lib@4851 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Record/Generator.php
+++ Doctrine/Record/Generator.php
@@ -356,7 +356,7 @@ abstract class Doctrine_Record_Generator extends Doctrine_Record_Abstract
             if (isset($this->_options['generatePath']) && $this->_options['generatePath']) {
                 $builder->setTargetPath($this->_options['generatePath']);
                 $builderOptions = isset($this->_options['builderOptions']) ? (array) $this->_options['builderOptions']:array();
-                $builder->setOptions($builderoptions);
+                $builder->setOptions($builderOptions);
                 $builder->buildRecord($definition);
             } else {
                 throw new Doctrine_Record_Exception('If you wish to generate files then you must specify the path to generate the files in.');
