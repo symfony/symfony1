0f6a3378b7103dfa3c17668b85ea1641c338b49f
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-09-14 15:23:48 +0000

    [1.2] added a new choice widget family
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@11539 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/widget/sfWidgetFormSelectCheckbox.class.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfWidgetFormSelectCheckbox represents an array of checkboxes.
+ *
+ * @package    symfony
+ * @subpackage widget
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+class sfWidgetFormSelectCheckbox extends sfWidgetForm
+{
+  /**
+   * Constructor.
+   *
+   * Available options:
+   *
+   *  * choices:         An array of possible choices (required)
+   *  * label_separator: The separator to use between the input checkbox and the label
+   *  * separator:       The separator to use between each input checkbox
+   *  * formatter:       A callable to call to format the checkbox choices
+   *                     The formatter callable receives the widget and the array of inputs as arguments
+   *
+   * @param array $options     An array of options
+   * @param array $attributes  An array of default HTML attributes
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array())
+  {
+    $this->addRequiredOption('choices');
+
+    $this->addOption('label_separator', '&nbsp;');
+    $this->addOption('separator', "\n");
+    $this->addOption('formatter', array($this, 'formatter'));
+  }
+
+  /**
+   * @param  string $name        The element name
+   * @param  string $value       The value selected in this widget
+   * @param  array  $attributes  An array of HTML attributes to be merged with the default HTML attributes
+   * @param  array  $errors      An array of errors for the field
+   *
+   * @return string An HTML tag string
+   *
+   * @see sfWidgetForm
+   */
+  public function render($name, $value = null, $attributes = array(), $errors = array())
+  {
+    if ('[]' != substr($name, -2))
+    {
+      $name .= '[]';
+    }
+
+    $choices = $this->getOption('choices');
+    if ($choices instanceof sfCallable)
+    {
+      $choices = $choices->call();
+    }
+
+    $inputs = array();
+    foreach ($choices as $key => $option)
+    {
+      $baseAttributes = array(
+        'name'  => $name,
+        'type'  => 'checkbox',
+        'value' => self::escapeOnce($key),
+        'id'    => $id = $this->generateId($name, self::escapeOnce($key)),
+      );
+
+      if (in_array(strval($key), $value))
+      {
+        $baseAttributes['checked'] = 'checked';
+      }
+
+      $inputs[] = array(
+        'input' => $this->renderTag('input', array_merge($baseAttributes, $attributes)),
+        'label' => $this->renderContentTag('label', $option, array('for' => $id)),
+      );
+    }
+
+    return call_user_func($this->getOption('formatter'), $this, $inputs);
+  }
+
+  public function formatter($widget, $inputs)
+  {
+    $rows = array();
+    foreach ($inputs as $input)
+    {
+      $rows[] = $this->renderContentTag('li', $input['input'].$this->getOption('label_separator').$input['label']);
+    }
+
+    return $this->renderContentTag('ul', implode($this->getOption('separator'), $rows), array('class' => 'checkbox_list'));
+  }
+}
