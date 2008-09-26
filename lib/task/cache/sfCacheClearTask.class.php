a8a971a8670c9e80a22cb05b7132676ede7ec49a
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-09-26 13:07:54 +0000

    [1.2] fixed locking problem with symfony cache:clear task (closes #4470)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@11798 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/cache/sfCacheClearTask.class.php
+++ lib/task/cache/sfCacheClearTask.class.php
@@ -101,9 +101,10 @@ EOF;
 
         $this->logSection('cache', sprintf('Clearing cache type "%s" for "%s" app and "%s" env', $options['type'], $app, $env));
 
+        $appConfiguration = ProjectConfiguration::getApplicationConfiguration($app, $env, true);
+
         $this->lock($app, $env);
 
-        $appConfiguration = ProjectConfiguration::getApplicationConfiguration($app, $env, true);
         $event = $appConfiguration->getEventDispatcher()->notifyUntil(new sfEvent($this, 'task.cache.clear', array('app' => $appConfiguration, 'env' => $env, 'type' => $options['type'])));
         if (!$event->isProcessed())
         {
