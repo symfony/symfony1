138abb8dcfe6086b8700eef45477ad00f31a633a
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-10-07 08:13:10 +0000

    [1.2] fixed coding standard
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@12018 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/storage/sfCacheSessionStorage.class.php
+++ lib/storage/sfCacheSessionStorage.class.php
@@ -12,13 +12,13 @@
  */
 class sfCacheSessionStorage extends sfStorage
 {
-  protected $context = null,
-            $request = null,
-            $response = null,
-						$cache = null;
-
-  private $data = array(),
-          $dataChanged = false;
+  protected
+    $context     = null,
+    $request     = null,
+    $response    = null,
+    $cache       = null;
+    $data        = array(),
+    $dataChanged = false;
 
   /**
    * Initialize this Storage.
@@ -45,11 +45,11 @@ class sfCacheSessionStorage extends sfStorage
                                          'session_cookie_secure' => false,
                                          'session_cookie_http_only' => true,
                                          'session_cookie_secret' => 'sf$ecret'), $options));
-    
-		// create cache instance
+
+    // create cache instance
     if (isset($this->options['cache']) && $this->options['cache']['class'])
     {
-			$this->cache = new $this->options['cache']['class'](is_array($this->options['cache']['param']) ? $this->options['cache']['param'] : array());
+      $this->cache = new $this->options['cache']['class'](is_array($this->options['cache']['param']) ? $this->options['cache']['param'] : array());
     }
     else
     {
@@ -62,65 +62,65 @@ class sfCacheSessionStorage extends sfStorage
     $this->request     = $this->context->getRequest();
     $this->response    = $this->context->getResponse();
 
-		$cookie = $this->request->getCookie($this->options['session_name']);
-		
-		if(strpos($cookie, ':') !== false)
-		{
-			// split cookie data id:signature(id+secret)
-			list($id, $signature) = explode(':', $cookie, 2);
+    $cookie = $this->request->getCookie($this->options['session_name']);
+    
+    if(strpos($cookie, ':') !== false)
+    {
+      // split cookie data id:signature(id+secret)
+      list($id, $signature) = explode(':', $cookie, 2);
 
-			if($signature == sha1($id.':'.$this->options['session_cookie_secret']))
-			{
-				// cookie is valid
-				$this->id = $id;
-			}
-			else
-			{
-				// cookie signature broken
-				$this->id = null;
-			}
-		}
-		else
-		{
-			// cookie format wrong
-			$this->id = null;
-		}
+      if($signature == sha1($id.':'.$this->options['session_cookie_secret']))
+      {
+        // cookie is valid
+        $this->id = $id;
+      }
+      else
+      {
+        // cookie signature broken
+        $this->id = null;
+      }
+    }
+    else
+    {
+      // cookie format wrong
+      $this->id = null;
+    }
 
-		if(empty($this->id))
-		{
-			 $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'localhost';
-			 $ua = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'ua';
+    if(empty($this->id))
+    {
+       $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'localhost';
+       $ua = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'ua';
 
-			 // generate new id based on random # / ip / user agent / secret
-			 $this->id = md5(rand(0, 999999).$ip.$ua.$this->options['session_cookie_secret']);
+       // generate new id based on random # / ip / user agent / secret
+       $this->id = md5(rand(0, 999999).$ip.$ua.$this->options['session_cookie_secret']);
 
-  		 if(sfConfig::get('sf_logging_enabled'))
-			 {
-			   $this->dispatcher->notify(new sfEvent($this, 'application.log', array('New session created')));
-			 }
-			
-			 // only send cookie when id is issued
- 	     $this->response->setCookie($this->options['session_name'],
-	                                $this->id.':'.sha1($this->id.':'.$this->options['session_cookie_secret']),
-	                                $this->options['session_cookie_lifetime'],
-	                                $this->options['session_cookie_path'],
-	                                $this->options['session_cookie_domain'],
-	                                $this->options['session_cookie_secure'],
-	                                $this->options['session_cookie_http_only']);
-			
+       if(sfConfig::get('sf_logging_enabled'))
+       {
+         $this->dispatcher->notify(new sfEvent($this, 'application.log', array('New session created')));
+       }
+
+       // only send cookie when id is issued
+       $this->response->setCookie($this->options['session_name'],
+                                  $this->id.':'.sha1($this->id.':'.$this->options['session_cookie_secret']),
+                                  $this->options['session_cookie_lifetime'],
+                                  $this->options['session_cookie_path'],
+                                  $this->options['session_cookie_domain'],
+                                  $this->options['session_cookie_secure'],
+                                  $this->options['session_cookie_http_only']);
+
        $this->data = array();
-		}
-		else
-		{
-			// load data from cache
-			$this->data = $this->cache->get($this->id, array());
-			
+    }
+    else
+    {
+      // load data from cache
+      $this->data = $this->cache->get($this->id, array());
+
       if(sfConfig::get('sf_logging_enabled'))
       {
         $this->dispatcher->notify(new sfEvent($this, 'application.log', array('Restored previous session')));
         // $this->dispatcher->notify(new sfEvent($this, 'application.log', array(var_export($this->data, true))));
       }
-		}
+    }
 
     return true;
   }
@@ -137,7 +137,7 @@ class sfCacheSessionStorage extends sfStorage
    */
   public function write($key, $data)
   {
-	  $this->dataChanged = true;
+    $this->dataChanged = true;
 
     $this->data[$key] =& $data;
   }
@@ -178,7 +178,7 @@ class sfCacheSessionStorage extends sfStorage
 
     if (isset($this->data[$key]))
     {
-		  $this->dataChanged = true;
+      $this->dataChanged = true;
 
       $retval =& $this->data[$key];
       unset($this->data[$key]);
@@ -198,20 +198,19 @@ class sfCacheSessionStorage extends sfStorage
    */
   public function regenerate($destroy = false)
   {
-	
-		if($destroy)
-		{
-			$this->data = array();
-			$this->cache->remove($this->id);
-		}
-		
-		// generate session id
-		$this->id = md5(rand(0, 999999).$_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT'].$this->options['session_cookie_secret']);
-		
-		// save data to cache
-		$this->cache->set($this->id, $this->data);
-		
-		// update session id in signed cookie
+    if($destroy)
+    {
+      $this->data = array();
+      $this->cache->remove($this->id);
+    }
+
+    // generate session id
+    $this->id = md5(rand(0, 999999).$_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT'].$this->options['session_cookie_secret']);
+
+    // save data to cache
+    $this->cache->set($this->id, $this->data);
+
+    // update session id in signed cookie
     $this->response->setCookie($this->options['session_name'],
                                $this->id.':'.sha1($this->id.':'.$this->options['session_cookie_secret']),
                                $this->options['session_cookie_lifetime'],
@@ -219,17 +218,17 @@ class sfCacheSessionStorage extends sfStorage
                                $this->options['session_cookie_domain'],
                                $this->options['session_cookie_secure'],
                                $this->options['session_cookie_http_only']);
-	
+
     return true;
   }
 
   /**
-  * Expires the session storage instance.
-  */
+   * Expires the session storage instance.
+   */
   public function expire()
   {
-	  // destroy data and regenerate id
-		$this->regenerate(true);
+    // destroy data and regenerate id
+    $this->regenerate(true);
 
     if(sfConfig::get('sf_logging_enabled'))
     {
@@ -237,23 +236,22 @@ class sfCacheSessionStorage extends sfStorage
     }
   }
 
-
   /**
    * Executes the shutdown procedure.
    *
    * @throws <b>sfStorageException</b> If an error occurs while shutting down this storage
    */
   public function shutdown()
-  { 
-	  // only update cache if session has changed
-	  if($this->dataChanged === true)
-	  {
-  		$this->cache->set($this->id, $this->data);
-	    if(sfConfig::get('sf_logging_enabled'))
-	    {
-	      $this->dispatcher->notify(new sfEvent($this, 'application.log', array('Storing session to cache')));
-	      // $this->dispatcher->notify(new sfEvent($this, 'application.log', array(var_export($this->data, true))));
-	    }
-	  }
+  {
+    // only update cache if session has changed
+    if($this->dataChanged === true)
+    {
+      $this->cache->set($this->id, $this->data);
+      if(sfConfig::get('sf_logging_enabled'))
+      {
+        $this->dispatcher->notify(new sfEvent($this, 'application.log', array('Storing session to cache')));
+        // $this->dispatcher->notify(new sfEvent($this, 'application.log', array(var_export($this->data, true))));
+      }
+    }
   }
 }
