217cd24ae636d071ee30509c4ffb6783ee9f7a4d
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-10-07 21:17:59 +0000

    [1.0, 1.1] fixes #1545 - fixes issue with record filters and null values
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5065 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Record.php
+++ Doctrine/Record.php
@@ -1149,6 +1149,8 @@ abstract class Doctrine_Record extends Doctrine_Record_Abstract implements Count
     {
         if ($type == 'boolean' && (is_bool($old) || is_numeric($old)) && (is_bool($new) || is_numeric($new)) && $old == $new) {
             return false;
+        } else if (in_array($type, array('decimal', 'float')) && is_numeric($old) && is_numeric($new)) {
+            return $old * 100 != $new * 100;
         } else {
             return $old !== $new;
         }
