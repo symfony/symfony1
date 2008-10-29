3e20983627cda8936a3180f8a763702d89f41544
Fabian Lange <fabian.lange@symfony-project.org>
2008-10-29 10:06:09 +0000

    [1.2] changed ConfigureCore task to PublishAssets. It also now iterates over all installed plugins, rather than only the core plugins. Only working with core plugins can be enforced with --only-core
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@12403 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/plugin/sfPluginPublishAssetsTask.class.php
+++ lib/task/plugin/sfPluginPublishAssetsTask.class.php
@@ -11,32 +11,36 @@
 require_once(dirname(__FILE__).'/sfPluginBaseTask.class.php');
 
 /**
- * Lists installed plugins.
+ * Publishes Web Assets for Core and third party plugins
  *
  * @package    symfony
  * @subpackage task
  * @author     Fabian Lange <fabian.lange@symfony-project.com>
- * @version    SVN: $Id: sfPluginConfigureCoreTask.class.php 7655 2008-02-28 09:52:40Z fabien $
+ * @version    SVN: $Id: sfPluginPublishAssetsTask.class.php 7655 2008-02-28 09:52:40Z fabien $
  */
-class sfPluginConfigureCoreTask extends sfPluginBaseTask
+class sfPluginPublishAssetsTask extends sfPluginBaseTask
 {
   /**
    * @see sfTask
    */
   protected function configure()
   {
+    $this->addArguments(array(
+      new sfCommandArgument('only-core', sfCommandArgument::OPTIONAL, 'if set only core plugins will pubish their assets'),
+    ));
+    
     $this->aliases = array();
     $this->namespace = 'plugin';
-    $this->name = 'configure-core';
+    $this->name = 'publish-assets';
 
-    $this->briefDescription = 'Configures symfony core plugins';
+    $this->briefDescription = 'Publishes web assets for all plugins';
 
     $this->detailedDescription = <<<EOF
-The [plugin:coreconfigure|INFO] task will configure core plugins for use:
+The [plugin:publish-assets|INFO] task will publish web assets from all plugins.
 
-  [./symfony plugin:configure-core|INFO]
+  [./symfony plugin:publish-assets|INFO]
 
-In fact this will send the [plugin.post_install|INFO] event for each core plugin.
+In fact this will send the [plugin.post_install|INFO] event to each plugin.
 
 EOF;
   }
@@ -54,6 +58,14 @@ EOF;
     {
       $this->logSection('plugin', 'Configuring core plugin - '.$plugin);
       $this->dispatcher->notify(new sfEvent($this, 'plugin.post_install', array('plugin' => $plugin, 'plugin_dir' => $corePluginsDir)));
+    }
+    if (! $arguments['only-core']) {
+      $thirdPartyPlugins = sfConfig::get('sf_plugins_dir');
+      foreach (sfFinder::type('dir')->relative()->maxdepth(0)->in($thirdPartyPlugins) as $plugin)
+      {
+        $this->logSection('plugin', 'Configuring plugin - '.$plugin);
+        $this->dispatcher->notify(new sfEvent($this, 'plugin.post_install', array('plugin' => $plugin, 'plugin_dir' => $thirdPartyPlugins)));
+      }
     }
   }
 }
