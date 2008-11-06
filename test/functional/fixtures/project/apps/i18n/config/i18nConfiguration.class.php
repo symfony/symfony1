3af18e2133bbbe2e73e63f022f6322cf2bdf4bf8
Kris Wallsmith <kris.wallsmith@gmail.com>
2008-11-06 01:35:07 +0000

    [1.2] fixed broken tests
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@12673 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/functional/fixtures/project/apps/i18n/config/i18nConfiguration.class.php
+++ test/functional/fixtures/project/apps/i18n/config/i18nConfiguration.class.php
@@ -2,8 +2,9 @@
 
 class i18nConfiguration extends sfApplicationConfiguration
 {
-  public function configure()
+  public function setup()
   {
+    parent::setup();
     $this->enablePlugins('sfI18NPlugin');
   }
 }
