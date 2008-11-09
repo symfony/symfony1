7a1a144423dd57079f1a459b4cceb425094adb6c
Fabian Lange <fabian.lange@symfony-project.org>
2008-11-09 19:41:54 +0000

    [1.2] new unit test for calculateRelativeDir()
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@12849 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/task/sfFilesystemTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+error_reporting(error_reporting() & ~E_STRICT);
+
+require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
+
+class myFilesystem extends sfFilesystem
+{
+  public function calculateRelativeDir($from, $to)
+  {
+    return parent::calculateRelativeDir($from, $to);
+  }
+}
+
+$t = new lime_test(3, new lime_output_color());
+
+$dispatcher = new sfEventDispatcher();
+$filesystem = new myFilesystem($dispatcher, null);
+
+$t->diag('sfFilesystem calculates relative pathes');
+$common = DIRECTORY_SEPARATOR.'tmp'.DIRECTORY_SEPARATOR.'sfproject'.DIRECTORY_SEPARATOR;
+$source = $common.'web'.DIRECTORY_SEPARATOR.'myplugin';
+$target = $common.'plugins'.DIRECTORY_SEPARATOR.'myplugin'.DIRECTORY_SEPARATOR.'web';
+$t->is($filesystem->calculateRelativeDir($source, $target), '..'.DIRECTORY_SEPARATOR.'plugins'.DIRECTORY_SEPARATOR.'myplugin'.DIRECTORY_SEPARATOR.'web', '->calculateRelativeDir() correctly calculates the relative path');
+
+$source = $common.'web'.DIRECTORY_SEPARATOR.'myplugin';
+$target = $common.'web'.DIRECTORY_SEPARATOR.'otherplugin'.DIRECTORY_SEPARATOR.'sub';
+$t->is($filesystem->calculateRelativeDir($source, $target), 'otherplugin'.DIRECTORY_SEPARATOR.'sub', '->calculateRelativeDir() works without going up one dir');
+
+$source = 'c:\sfproject\web\myplugin';
+$target = 'd:\symfony\plugins\myplugin\web';
+$t->is($filesystem->calculateRelativeDir($source, $target), 'd:\symfony\plugins\myplugin\web', '->calculateRelativeDir() returns absolute path when no relative path possible');
