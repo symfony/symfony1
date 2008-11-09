a2646891e4ddf5dc9621e74182de4cb0d671e0f2
Fabian Lange <fabian.lange@symfony-project.org>
2008-11-09 19:42:38 +0000

    [1.2] fixed typo
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@12850 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/task/sfFilesystemTest.php
+++ test/unit/task/sfFilesystemTest.php
@@ -7,9 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-error_reporting(error_reporting() & ~E_STRICT);
-
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
 class myFilesystem extends sfFilesystem
