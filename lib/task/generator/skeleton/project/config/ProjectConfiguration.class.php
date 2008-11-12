ea4ce755fb718960a1a8f1a53a8880f49473ce5e
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-11-12 18:33:00 +0000

    [1.2] added sfCompat10Plugin as an excluded plugin in the default skeleton
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@12960 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/generator/skeleton/project/config/ProjectConfiguration.class.php
+++ lib/task/generator/skeleton/project/config/ProjectConfiguration.class.php
@@ -8,6 +8,6 @@ class ProjectConfiguration extends sfProjectConfiguration
   public function setup()
   {
     // for compatibility / remove and enable only the plugins you want
-    $this->enableAllPluginsExcept('sfDoctrinePlugin');
+    $this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin'));
   }
 }
