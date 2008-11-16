817e7938822a8f52c44a874eecaa5a0f5e2ef2e8
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-11-16 19:34:05 +0000

    [1.2] fixed the configure:database task for Propel 1.3
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@13035 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/configure/sfConfigureDatabaseTask.class.php
+++ lib/task/configure/sfConfigureDatabaseTask.class.php
@@ -100,10 +100,10 @@ EOF;
       if (file_exists($propelini))
       {
         $content = file_get_contents($propelini);
-
-        if (preg_match('/^(.+?):\/\//', $arguments['dsn'], $match))
+        if (preg_match('/^(.+?):/', $arguments['dsn'], $match))
         {
           $content = preg_replace('/^propel\.database(\s*)=(\s*)(.+?)$/m', 'propel.database$1=$2'.$match[1], $content);
+          $content = preg_replace('/^propel\.database.driver(\s*)=(\s*)(.+?)$/m', 'propel.database.driver$1=$2'.$match[1], $content);
           $content = preg_replace('/^propel\.database\.createUrl(\s*)=(\s*)(.+?)$/m', 'propel.database.createUrl$1=$2'.$arguments['dsn'], $content);
           $content = preg_replace('/^propel\.database\.url(\s*)=(\s*)(.+?)$/m', 'propel.database.url$1=$2'.$arguments['dsn'], $content);
 
