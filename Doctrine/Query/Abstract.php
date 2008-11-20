af5d4938d6782279e667e6b8b08574ac486e2424
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-11-20 17:51:55 +0000

    [1.0, 1.1] Fixing issue with update() and delete() not being consistant.
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5195 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Query/Abstract.php
+++ Doctrine/Query/Abstract.php
@@ -1636,9 +1636,12 @@ abstract class Doctrine_Query_Abstract
      *
      * @return Doctrine_Query
      */
-    public function delete()
+    public function delete($from = null)
     {
         $this->_type = self::DELETE;
+        if ($from != null) {
+            return $this->_addDqlQueryPart('from', $from);
+        }
         return $this;
     }
 
@@ -1649,10 +1652,13 @@ abstract class Doctrine_Query_Abstract
      * @param string $update        Query UPDATE part
      * @return Doctrine_Query
      */
-    public function update($update)
+    public function update($from = null)
     {
         $this->_type = self::UPDATE;
-        return $this->_addDqlQueryPart('from', $update);
+        if ($from != null) {
+            return $this->_addDqlQueryPart('from', $from);
+        }
+        return $this;
     }
 
     /**
