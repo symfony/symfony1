0141537a1fd0457a0f272db16312f18492cc4c7b
guilhermeblanco <guilhermeblanco@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-11-21 13:16:05 +0000

    [1.0, 1.1] fixes #1685. Fixes Query Cache that was not respecting TTL. Thanks for the tip and patch!
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5206 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Query/Abstract.php
+++ Doctrine/Query/Abstract.php
@@ -1973,7 +1973,7 @@ abstract class Doctrine_Query_Abstract
         $this->_queryCache = $driver;
 
         if ($timeToLive !== null) {
-            $this->setResultCacheLifeSpan($timeToLive);
+            $this->setQueryCacheLifeSpan($timeToLive);
         }
         return $this;
     }
