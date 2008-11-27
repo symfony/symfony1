54903578978034f92ff991810cd68b605633b88b
Ian Christian <ian.christian@symfony-project.org>
2008-11-27 13:30:37 +0000

    checking that upload directory exists before chmoding - removing error
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@13423 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/project/sfProjectPermissionsTask.class.php
+++ lib/task/project/sfProjectPermissionsTask.class.php
@@ -40,9 +40,12 @@ EOF;
    */
   protected function execute($arguments = array(), $options = array())
   {
+    if (file_exists(sfConfig::get('sf_upload_dir')))
+    {
+      $this->getFilesystem()->chmod(sfConfig::get('sf_upload_dir'), 0777);
+    }
     $this->getFilesystem()->chmod(sfConfig::get('sf_cache_dir'), 0777);
     $this->getFilesystem()->chmod(sfConfig::get('sf_log_dir'), 0777);
-    $this->getFilesystem()->chmod(sfConfig::get('sf_upload_dir'), 0777);
     $this->getFilesystem()->chmod(sfConfig::get('sf_root_dir').DIRECTORY_SEPARATOR.'symfony', 0777);
 
     $dirs = array(sfConfig::get('sf_cache_dir'), sfConfig::get('sf_upload_dir'), sfConfig::get('sf_log_dir'));
