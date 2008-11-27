2f9b35500f9a7facece183926c0960e6ebd101bb
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-11-27 10:50:42 +0000

    [1.1, 1.2] added DOCTYPE to generated XLIFF files
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@13398 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/i18n/sfMessageSource_XLIFF.class.php
+++ lib/i18n/sfMessageSource_XLIFF.class.php
@@ -430,6 +430,7 @@ class sfMessageSource_XLIFF extends sfMessageSource_File
 
     return <<<EOD
 <?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE xliff PUBLIC "-//XLIFF//DTD XLIFF//EN" "http://www.oasis-open.org/committees/xliff/documents/xliff.dtd" >
 <xliff version="1.0">
   <file source-language="EN" target-language="{$this->culture}" datatype="plaintext" original="$catalogue" date="$date" product-name="$catalogue">
     <header />
