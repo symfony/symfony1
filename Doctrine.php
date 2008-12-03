5e796ba1e7246b0d94216936c5a857efb3303b4d
pookey <pookey@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-12-03 01:45:23 +0000

    [1.1] Releasing 1.1.0-ALPHA1
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5243 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine.php
+++ Doctrine.php
@@ -36,7 +36,7 @@ final class Doctrine
     /**
      * VERSION
      */
-    const VERSION                   = '1.1.0-DEV';
+    const VERSION                   = '1.1.0-ALPHA1';
 
     /**
      * ERROR CONSTANTS
