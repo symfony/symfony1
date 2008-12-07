9e59d652700357aeb784c1b76aed98129fe1186d
Fabien Potencier <fabien.potencier@symfony-project.org>
2008-12-07 09:44:09 +0000

    [1.2] fixed a potential warning
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@13805 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/parts/sortingConfiguration.php
+++ lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/parts/sortingConfiguration.php
@@ -2,7 +2,7 @@
   {
 <?php if ($sort = (isset($this->config['list']['sort']) ? $this->config['list']['sort'] : false)): ?>
 <?php if (!is_array($sort)) $sort = array($sort, 'asc'); ?>
-    return array('<?php echo $sort[0] ?>', '<?php echo $sort[1] ?>');
+    return array('<?php echo $sort[0] ?>', '<?php echo isset($sort[1]) ? $sort[1] : 'asc' ?>');
 <?php else: ?>
     return array(null, null);
 <?php endif; ?>
