ca2d6cd96c3f0e164a419be73bed603609572081
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2008-12-19 21:00:29 +0000

    [1.0, 1.1] Fixing strict standards notice that happens in certain cases
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5319 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Validator/Timestamp.php
+++ Doctrine/Validator/Timestamp.php
@@ -44,7 +44,9 @@ class Doctrine_Validator_Timestamp
             return true;
         }
 
-        list($date, $time) = explode(' ', trim($value));
+        $e = explode(' ', trim($value));
+        $date = isset($e[0]) ? $e[0]:null;
+        $time = isset($e[1]) ? $e[1]:null;
 
         $dateValidator = Doctrine_Validator::getValidator('date');
         $timeValidator = Doctrine_Validator::getValidator('time');
