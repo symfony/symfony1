733b220d3374db3361ef4385ecee3f7763f78b78
Carl Vondrick <carl.vondrick@symfony-project.org>
2009-01-02 03:48:52 +0000

    1.2: updated sfProjectFreezeTask help text to be more verbose
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.2@14421 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/project/sfProjectFreezeTask.class.php
+++ lib/task/project/sfProjectFreezeTask.class.php
@@ -36,7 +36,10 @@ class sfProjectFreezeTask extends sfBaseTask
 The [project:freeze|INFO] task copies all the symfony core files to
 the current project:
 
-  [./symfony project:freeze|INFO]
+  [./symfony project:freeze /path/to/symfony/data/directory|INFO]
+
+The task takes a mandatory argument of the path to the symfony data
+directory.
 
 The task also changes [config/config.php|COMMENT] to switch to the
 embedded symfony files.
