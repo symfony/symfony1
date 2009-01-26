b99abaf5b3deab3c1fb0f1d8725e1544f420fb13
guilhermeblanco <guilhermeblanco@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2009-01-26 20:40:56 +0000

    [1.0, 1.1] Fixed wrong code I introduced. No fatal errors.
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5420 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Import/Schema.php
+++ Doctrine/Import/Schema.php
@@ -195,7 +195,7 @@ class Doctrine_Import_Schema
 
         foreach ((array) $schema AS $s) {
             if (is_file($s)) {
-                $e = explode('.', $file->getFileName());
+                $e = explode('.', $s);
                 if (end($e) === $format) {
                     $array = array_merge($array, $this->parseSchema($s, $format));
                 }          
