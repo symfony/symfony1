ab2ad2af6d6bbc1ee2cf979e939c5b88e8852d93
Dustin Whittle <dustin.whittle@symfony-project.org>
2009-02-10 07:33:14 +0000

    [1.3] updated admin batch csrf token to generate at runtime (closes #5774, thanks jeremy & ansata)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@15381 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/data/generator/sfPropelModule/admin/template/templates/_list_batch_actions.php
+++ lib/plugins/sfPropelPlugin/data/generator/sfPropelModule/admin/template/templates/_list_batch_actions.php
@@ -7,9 +7,9 @@
 
 <?php endforeach; ?>
   </select>
-<?php $form = new sfForm(); if ($form->isCSRFProtected()): ?>
-  <input type="hidden" name="<?php echo $form->getCSRFFieldName() ?>" value="<?php echo $form->getCSRFToken() ?>" />
-<?php endif; ?>
+  [?php $form = new sfForm(); if ($form->isCSRFProtected()): ?]
+    <input type="hidden" name="[?php echo $form->getCSRFFieldName() ?]" value="[?php echo $form->getCSRFToken() ?]" />
+  [?php endif; ?]
   <input type="submit" value="[?php echo __('go', array(), 'sf_admin') ?]" />
 </li>
 <?php endif; ?>
