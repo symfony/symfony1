0d53c9f46097b961b232d0394f2c96fbfc313894
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2009-02-16 20:24:42 +0000

    [1.0, 1.1] Fixes and documents issue with importing nested set data fixtures (closes #1919)
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5500 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Data/Import.php
+++ Doctrine/Data/Import.php
@@ -250,10 +250,21 @@ class Doctrine_Data_Import extends Doctrine_Data
     * @param $data
     * @return boolean
     */
-    protected function _hasNaturalNestedSetFormat($className, array $data) {
-		$first = current($data);
-		return isset($first['children']) && Doctrine::getTable($className)->isTree();
+    protected function _hasNaturalNestedSetFormat($className, array &$data)
+    {
+        if (Doctrine::getTable($className)->isTree()) {
+            if (isset($data['NestedSet']) && $data['NestedSet'] == true) {
+                unset($data['NestedSet']);
+                return true;
+            } else {
+                $first = current($data);
+                return array_key_exists('children', $first);
+            }
+        } else {
+            return false;
+        }
     }
+
     /**
      * Perform the loading of the data from the passed array
      *
@@ -339,7 +350,7 @@ class Doctrine_Data_Import extends Doctrine_Data
             $data  = array();
 
             if (array_key_exists('children', $nestedSet)) {
-                $children = $nestedSet['children'];
+                $children = (array) $nestedSet['children'];
                 $children = array_reverse($children, true);
                 unset($nestedSet['children']);
             }
