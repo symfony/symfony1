bf3a863aaeae082ece5e445ce44fc221c2fdc180
guilhermeblanco <guilhermeblanco@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2009-02-27 04:02:18 +0000

    [1.0, 1.1] Fixed #1950. Added possibility to fetchTree and define a depth limit.
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5558 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Tree/NestedSet.php
+++ Doctrine/Tree/NestedSet.php
@@ -167,7 +167,9 @@ class Doctrine_Tree_NestedSet extends Doctrine_Tree implements Doctrine_Tree_Int
         // fetch tree
         $q = $this->getBaseQuery();
 
-        $q = $q->addWhere($this->_baseAlias . ".lft >= ?", 1);
+        $depth = isset($options['depth']) ? $options['depth'] : null;
+
+        $q->addWhere($this->_baseAlias . ".lft >= ?", 1);
 
         // if tree has many roots, then specify root id
         $rootId = isset($options['root_id']) ? $options['root_id'] : '1';
@@ -176,6 +178,10 @@ class Doctrine_Tree_NestedSet extends Doctrine_Tree implements Doctrine_Tree_Int
                     ", " . $this->_baseAlias . ".lft ASC");
         } else {
             $q->addOrderBy($this->_baseAlias . ".lft ASC");
+        }
+
+        if ( ! is_null($depth)) { 
+            $q->addWhere($this->_baseAlias . ".level BETWEEN ? AND ?", array(0, $depth)); 
         }
 
         $q = $this->returnQueryWithRootId($q, $rootId);
@@ -213,7 +219,7 @@ class Doctrine_Tree_NestedSet extends Doctrine_Tree implements Doctrine_Tree_Int
         $q->addWhere($this->_baseAlias . ".lft >= ? AND " . $this->_baseAlias . ".rgt <= ?", $params)
                 ->addOrderBy($this->_baseAlias . ".lft asc");
 
-		if ( ! is_null($depth)) { 
+        if ( ! is_null($depth)) { 
             $q->addWhere($this->_baseAlias . ".level BETWEEN ? AND ?", array($record->get('level'), $record->get('level')+$depth)); 
         }
 
