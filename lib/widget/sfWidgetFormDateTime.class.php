8531399195cc0bf61fdc3b6a4a21b294538c3555
Romain Dorgueil <romain.dorgueil@symfony-project.org>
2009-03-03 16:02:15 +0000

    [1.1][1.2][1.3] code conventions: useless comment removed, trailing spaces
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@15962 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/sfWidgetFormDateTime.class.php
+++ lib/widget/sfWidgetFormDateTime.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -58,7 +58,6 @@ class sfWidgetFormDateTime extends sfWidgetForm
    */
   function render($name, $value = null, $attributes = array(), $errors = array())
   {
-    // date
     $date = $this->getDateWidget($attributes)->render($name, $value);
 
     if (!$this->getOption('with_time'))
