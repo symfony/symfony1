2ec14759353bce223e43d1fe563d60a15e39e743
Fabien Potencier <fabien.potencier@symfony-project.org>
2009-03-11 08:14:07 +0000

    [1.2, 1.3] fixed cannot check all checkbox in Admin generator (IE) (closes #6006)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@16172 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/template/templates/_list.php
+++ lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/admin/template/templates/_list.php
@@ -48,7 +48,7 @@
 /* <![CDATA[ */
 function checkAll()
 {
-  var boxes = document.getElementsByTagName('input'); for(index in boxes) { box = boxes[index]; if (box.type == 'checkbox' && box.className == 'sf_admin_batch_checkbox') box.checked = document.getElementById('sf_admin_list_batch_checkbox').checked } return true;
+  var boxes = document.getElementsByTagName('input'); for(var index = 0; index < boxes.length; index++) { box = boxes[index]; if (box.type == 'checkbox' && box.className == 'sf_admin_batch_checkbox') box.checked = document.getElementById('sf_admin_list_batch_checkbox').checked } return true;
 }
 /* ]]> */
 </script>
