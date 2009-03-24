4c3e3a131de39c6b11302c7023ff2a84999a8c72
Kris Wallsmith <kris.wallsmith@gmail.com>
2009-03-24 16:49:06 +0000

    [1.1, 1.2, 1.3] Added proxies to sfOutputEscaperSafe so safe vars work as expected when accessed from an action (closes #6147)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@16553 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/escaper/sfOutputEscaperSafe.class.php
+++ lib/escaper/sfOutputEscaperSafe.class.php
@@ -16,7 +16,7 @@
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
  * @version    SVN: $Id$
  */
-class sfOutputEscaperSafe
+class sfOutputEscaperSafe extends ArrayIterator
 {
   protected
     $value = null;
@@ -29,11 +29,41 @@ class sfOutputEscaperSafe
   public function __construct($value)
   {
     $this->value = $value;
+
+    if (is_array($value) || is_object($value))
+    {
+      parent::__construct($value);
+    }
   }
 
   public function __toString()
   {
-    return $this->value;
+    return (string) $this->value;
+  }
+
+  public function __get($key)
+  {
+    return $this->value->$key;
+  }
+
+  public function __set($key, $value)
+  {
+    $this->value->$key = $value;
+  }
+
+  public function __call($method, $arguments)
+  {
+    return call_user_func_array(array($this->value, $method), $arguments);
+  }
+
+  public function __isset($key)
+  {
+    return isset($this->value->$key);
+  }
+
+  public function __unset($key)
+  {
+    unset($this->value->$key);
   }
 
   /**
