0018cf8b98276f171c55b8c10dc2386b81fa32e7
Kris Wallsmith <kris.wallsmith@gmail.com>
2009-04-17 16:01:14 +0000

    [1.2, 1.3] fixed possible php error, added unit tests
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@17398 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/routing/sfObjectRouteCollectionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
+
+$t = new lime_test(3, new lime_output_color());
+
+// ->__construct()
+$t->diag('->__construct()');
+
+try
+{
+  $collection = new sfObjectRouteCollection(array('name' => 'test_collection'));
+  $t->fail('->__construct() throws an exception if no "model" option is provided');
+}
+catch (InvalidArgumentException $e)
+{
+  $t->pass('->__construct() throws an exception if no "model" option is provided');
+}
+
+$collection = new sfObjectRouteCollection(array('name' => 'test_collection', 'model' => 'TestModel'));
+$options = $collection->getOptions();
+$t->is($options['column'], 'id', '->__construct() defaults "column" option to "id"');
+$t->is_deeply($options['requirements'], array('id' => '\d+'), '->__construct() defaults "requirements" for column to "\d+"');
