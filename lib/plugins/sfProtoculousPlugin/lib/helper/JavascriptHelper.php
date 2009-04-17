49615e4e0417a49c93b05e0d1114b500452631ef
Dustin Whittle <dustin.whittle@symfony-project.org>
2009-04-17 01:10:41 +0000

    [1.3] fixed confirm dialog does not work in ie6 (closes #4152)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@17383 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfProtoculousPlugin/lib/helper/JavascriptHelper.php
+++ lib/plugins/sfProtoculousPlugin/lib/helper/JavascriptHelper.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__).'/../../../../helper/JavascriptBaseHelper.php';
 
 /*
  * This file is part of the symfony package.
- * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * (c) 2004-2009 Fabien Potencier <fabien.potencier@symfony-project.com>
  * (c) 2004 David Heinemeier Hansson
  *
  * For the full copyright and license information, please view the LICENSE
@@ -437,7 +437,7 @@ require_once dirname(__FILE__).'/../../../../helper/JavascriptBaseHelper.php';
     }
     if (isset($options['confirm']))
     {
-      $function = "if (confirm('".escape_javascript($options['confirm'])."')) { $function; }";
+      $function = "if (window.confirm('".escape_javascript($options['confirm'])."')) { $function; }";
       if (isset($options['cancel']))
       {
         $function = $function.' else { '.$options['cancel'].' }';
