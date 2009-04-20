e5bc0f20064f530c711b8c1dd55befb538cc30dc
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2009-04-20 21:09:17 +0000

    [1.0, 1.1] Fixing issue with cache expire functions not making use of bool argument (closes #1961)
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5655 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Query/Abstract.php
+++ Doctrine/Query/Abstract.php
@@ -2033,7 +2033,7 @@ abstract class Doctrine_Query_Abstract
      */
     public function expireResultCache($expire = true)
     {
-        $this->_expireResultCache = true;
+        $this->_expireResultCache = $expire;
         return $this;
     }
 
@@ -2045,7 +2045,7 @@ abstract class Doctrine_Query_Abstract
      */
     public function expireQueryCache($expire = true)
     {
-        $this->_expireQueryCache = true;
+        $this->_expireQueryCache = $expire;
         return $this;
     }
 
