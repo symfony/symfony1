2f5a8830671bed95282ccc5927926578558c282c
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2009-05-11 17:25:43 +0000

    [1.1] Releasing Doctrine 1.1.1
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5708 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine.php
+++ Doctrine.php
@@ -36,7 +36,7 @@ final class Doctrine
     /**
      * VERSION
      */
-    const VERSION                   = '1.1.0';
+    const VERSION                   = '1.1.1';
 
     /**
      * ERROR CONSTANTS
