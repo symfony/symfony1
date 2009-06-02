05209b4d941282734ed6f407ca2d8d281ebb2e39
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2009-06-02 22:44:48 +0000

    [1.1] Fixing conversion of booleans for SoftDelete BC behavior (closes #2154)
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.1/lib@5808 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Template/Listener/SoftDelete.php
+++ Doctrine/Template/Listener/SoftDelete.php
@@ -103,7 +103,7 @@ class Doctrine_Template_Listener_SoftDelete extends Doctrine_Record_Listener
                 $query->set($field, '?', date('Y-m-d H:i:s', time()));
                 $query->addWhere($field . ' IS NULL');
             } else if ($this->_options['type'] == 'boolean') {
-                $query->set($field, '?', array('true'));
+                $query->set($field, $query->getConnection()->convertBooleans(true));
                 $query->addWhere(
                     $field . ' = ' . $query->getConnection()->convertBooleans(false)
                 );
