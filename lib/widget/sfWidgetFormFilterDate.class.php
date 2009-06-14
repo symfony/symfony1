23f872ab829e142e81f67ba3c0e1662f7903f754
Fabien Potencier <fabien.potencier@symfony-project.org>
2009-06-14 14:29:19 +0000

    [1.2, 1.3] added some missing PHPDoc (closes #6620)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@19243 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/sfWidgetFormFilterDate.class.php
+++ lib/widget/sfWidgetFormFilterDate.class.php
@@ -25,6 +25,8 @@ class sfWidgetFormFilterDate extends sfWidgetFormDateRange
    *
    *  * with_empty:      Whether to add the empty checkbox (true by default)
    *  * empty_label:     The label to use when using an empty checkbox
+   *  * template:        The template used for from date and to date
+   *                     Available placeholders: %from_date%, %to_date%
    *  * filter_template: The template to use to render the widget
    *                     Available placeholders: %date_range%, %empty_checkbox%, %empty_label%
    *
