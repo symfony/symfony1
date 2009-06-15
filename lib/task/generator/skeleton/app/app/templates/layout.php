900cd3617ba9fefd570deea88fe01ebeb1a1f611
Fabien Potencier <fabien.potencier@symfony-project.org>
2009-06-15 08:56:56 +0000

    [1.3] fixed missing css and js inclusion on new projects (closes #6626)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@19259 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/generator/skeleton/app/app/templates/layout.php
+++ lib/task/generator/skeleton/app/app/templates/layout.php
@@ -5,6 +5,8 @@
     <?php include_metas() ?>
     <?php include_title() ?>
     <link rel="shortcut icon" href="/favicon.ico" />
+    <?php include_stylesheets() ?>
+    <?php include_javascripts() ?>
   </head>
   <body>
     <?php echo $sf_content ?>
