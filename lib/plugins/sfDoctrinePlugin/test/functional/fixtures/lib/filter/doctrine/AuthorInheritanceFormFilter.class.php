e10c08454dea0aae4ecb8f324d937549e4231b88
Jonathan Wage <jonwage@gmail.com>
2009-07-07 21:24:41 +0000

    [1.3] sfDoctrinePlugin: adding missing file
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@19991 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/plugins/sfDoctrinePlugin/test/functional/fixtures/lib/filter/doctrine/AuthorInheritanceFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * AuthorInheritance filter form.
+ *
+ * @package    filters
+ * @subpackage AuthorInheritance *
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ */
+class AuthorInheritanceFormFilter extends BaseAuthorInheritanceFormFilter
+{
+  public function configure()
+  {
+  }
+}
