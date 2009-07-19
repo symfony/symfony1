c80c84dfc1ae024f073b8b269aa9743c63de7168
Fabien Potencier <fabien.potencier@symfony-project.org>
2009-07-19 10:36:56 +0000

    [1.2, 1.3] fixed finfo() usage for PHP 5.3 (closes #)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@20296 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/validator/sfValidatorFile.class.php
+++ lib/validator/sfValidatorFile.class.php
@@ -212,6 +212,12 @@ class sfValidatorFile extends sfValidatorBase
 
     $type = $finfo->file($file);
 
+    // remove charset (added as of PHP 5.3)
+    if (false !== $pos = strpos($type, ';'))
+    {
+      $type = substr($type, 0, $pos);
+    }
+
     return $type;
   }
 
