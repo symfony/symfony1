ef7fc38be451e9bdbea1efd379ce294f0a418857
Kris Wallsmith <kris.wallsmith@gmail.com>
2009-08-12 18:07:26 +0000

    [1.3] added rebuild of database to doctrine:build --all option, added --all-classes options for only generating classes, added exception if no build options are provided
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@21106 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineBuildTask.class.php
+++ lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineBuildTask.class.php
@@ -21,6 +21,16 @@ require_once(dirname(__FILE__).'/sfDoctrineBaseTask.class.php');
  */
 class sfDoctrineBuildTask extends sfDoctrineBaseTask
 {
+  const
+    BUILD_MODEL   = 1,
+    BUILD_FORMS   = 2,
+    BUILD_FILTERS = 4,
+    BUILD_SQL     = 8,
+    BUILD_DB      = 16,
+
+    BUILD_ALL_CLASSES = 7,  // model, forms, filters
+    BUILD_ALL         = 31; // model, forms, filters, sql, db
+
   /**
    * @see sfTask
    */
@@ -30,12 +40,13 @@ class sfDoctrineBuildTask extends sfDoctrineBaseTask
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', true),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
       new sfCommandOption('no-confirmation', null, sfCommandOption::PARAMETER_NONE, 'Whether to force dropping of the database'),
-      new sfCommandOption('all', null, sfCommandOption::PARAMETER_NONE, 'Runs all code-generating options'),
-      new sfCommandOption('db', null, sfCommandOption::PARAMETER_NONE, 'Drop, create, and either insert SQL or migrate the database'),
+      new sfCommandOption('all', null, sfCommandOption::PARAMETER_NONE, 'Build everything and reset the database'),
+      new sfCommandOption('all-classes', null, sfCommandOption::PARAMETER_NONE, 'Build all classes'),
       new sfCommandOption('model', null, sfCommandOption::PARAMETER_NONE, 'Build model classes'),
       new sfCommandOption('forms', null, sfCommandOption::PARAMETER_NONE, 'Build form classes'),
       new sfCommandOption('filters', null, sfCommandOption::PARAMETER_NONE, 'Build filter classes'),
       new sfCommandOption('sql', null, sfCommandOption::PARAMETER_NONE, 'Build SQL'),
+      new sfCommandOption('db', null, sfCommandOption::PARAMETER_NONE, 'Drop, create, and either insert SQL or migrate the database'),
       new sfCommandOption('and-migrate', null, sfCommandOption::PARAMETER_NONE, 'Migrate the database'),
       new sfCommandOption('and-load', null, sfCommandOption::PARAMETER_OPTIONAL | sfCommandOption::IS_ARRAY, 'Load fixture data'),
       new sfCommandOption('and-append', null, sfCommandOption::PARAMETER_OPTIONAL | sfCommandOption::IS_ARRAY, 'Append fixture data'),
@@ -51,36 +62,44 @@ The [doctrine:build|INFO] task generates code based on your schema:
 
   [./symfony doctrine:build|INFO]
 
-You must specify what you would like built. If you want model and form classes
-built use the [--model|COMMENT] and [--forms|COMMENT] options:
+You must specify what you would like built. For instance, if you want model
+and form classes built use the [--model|COMMENT] and [--forms|COMMENT] options:
 
   [./symfony doctrine:build --model --forms|INFO]
 
-You can also use the [--all|COMMENT] option if you would like all classes and SQL files
-to be built.
+You can use the [--all|COMMENT] shortcut option if you would like all classes and
+SQL files generated and the database rebuilt:
 
   [./symfony doctrine:build --all|INFO]
 
 This is equivalent to running the following tasks:
 
+  [./symfony doctrine:drop-db|INFO]
+  [./symfony doctrine:build-db|INFO]
   [./symfony doctrine:build-model|INFO]
   [./symfony doctrine:build-forms|INFO]
   [./symfony doctrine:build-filters|INFO]
   [./symfony doctrine:build-sql|INFO]
+  [./symfony doctrine:insert-sql|INFO]
 
-The [--and-migrate|COMMENT] option will run any pending migrations once the building
-tasks are complete:
+You can also generate only class files by using the [--all-classes|COMMENT] shortcut
+option. When this option is used alone, the database will not be modified.
+
+  [./symfony doctrine:build --all-classes|INFO]
+
+The [--and-migrate|COMMENT] option will run any pending migrations once the builds
+are complete:
 
   [./symfony doctrine:build --db --and-migrate|INFO]
 
 The [--and-load|COMMENT] option will load data from the project and plugin
-[/data/fixtures|COMMENT] directories:
+[data/fixtures/|COMMENT] directories:
 
-  [./symfony doctrine:build --all --and-load|INFO]
+  [./symfony doctrine:build --db --and-migrate --and-load|INFO]
 
 To specify what fixtures are loaded, add a parameter to the [--and-load|COMMENT] option:
 
-  [./symfony doctrine:build --all --and-load=data/fixtures/dev/|INFO]
+  [./symfony doctrine:build --all --and-load="data/fixtures/dev/"|INFO]
 
 To append fixture data without erasing any records from the database, include
 the [--and-append|COMMENT] option:
@@ -94,7 +113,12 @@ EOF;
    */
   protected function execute($arguments = array(), $options = array())
   {
-    if ($options['db'])
+    if (!$mode = $this->calculateMode($options))
+    {
+      throw new InvalidArgumentException(sprintf("You must include one or more of the following build options:\n--%s\n\nSee this task's help page for more information:\n\n  php symfony help doctrine:build", join(', --', array_keys($this->getBuildOptions()))));
+    }
+
+    if (self::BUILD_DB == (self::BUILD_DB & $mode))
     {
       $task = new sfDoctrineDropDbTask($this->dispatcher, $this->formatter);
       $task->setCommandApplication($this->commandApplication);
@@ -119,7 +143,7 @@ EOF;
       // :insert-sql (or :migrate) will also be run, below
     }
 
-    if ($options['model'] || $options['forms'] || $options['filters'] || $options['sql'] || $options['all'])
+    if (self::BUILD_MODEL == (self::BUILD_MODEL & $mode))
     {
       $task = new sfDoctrineBuildModelTask($this->dispatcher, $this->formatter);
       $task->setCommandApplication($this->commandApplication);
@@ -132,7 +156,7 @@ EOF;
       }
     }
 
-    if ($options['forms'] || $options['all'])
+    if (self::BUILD_FORMS == (self::BUILD_FORMS & $mode))
     {
       $task = new sfDoctrineBuildFormsTask($this->dispatcher, $this->formatter);
       $task->setCommandApplication($this->commandApplication);
@@ -145,7 +169,7 @@ EOF;
       }
     }
 
-    if ($options['filters'] || $options['all'])
+    if (self::BUILD_FILTERS == (self::BUILD_FILTERS & $mode))
     {
       $task = new sfDoctrineBuildFiltersTask($this->dispatcher, $this->formatter);
       $task->setCommandApplication($this->commandApplication);
@@ -158,7 +182,7 @@ EOF;
       }
     }
 
-    if ($options['sql'] || $options['all'])
+    if (self::BUILD_SQL == (self::BUILD_SQL & $mode))
     {
       $task = new sfDoctrineBuildSqlTask($this->dispatcher, $this->formatter);
       $task->setCommandApplication($this->commandApplication);
@@ -183,7 +207,7 @@ EOF;
         return $ret;
       }
     }
-    else if ($options['db'])
+    else if (self::BUILD_DB == (self::BUILD_DB & $mode))
     {
       $task = new sfDoctrineInsertSqlTask($this->dispatcher, $this->formatter);
       $task->setCommandApplication($this->commandApplication);
@@ -227,5 +251,51 @@ EOF;
         }
       }
     }
+  }
+
+  /**
+   * Calculates a bit mode based on the supplied options.
+   *
+   * @param  array $options
+   *
+   * @return integer
+   */
+  protected function calculateMode($options = array())
+  {
+    $mode = 0;
+    foreach ($this->getBuildOptions() as $name => $value)
+    {
+      if (isset($options[$name]) && true === $options[$name])
+      {
+        if (0 === strpos($name, 'all'))
+        {
+          // use any selected "all" option as the mode
+          return $value;
+        }
+
+        $mode += $value;
+      }
+    }
+
+    return $mode;
+  }
+
+  /**
+   * Returns an array of valid build options.
+   *
+   * @return array An array of option names and their mode
+   */
+  protected function getBuildOptions()
+  {
+    $options = array();
+    foreach ($this->options as $option)
+    {
+      if (defined($constant = __CLASS__.'::BUILD_'.str_replace('-', '_', strtoupper($option->getName()))))
+      {
+        $options[$option->getName()] = constant($constant);
+      }
+    }
+
+    return $options;
   }
 }
