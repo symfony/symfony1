bd320571b075425bee2405babdb4c670a39fc1c8
Fabien Potencier <fabien.potencier@symfony-project.org>
2009-09-16 13:16:05 +0000

    [sfGuardPlugin] created branch for symfony 1.3
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@22076 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ 1.2/lib/validator/sfValidatorUrl.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfValidatorUrl validates Urls.
+ *
+ * @package    symfony
+ * @subpackage validator
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @version    SVN: $Id$
+ */
+class sfValidatorUrl extends sfValidatorRegex
+{
+  /**
+   * @param array $options   An array of options
+   * @param array $messages  An array of error messages
+   *
+   * @see sfValidatorRegex
+   */
+  protected function configure($options = array(), $messages = array())
+  {
+    parent::configure($options, $messages);
+
+    $this->setOption('pattern', '~^
+      (https?|ftps?)://                       # http or ftp (+SSL)
+      (
+        ([a-z0-9-]+\.)+[a-z]{2,6}             # a domain name
+          |                                   #  or
+        \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}    # a IP address
+      )
+      (:[0-9]+)?                              # a port (optional)
+      (/?|/\S+)                               # a /, nothing or a / with something
+    $~ix');
+  }
+}
