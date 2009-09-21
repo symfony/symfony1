symfony/symfony1

commit 3615501a7994df1a914c173b362e8e0c3246c5ce

[1.2][DC-1] Fixes issue with data fixtures import keys being overriden
git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@6357 2b2ae520-0f61-44a6-8c59-7e116ffd6265

Doctrine/Data/Import.php
@@ -171,6 +171,9 @@ class Doctrine_Data_Import extends Doctrine_Data
      */
     protected function _getImportedObject($rowKey, Doctrine_Record $record, $relationName, $referringRowKey)
     {
+        $relation = $record->getTable()->getRelation($relationName);
+        $rowKey = $relation->getClass() . $rowKey;
+
         if ( ! isset($this->_importedObjects[$rowKey])) {
             throw new Doctrine_Data_Exception(
                 sprintf('Invalid row key specified: %s, referred to in %s', $rowKey, $referringRowKey)
@@ -179,7 +182,6 @@ class Doctrine_Data_Import extends Doctrine_Data
 
         $relatedRowKeyObject = $this->_importedObjects[$rowKey];
 
-        $relation = $record->getTable()->getRelation($relationName);
         if ($relation->getClass() !== get_class($relatedRowKeyObject)) {
             if ( ! is_subclass_of($relatedRowKeyObject, $relation->getClass())) {
                 throw new Doctrine_Data_Exception(sprintf(
@@ -301,9 +303,9 @@ class Doctrine_Data_Import extends Doctrine_Data
         $buildRows = array();
         foreach ($this->_rows as $className => $classRows) {
             foreach ($classRows as $rowKey => $row) {
-                $buildRows[$rowKey] = $row;
-                $this->_importedObjects[$rowKey] = new $className();
-                $this->_importedObjects[$rowKey]->state('TDIRTY');
+                $buildRows[$className . $rowKey] = $row;
+                $this->_importedObjects[$className . $rowKey] = new $className();
+                $this->_importedObjects[$className . $rowKey]->state('TDIRTY');
             }
         }
 
@@ -360,9 +362,9 @@ class Doctrine_Data_Import extends Doctrine_Data
                 unset($nestedSet['children']);
             }
 
-            $record = $this->_importedObjects[$rowKey];
+            $record = $this->_importedObjects[$model . $rowKey];
             // remove this nested set from _importedObjects so it's not processed in the save routine for normal objects
-            unset($this->_importedObjects[$rowKey]);
+            unset($this->_importedObjects[$model . $rowKey]);
 
             if ( ! $parent) {
                 $record->save(); // save, so that createRoot can do: root id = id

commit 1c951ca27957e42c238e245563d706d7ab107656

[1.2][DC-1] fixed regression introduced in r6357 when a column aggregation subclass is referenced
git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@6394 2b2ae520-0f61-44a6-8c59-7e116ffd6265

Doctrine/Data/Import.php
@@ -184,7 +184,7 @@ class Doctrine_Data_Import extends Doctrine_Data
     protected function _getImportedObject($rowKey, Doctrine_Record $record, $relationName, $referringRowKey)
     {
         $relation = $record->getTable()->getRelation($relationName); 
-        $rowKey = $relation->getClass() . $rowKey;
+        $rowKey = $this->_getRowKeyPrefix($relation->getTable()) . $rowKey;
 
         if ( ! isset($this->_importedObjects[$rowKey])) {
             throw new Doctrine_Data_Exception(
@@ -194,12 +194,12 @@ class Doctrine_Data_Import extends Doctrine_Data
 
         $relatedRowKeyObject = $this->_importedObjects[$rowKey];
 
-        if ($relation->getClass() !== get_class($relatedRowKeyObject)) {
-            if ( ! is_subclass_of($relatedRowKeyObject, $relation->getClass())) {
-                throw new Doctrine_Data_Exception(sprintf(
-                    'Class referred to in "%s" is expected to be "%s" and "%s" was given',
-                    $referringRowKey, $relation->getClass(), get_class($relatedRowKeyObject)));
-            }
+        $relationClass = $relation->getClass();
+        if ( ! $relatedRowKeyObject instanceof $relationClass) {
+            throw new Doctrine_Data_Exception(sprintf(
+                'Class referred to in "%s" is expected to be "%s" and "%s" was given',
+                $referringRowKey, $relation->getClass(), get_class($relatedRowKeyObject)
+            ));
         }
 
         return $relatedRowKeyObject;
@@ -314,10 +314,12 @@ class Doctrine_Data_Import extends Doctrine_Data
 
         $buildRows = array();
         foreach ($this->_rows as $className => $classRows) {
+            $rowKeyPrefix = $this->_getRowKeyPrefix(Doctrine::getTable($className));
             foreach ($classRows as $rowKey => $row) {
-                $buildRows[$className . $rowKey] = $row;
-                $this->_importedObjects[$className . $rowKey] = new $className();
-                $this->_importedObjects[$className . $rowKey]->state('TDIRTY');
+                $rowKey = $rowKeyPrefix . $rowKey;
+                $buildRows[$rowKey] = $row;
+                $this->_importedObjects[$rowKey] = new $className();
+                $this->_importedObjects[$rowKey]->state('TDIRTY');
             }
         }
 
@@ -374,9 +376,11 @@ class Doctrine_Data_Import extends Doctrine_Data
                 unset($nestedSet['children']);
             }
 
-            $record = $this->_importedObjects[$model . $rowKey];
+            $rowKey = $this->_getRowKeyPrefix(Doctrine::getTable($model)) . $rowKey;
+
+            $record = $this->_importedObjects[$rowKey];
             // remove this nested set from _importedObjects so it's not processed in the save routine for normal objects
-            unset($this->_importedObjects[$model . $rowKey]);
+            unset($this->_importedObjects[$rowKey]);
 
             if ( ! $parent) {
                 $record->save(); // save, so that createRoot can do: root id = id
@@ -389,5 +393,16 @@ class Doctrine_Data_Import extends Doctrine_Data
                 $this->_loadNestedSetData($model, $children, $record);
             }
         }
+    }
+
+    /**
+     * Returns the prefix to use when indexing an object from the supplied table.
+     *
+     * @param Doctrine_Table $table
+     * @return string
+     */
+    protected function _getRowKeyPrefix(Doctrine_Table $table)
+    {
+        return sprintf('(%s) ', $table->getTableName());
     }
 }