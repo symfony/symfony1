1b02583345736a599381931c950759061e43153a
Jonathan Wage <jonwage@gmail.com>
2009-09-28 19:51:05 +0000

    [1.3] Turning off Doctrine level validation by default since it is not integrated with forms validation (addresses #5215)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@22569 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/config/sfDoctrinePluginConfiguration.class.php
+++ lib/plugins/sfDoctrinePlugin/config/sfDoctrinePluginConfiguration.class.php
@@ -55,7 +55,7 @@ class sfDoctrinePluginConfiguration extends sfPluginConfiguration
 
     $manager = Doctrine_Manager::getInstance();
     $manager->setAttribute(Doctrine::ATTR_EXPORT, Doctrine::EXPORT_ALL);
-    $manager->setAttribute(Doctrine::ATTR_VALIDATE, Doctrine::VALIDATE_ALL);
+    $manager->setAttribute(Doctrine::ATTR_VALIDATE, Doctrine::VALIDATE_NONE);
     $manager->setAttribute(Doctrine::ATTR_RECURSIVE_MERGE_FIXTURES, true);
     $manager->setAttribute(Doctrine::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
     $manager->setAttribute(Doctrine::ATTR_AUTOLOAD_TABLE_CLASSES, true);
