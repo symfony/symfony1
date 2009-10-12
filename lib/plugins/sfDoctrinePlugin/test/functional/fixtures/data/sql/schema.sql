8adf754473ced9e3aacfc5fef532a0cda6c03734
Kris Wallsmith <kris.wallsmith@gmail.com>
2009-10-12 19:52:33 +0000

    [1.3] updated fixture
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@22964 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/test/functional/fixtures/data/sql/schema.sql
+++ lib/plugins/sfDoctrinePlugin/test/functional/fixtures/data/sql/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE article (id INTEGER PRIMARY KEY AUTOINCREMENT, author_id INTEGER, is_on_homepage INTEGER, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL);
 CREATE TABLE author (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), type VARCHAR(255));
+CREATE TABLE author_inheritance_concrete (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), type VARCHAR(255), additional VARCHAR(255));
 CREATE TABLE camel_case (id INTEGER PRIMARY KEY AUTOINCREMENT, article_id INTEGER, testcamelcase VARCHAR(255));
 CREATE TABLE default_value_test (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) DEFAULT '' NOT NULL);
 CREATE TABLE form_generator_test (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255));
@@ -13,6 +14,7 @@ CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(255) U
 CREATE TABLE user_group (user_id INTEGER, group_id INTEGER, PRIMARY KEY(user_id, group_id));
 CREATE TABLE user_permission (user_id INTEGER, permission_id INTEGER, PRIMARY KEY(user_id, permission_id));
 CREATE INDEX type_idx ON author (type);
+CREATE INDEX type_idx ON author_inheritance_concrete (type);
 CREATE UNIQUE INDEX unique_test_idx ON unique_test (unique_test1, unique_test2);
 CREATE UNIQUE INDEX unique_test2_idx ON unique_test (unique_test4 ASC);
 CREATE TABLE subscription (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), status VARCHAR(255));
