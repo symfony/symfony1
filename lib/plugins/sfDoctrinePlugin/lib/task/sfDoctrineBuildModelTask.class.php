a803387c9231d1eb84fc3995d69e93d71bf63f80
Kris Wallsmith <kris.wallsmith@gmail.com>
2009-10-25 00:47:39 +0000

    [1.3] added doc tags for sfDoctrineRecord magic methods (closes #7064)
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@23314 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineBuildModelTask.class.php
+++ lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineBuildModelTask.class.php
@@ -65,10 +65,48 @@ EOF;
     $finder = sfFinder::type('file')->maxdepth(0)->name('*'.$builderOptions['suffix']);
     $before = $finder->in($config['models_path']);
 
+    $schema = $this->prepareSchemaFile($config['yaml_schema_path']);
+
     $import = new Doctrine_Import_Schema();
     $import->setOptions($builderOptions);
-    $import->importSchema($this->prepareSchemaFile($config['yaml_schema_path']), 'yml', $config['models_path']);
+    $import->importSchema($schema, 'yml', $config['models_path']);
 
+    // markup base classes with magic methods
+    foreach (sfYaml::load($schema) as $model => $definition)
+    {
+      $file = sprintf('%s%s/%s/Base%s%s', $config['models_path'], isset($definition['package']) ? '/'.substr($definition['package'], 0, strpos($definition['package'], '.')) : '', $builderOptions['baseClassesDirectory'], $model, $builderOptions['suffix']);
+      $code = file_get_contents($file);
+
+      // introspect the model without loading the class
+      if (preg_match_all('/@property (\w+) \$(\w+)/', $code, $matches, PREG_SET_ORDER))
+      {
+        $properties = array();
+        foreach ($matches as $match)
+        {
+          $properties[$match[2]] = $match[1];
+        }
+
+        $typePad = max(array_map('strlen', array_merge(array_values($properties), array($model))));
+        $namePad = max(array_map('strlen', array_keys(array_map(array('sfInflector', 'camelize'), $properties))));
+        $setters = array();
+        $getters = array();
+
+        foreach ($properties as $name => $type)
+        {
+          $camelized = sfInflector::camelize($name);
+          $collection = 'Doctrine_Collection' == $type;
+
+          $getters[] = sprintf('@method %-'.$typePad.'s %s%-'.($namePad + 2).'s Returns the current record\'s "%s" %s', $type, 'get', $camelized.'()', $name, $collection ? 'collection' : 'value');
+          $setters[] = sprintf('@method %-'.$typePad.'s %s%-'.($namePad + 2).'s Sets the current record\'s "%s" %s', $model, 'set', $camelized.'()', $name, $collection ? 'collection' : 'value');
+        }
+
+        // use the last match as a search string
+        $code = str_replace($match[0], $match[0].PHP_EOL.' * '.PHP_EOL.' * '.implode(PHP_EOL.' * ', array_merge($getters, $setters)), $code);
+        file_put_contents($file, $code);
+      }
+    }
+
+    // cleanup stub classes
     $properties = parse_ini_file(sfConfig::get('sf_config_dir').'/properties.ini', true);
     $this->getFilesystem()->replaceTokens(array_diff($finder->in($config['models_path']), $before), '', '', array(
       '##PACKAGE##'    => isset($properties['symfony']['name']) ? $properties['symfony']['name'] : 'symfony',
