920731fa3c81a7ac17ab3cb3bc5a0ace14279395
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2009-11-02 23:22:53 +0000

    [1.2][DC-137] Fixing autoloading for record generator models
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@6616 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Record/Generator.php
+++ Doctrine/Record/Generator.php
@@ -156,7 +156,7 @@ abstract class Doctrine_Record_Generator extends Doctrine_Record_Abstract
         $this->_options['className'] = str_replace('%CLASS%', $ownerClassName, $className);
 
         // check that class doesn't exist (otherwise we cannot create it)
-        if ($this->_options['generateFiles'] === false && class_exists($this->_options['className'], false)) {
+        if ($this->_options['generateFiles'] === false && class_exists($this->_options['className'])) {
             return false;
         }
 
