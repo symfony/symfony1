ff5ce40e96ebde56aa99b3dae8940c7cdf544ef9
Fabian Lange <fabian.lange@symfony-project.org>
2009-11-08 22:27:17 +0000

    [1.3] more tests for sfWebDebugPanelPropel covering glue strings and slow query flagging
    git-svn-id: file:///var/www/production/sfweb/svn/branches/1.3@23705 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/plugins/sfPropelPlugin/test/unit/debug/sfWebDebugPanelPropelTest.php
+++ lib/plugins/sfPropelPlugin/test/unit/debug/sfWebDebugPanelPropelTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(3);
+$t = new lime_test(5);
 
 class sfWebDebugPanelPropelTest extends sfWebDebugPanelPropel
 {
@@ -19,6 +19,19 @@ class sfWebDebugPanelPropelTest extends sfWebDebugPanelPropel
     $config = new PropelConfiguration(array());
     $config->setParameter('debugpdo.logging.details.slow.enabled', true);
     $config->setParameter('debugpdo.logging.details.slow.threshold', 1);
+    return $config;
+  }
+}
+
+class sfWebDebugPanelPropelTestDifferentGlue extends sfWebDebugPanelPropel
+{
+  protected function getPropelConfiguration()
+  {
+    $config = new PropelConfiguration(array());
+    $config->setParameter('debugpdo.logging.outerglue', 'xx');
+    $config->setParameter('debugpdo.logging.innerglue', '/ ');
+    $config->setParameter('debugpdo.logging.details.slow.enabled', true);
+    $config->setParameter('debugpdo.logging.details.slow.threshold', 5);
     return $config;
   }
 }
@@ -35,3 +48,10 @@ $content = $panel->getPanelContent();
 $t->like($content, '/bar&lt;1/', '->getPanelContent() returns escaped queries');
 $t->like($content, '/aText like &#039; | foo&#039;/', '->getPanelContent() works with glue string in SQL');
 $t->like($content, '/sfWebDebugWarning/', '->getPanelContent() contains a slow query warning');
+
+$logger = new sfVarLogger($dispatcher);
+$logger->log('{sfPropelLogger} time/ 3.42 secxxmem/ 2.8 MB / SELECT * FROM foo WHERE bar == 42');
+$panel = new sfWebDebugPanelPropelTestDifferentGlue(new sfWebDebug($dispatcher, $logger));
+$content = $panel->getPanelContent();
+$t->like($content, '/time\/ 3.42 sec, mem\/ 2.8 MB/', '->getPanelContent() works with strange glue strings');
+$t->unlike($content, '/sfWebDebugWarning/', '->getPanelContent() should not contain a slow warning');
