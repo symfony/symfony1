01f271117b15cd17c9be7ff1a382a6ee680aeef4
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2010-03-02 01:08:51 +0000

    [1.2][DC-374] Fixing autoincrement issue with importing sqlite db
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@7280 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Import/Sqlite.php
+++ Doctrine/Import/Sqlite.php
@@ -139,17 +139,18 @@ class Doctrine_Import_Sqlite extends Doctrine_Import
             $decl = $this->conn->dataDict->getPortableDeclaration($val);
 
             $description = array(
-                    'name'      => $val['name'],
-                    'ntype'     => $val['type'],
-                    'type'      => $decl['type'][0],
-                    'alltypes'  => $decl['type'],
-                    'notnull'   => (bool) $val['notnull'],
-                    'default'   => $val['dflt_value'],
-                    'primary'   => (bool) $val['pk'],
-                    'length'    => null,
-                    'scale'     => null,
-                    'precision' => null,
-                    'unsigned'  => null,
+                    'name'          => $val['name'],
+                    'ntype'         => $val['type'],
+                    'type'          => $decl['type'][0],
+                    'alltypes'      => $decl['type'],
+                    'notnull'       => (bool) $val['notnull'],
+                    'default'       => $val['dflt_value'],
+                    'primary'       => (bool) $val['pk'],
+                    'length'        => null,
+                    'scale'         => null,
+                    'precision'     => null,
+                    'unsigned'      => null,
+                    'autoincrement' => (bool) ($val['pk'] == 1 && $decl['type'][0] == 'integer'),
                     );
             $columns[$val['name']] = $description;
         }
