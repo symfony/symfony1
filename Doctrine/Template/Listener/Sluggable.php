8bf818d5bd03db022417667ad25d4f59c2c11dee
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2010-03-15 18:11:17 +0000

    [1.2][DC-419] Fixing issue with sluggable behavior and inheritance
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@7360 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Template/Listener/Sluggable.php
+++ Doctrine/Template/Listener/Sluggable.php
@@ -152,7 +152,22 @@ class Doctrine_Template_Listener_Sluggable extends Doctrine_Record_Listener
      */
     public function getUniqueSlug($record, $slugFromFields)
     {
-        $name = $record->getTable()->getFieldName($this->_options['name']);
+        /* fix for use with Column Aggregation Inheritance */
+        if ($record->getTable()->getOption('inheritanceMap')) {
+          $parentTable = $record->getTable()->getOption('parents');
+          $i = 0;
+          // Be sure that you do not instanciate an abstract class;
+          $reflectionClass = new ReflectionClass($parentTable[$i]);
+          while ($reflectionClass->isAbstract()) {
+            $i++;
+            $reflectionClass = new ReflectionClass($parentTable[$i]);
+          }
+          $table = Doctrine::getTable($parentTable[$i]);
+        } else {
+          $table = $record->getTable();
+        }
+
+        $name = $table->getFieldName($this->_options['name']);
         $proposal =  call_user_func_array($this->_options['builder'], array($slugFromFields, $record));
         $slug = $proposal;
 
@@ -161,7 +176,7 @@ class Doctrine_Template_Listener_Sluggable extends Doctrine_Record_Listener
 
         if ($record->exists()) {
             $identifier = $record->identifier();
-            $whereString .= ' AND r.' . implode(' != ? AND r.', $record->getTable()->getIdentifierColumnNames()) . ' != ?';
+            $whereString .= ' AND r.' . implode(' != ? AND r.', $table->getIdentifierColumnNames()) . ' != ?';
             $whereParams = array_merge($whereParams, array_values($identifier));
         }
 
@@ -179,18 +194,17 @@ class Doctrine_Template_Listener_Sluggable extends Doctrine_Record_Listener
         }
 
         // Disable indexby to ensure we get all records
-        $originalIndexBy = $record->getTable()->getBoundQueryPart('indexBy');
-        $record->getTable()->bindQueryPart('indexBy', null);
+        $originalIndexBy = $table->getBoundQueryPart('indexBy');
+        $table->bindQueryPart('indexBy', null);
 
-        $query = $record->getTable()
-            ->createQuery('r')
+        $query = $table->createQuery('r')
             ->select('r.' . $name)
             ->where($whereString , $whereParams)
             ->setHydrationMode(Doctrine_Core::HYDRATE_ARRAY);
 
         // We need to introspect SoftDelete to check if we are not disabling unique records too
-        if ($record->getTable()->hasTemplate('Doctrine_Template_SoftDelete')) {
-	        $softDelete = $record->getTable()->getTemplate('Doctrine_Template_SoftDelete');
+        if ($table->hasTemplate('Doctrine_Template_SoftDelete')) {
+	        $softDelete = $table->getTemplate('Doctrine_Template_SoftDelete');
 
 	        // we have to consider both situations here
             if ($softDelete->getOption('type') == 'boolean') {
@@ -209,7 +223,7 @@ class Doctrine_Template_Listener_Sluggable extends Doctrine_Record_Listener
         $query->free();
 
         // Change indexby back
-        $record->getTable()->bindQueryPart('indexBy', $originalIndexBy);
+        $table->bindQueryPart('indexBy', $originalIndexBy);
 
         $similarSlugs = array();
         foreach ($similarSlugResult as $key => $value) {
@@ -224,7 +238,7 @@ class Doctrine_Template_Listener_Sluggable extends Doctrine_Record_Listener
 
         // If slug is longer then the column length then we need to trim it
         // and try to generate a unique slug again
-        $length = $record->getTable()->getFieldLength($this->_options['name']);
+        $length = $table->getFieldLength($this->_options['name']);
         if (strlen($slug) > $length) {
             $slug = substr($slug, 0, $length - (strlen($i) + 1));
             $slug = $this->getUniqueSlug($record, $slug);
