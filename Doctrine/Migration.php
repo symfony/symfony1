286d289445f4d2cf24690594dd8bd91c6bf89e94
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2010-03-15 16:11:44 +0000

    [1.2][DC-508] Fixes issue with migrations on pgsql
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@7352 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Migration.php
+++ Doctrine/Migration.php
@@ -313,6 +313,9 @@ class Doctrine_Migration
     public function migrate($to = null, $dryRun = false)
     {
         $this->clearErrors();
+
+        $this->_createMigrationTable();
+
         $this->_connection->beginTransaction();
 
         try {
@@ -463,8 +466,6 @@ class Doctrine_Migration
      */
     protected function _doMigrate($to)
     {
-        $this->_createMigrationTable();
-
         $from = $this->getCurrentVersion();
 
         if ($from == $to) {
