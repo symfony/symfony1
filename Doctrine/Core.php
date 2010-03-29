29e3a0afeee62fd3f18139fbbc1b3ea043e52904
jwage <jwage@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2010-03-29 21:08:46 +0000

    [1.2] Releasing Doctrine 1.2.2
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@7492 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Core.php
+++ Doctrine/Core.php
@@ -35,7 +35,7 @@ class Doctrine_Core
     /**
      * VERSION
      */
-    const VERSION                   = '1.2.1';
+    const VERSION                   = '1.2.2';
 
     /**
      * ERROR CONSTANTS
