74df158619a935c85d72c719adaab2454df89ce4
adrive <adrive@2b2ae520-0f61-44a6-8c59-7e116ffd6265>
2010-05-13 09:33:47 +0000

    [1.2] Added oci8 driverName to condition for adding orderby fields to select clause in getLimitSubquery
    git-svn-id: file:///var/www/production/svnmirror/svn_doctrine/branches/1.2/lib@7605 2b2ae520-0f61-44a6-8c59-7e116ffd6265

--- Doctrine/Query.php
+++ Doctrine/Query.php
@@ -1391,7 +1391,7 @@ class Doctrine_Query extends Doctrine_Query_Abstract implements Countable
         $subquery .= $this->_conn->quoteIdentifier($primaryKey);
 
         // pgsql & oracle need the order by fields to be preserved in select clause
-        if ($driverName == 'pgsql' || $driverName == 'oracle' || $driverName == 'oci' || $driverName == 'mssql' || $driverName == 'odbc') {
+        if ($driverName == 'pgsql' || $driverName == 'oracle' || $driverName == 'oci' || $driverName == 'oci8' || $driverName == 'mssql' || $driverName == 'odbc') {
             foreach ($this->_sqlParts['orderby'] as $part) {
                 // Remove identifier quoting if it exists
                 $e = $this->_tokenizer->bracketExplode($part, ' ');
