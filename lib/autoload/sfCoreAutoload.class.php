ce94d4217e6d1113e697715d7c117f7833f6ff8f
Pierre-Yves LEBECQ <py.lebecq@gmail.com>
2010-10-09 03:06:50 +0200

    RELEASE_1_4_3 => v1.4.3 commit

--- lib/autoload/sfCoreAutoload.class.php
+++ lib/autoload/sfCoreAutoload.class.php
@@ -11,7 +11,7 @@
 /**
  * The current symfony version.
  */
-define('SYMFONY_VERSION', '1.4.2');
+define('SYMFONY_VERSION', '1.4.3');
 
 /**
  * sfCoreAutoload class.
@@ -22,7 +22,7 @@ define('SYMFONY_VERSION', '1.4.2');
  * @package    symfony
  * @subpackage autoload
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
- * @version    SVN: $Id: sfCoreAutoload.class.php 27968 2010-02-12 19:38:41Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfCoreAutoload.class.php 28268 2010-02-25 07:01:44Z Kris.Wallsmith $
  */
 class sfCoreAutoload
 {
--- lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Parser/sfYaml/sfYamlInline.php
+++ lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Parser/sfYaml/sfYamlInline.php
@@ -89,7 +89,7 @@ class sfYamlInline
         return is_infinite($value) ? str_ireplace('INF', '.Inf', strval($value)) : (is_string($value) ? "'$value'" : $value);
       case false !== strpos($value, "\n") || false !== strpos($value, "\r"):
         return sprintf('"%s"', str_replace(array('"', "\n", "\r"), array('\\"', '\n', '\r'), $value));
-      case preg_match('/[ \s \' " \: \{ \} \[ \] , & \* \#] | \A[ - ? | < > = ! % @ ]/x', $value):
+      case preg_match('/[ \s \' " \: \{ \} \[ \] , & \* \# \?] | \A[ - ? | < > = ! % @ ` ]/x', $value):
         return sprintf("'%s'", str_replace('\'', '\'\'', $value));
       case '' == $value:
         return "''";
