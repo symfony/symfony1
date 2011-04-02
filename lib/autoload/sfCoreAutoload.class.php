57feb2b73ca65b390aaaa83b09028232257915db
Pierre-Yves LEBECQ <py.lebecq@gmail.com>
2011-04-02 21:59:43 +0200

    RELEASE_1_3_3 => v1.3.3 commit

--- lib/autoload/sfCoreAutoload.class.php
+++ lib/autoload/sfCoreAutoload.class.php
@@ -11,7 +11,7 @@
 /**
  * The current symfony version.
  */
-define('SYMFONY_VERSION', '1.3.2');
+define('SYMFONY_VERSION', '1.3.3');
 
 /**
  * sfCoreAutoload class.
@@ -22,7 +22,7 @@ define('SYMFONY_VERSION', '1.3.2');
  * @package    symfony
  * @subpackage autoload
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
- * @version    SVN: $Id: sfCoreAutoload.class.php 27973 2010-02-12 20:04:05Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfCoreAutoload.class.php 28272 2010-02-25 07:16:16Z Kris.Wallsmith $
  */
 class sfCoreAutoload
 {
